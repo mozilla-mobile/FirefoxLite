b4f19a8fc120941444d1871557bc0df1497b6851
Ann <wei.juin.lin@gmail.com>
2017-08-01 14:46:41 +0800

    Create download record list.

--- app/src/main/java/org/mozilla/focus/activity/MainActivity.java
+++ app/src/main/java/org/mozilla/focus/activity/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -28,6 +30,7 @@ import org.mozilla.focus.utils.Settings;
 import org.mozilla.focus.web.BrowsingSession;
 import org.mozilla.focus.web.IWebView;
 import org.mozilla.focus.web.WebViewProvider;
+import org.mozilla.focus.widget.DownloadListAdapter;
 import org.mozilla.focus.widget.FragmentListener;
 
 public class MainActivity extends LocaleAwareAppCompatActivity implements FragmentListener {
@@ -189,6 +192,9 @@ public class MainActivity extends LocaleAwareAppCompatActivity implements Fragme
 
     private void setUpHistoryAndDownload() {
         final View sheet = getLayoutInflater().inflate(R.layout.bottom_sheet_history_download, null);
+        RecyclerView downloadList = (RecyclerView) sheet.findViewById(R.id.list);
+        downloadList.setAdapter(new DownloadListAdapter());
+        downloadList.setLayoutManager(new LinearLayoutManager(getBaseContext(),LinearLayoutManager.VERTICAL,false));
         historyAndDownload = new BottomSheetDialog(this);
         historyAndDownload.setContentView(sheet);
     }
