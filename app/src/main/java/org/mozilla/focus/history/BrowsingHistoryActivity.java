f4f3f9145021a121e53c8f71ce5080d9a7fa4e3f
Teng-pao Yu <osimpleo@gmail.com>
2017-08-14 14:30:35 +0800

    Remove BrowsingHistoryActivity dead code

--- app/src/main/java/org/mozilla/focus/history/BrowsingHistoryActivity.java
+++ app/src/main/java/org/mozilla/focus/history/BrowsingHistoryActivity.java
@@ -18,32 +18,25 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 public class BrowsingHistoryActivity extends AppCompatActivity implements View.OnClickListener, HistoryItemAdapter.EmptyListener {
 
     private Button mBtnClearHistory;
-    private ViewGroup mContainerRecyclerView;
+    private RecyclerView mContainerRecyclerView;
     private ViewGroup mContainerEmptyView;
     private HistoryItemAdapter mAdapter;
 
-    @SuppressFBWarnings(value = {"DLS"}, justification = "Needed for future versions")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browsing_history);
 
-        CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.browsing_history_view_root);
-
-        View bottomSheet = coordinatorLayout.findViewById(R.id.browsing_history_btm_sheet_container);
-        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-
         mBtnClearHistory = (Button) findViewById(R.id.browsing_history_btn_clear);
         mBtnClearHistory.setOnClickListener(this);
 
-        mContainerRecyclerView = (ViewGroup) findViewById(R.id.browsing_history_recycler_view);
+        mContainerRecyclerView = (RecyclerView) findViewById(R.id.browsing_history_recycler_view);
         mContainerEmptyView = (ViewGroup) findViewById(R.id.browsing_history_empty_view_container);
 
-        RecyclerView recyclerView = (RecyclerView) coordinatorLayout.findViewById(R.id.browsing_history_recycler_view);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        mAdapter = new HistoryItemAdapter(recyclerView, this, layoutManager);
-        recyclerView.setAdapter(mAdapter);
-        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new HistoryItemAdapter(mContainerRecyclerView, this, layoutManager);
+        mContainerRecyclerView.setAdapter(mAdapter);
+        mContainerRecyclerView.setLayoutManager(layoutManager);
     }
 
     @Override
