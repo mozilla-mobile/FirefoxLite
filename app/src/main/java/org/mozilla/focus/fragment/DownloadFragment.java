92cdf27fd37a08d284176fb8e55b3f64fb775523
Teng-pao Yu <osimpleo@gmail.com>
2017-08-14 14:19:51 +0800

    Generalize ListPanelDialog so it has a scrollview rather than a recyclerview
    Before the commit, we have a Recylerview in ListPanelDialog. This is a very hard
    requirement which does not give us the elasticity to change the inner list
    implementation. The Recyclerview is replaced by a NestedScrollView container
    so the Fragements or Views can be inserted. The implementation of the inner list
    is thus a responsibility of the inner Fragment/View.

---
+++ app/src/main/java/org/mozilla/focus/fragment/DownloadFragment.java
@@ -0,0 +1,76 @@
+package org.mozilla.focus.fragment;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.mozilla.focus.R;
+import org.mozilla.focus.widget.DownloadListAdapter;
+
+public class DownloadFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private DownloadListAdapter mDownloadListAdapter;
+    private BroadcastReceiver mDownloadReceiver;
+
+    public static DownloadFragment newInstance() {
+        return new DownloadFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        recyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_downloads, container, false);
+        return recyclerView;
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDownloadListAdapter = new DownloadListAdapter();
+        mDownloadListAdapter.fetchEntity();
+        mDownloadReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0L);
+                mDownloadListAdapter.updateItem(downloadId);
+            }
+        };
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        getActivity().registerReceiver(mDownloadReceiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(mDownloadReceiver);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        prepare();
+    }
+
+    private void prepare() {
+        recyclerView.setAdapter(mDownloadListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,true));
+    }
+}
--- app/src/main/java/org/mozilla/focus/fragment/ListPanelDialog.java
+++ app/src/main/java/org/mozilla/focus/fragment/ListPanelDialog.java
@@ -1,23 +1,18 @@
 package org.mozilla.focus.fragment;
 
 import android.app.Dialog;
-import android.app.DownloadManager;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 import org.mozilla.focus.R;
 import org.mozilla.focus.widget.DownloadListAdapter;
@@ -27,9 +22,8 @@ public class ListPanelDialog extends DialogFragment {
     public final static int TYPE_DOWNLOAD = 1;
     public final static int TYPE_HISTORY = 2;
     public final static int TYPE_SCREENSHOTS = 3;
-    private DownloadListAdapter mDownloadListAdapter;
-    private BroadcastReceiver mDownloadReceiver;
-    private RecyclerView recyclerView;
+
+    private NestedScrollView scrollView;
     private static final String TYPE = "TYPE";
 
     public static ListPanelDialog newInstance(int type) {
@@ -41,22 +35,8 @@ public class ListPanelDialog extends DialogFragment {
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mDownloadReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                Long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0L);
-                mDownloadListAdapter.updateItem(downloadId);
-            }
-        };
-    }
-
-    @Override
     public void onResume() {
         super.onResume();
-        IntentFilter intentFilter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
-        getActivity().registerReceiver(mDownloadReceiver, intentFilter);
         switch (getArguments().getInt(TYPE)) {
             case TYPE_DOWNLOAD:
                 showDownload();
@@ -84,18 +64,12 @@ public class ListPanelDialog extends DialogFragment {
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        getActivity().unregisterReceiver(mDownloadReceiver);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_listpanel_dialog, container, false);
-        recyclerView = (RecyclerView) v.findViewById(R.id.bottom_sheet);
-        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(recyclerView);
+        scrollView = (NestedScrollView) v.findViewById(R.id.main_content);
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(scrollView);
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
         bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
@@ -137,28 +111,16 @@ public class ListPanelDialog extends DialogFragment {
         return v;
     }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        prepareDownload();
-    }
-
-    private void prepareDownload() {
-        mDownloadListAdapter = new DownloadListAdapter();
-        mDownloadListAdapter.fetchEntity();
-    }
-
     private void showDownload() {
-        recyclerView.setAdapter(mDownloadListAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,true));
-        recyclerView.scrollToPosition(0);
+        DownloadFragment downloadFragment = DownloadFragment.newInstance();
+        getChildFragmentManager().beginTransaction().replace(R.id.main_content, downloadFragment).commit();
     }
 
     private void showHistory() {
-        recyclerView.setAdapter(null);
+
     }
 
     private void showScreenshots() {
-        recyclerView.setAdapter(null);
+
     }
 }
