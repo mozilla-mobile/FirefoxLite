982a4d221bed04a71030c03a28ed7c0c2d28688d
Julian_Chu <walkingice0204@gmail.com>
2017-08-15 18:27:02 +0800

    indent to make Android Studio happy

--- app/src/main/java/org/mozilla/focus/web/BrowsingSession.java
+++ app/src/main/java/org/mozilla/focus/web/BrowsingSession.java
@@ -5,18 +5,17 @@
 
 package org.mozilla.focus.web;
 
-import java.lang.ref.WeakReference;
-
 import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import org.mozilla.focus.utils.SafeIntent;
 
+import java.lang.ref.WeakReference;
+
 /**
  * A global object keeping the state of the current browsing session.
- *
+ * <p>
  * For now it only tracks whether a browsing session is active or not.
  */
 public class BrowsingSession {
@@ -35,7 +34,9 @@ public class BrowsingSession {
 
     private int blockedTrackers;
     private WeakReference<TrackingCountListener> listenerWeakReference;
-    private @Nullable CustomTabConfig customTabConfig;
+
+    @Nullable
+    private CustomTabConfig customTabConfig;
 
     private BrowsingSession() {
         listenerWeakReference = new WeakReference<>(null);
@@ -76,7 +77,8 @@ public class BrowsingSession {
         return customTabConfig != null;
     }
 
-    public @NonNull CustomTabConfig getCustomTabConfig() {
+    @NonNull
+    public CustomTabConfig getCustomTabConfig() {
         if (!isCustomTab()) {
             throw new IllegalStateException("Can't retrieve custom tab config for normal browsing session");
         }
