f0d21907e717be76b43564749de5b3f80239442b
Julian_Chu <walkingice0204@gmail.com>
2017-08-24 16:51:05 +0800

    add Constants class
    To store constants which will be used cross classes

---
+++ app/src/main/java/org/mozilla/focus/utils/Constants.java
@@ -0,0 +1,43 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.utils;
+
+public class Constants {
+
+    /**
+     * Activity action, to prompt dialog to grant permission
+     */
+    public static final String ACTION_REQUEST_PERMISSION = "org.mozilla.action.REQUEST_PERMISSION";
+
+    /**
+     * Activity action, to show a message to user
+     */
+    public static final String ACTION_NOTIFY_UI = "org.mozilla.action.NOTIFY_UI";
+
+    /**
+     * To indicate this intent is created when we were operating file
+     */
+    public static final String CATEGORY_FILE_OPERATION = "org.mozilla.category.FILE_OPERATION";
+
+    /**
+     * Option value for download id comes from Download Manager. Its value type should be *long*
+     */
+    public static final String EXTRA_DOWNLOAD_ID = "org.mozilla.extra.download_id";
+
+    /**
+     * Option value for file path. Its value type should be *string*
+     */
+    public static final String EXTRA_FILE_PATH = "org.mozilla.extra.file_path";
+
+    /**
+     * Option value as message. Its value type should be *CharSequence*
+     */
+    public static final String EXTRA_MESSAGE = "org.mozilla.extra.message";
+
+    private Constants() {
+        throw new RuntimeException("How dare you try to initialize Constants");
+    }
+}
