d7d95c7d166d1d2d6cb5d3b965789c2f4b6938dc
Julian_Chu <walkingice0204@gmail.com>
2017-08-28 15:05:44 +0800

    implement Receiver and Service for file moving
    DownloadCompleteReceiver is a receiver to listen Download-complete
    broadcast, it will start RelocateService in another thread to perform
    advance file operation.
    
    RelocateService is a service running on working thread, to perform
    advanced file operation for downloaded file - to relocate file to
    respect user's preferences.
    
    If the relocating success, the serivce will also update database.

---
+++ app/src/main/java/org/mozilla/focus/components/DownloadCompleteReceiver.java
@@ -0,0 +1,63 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.components;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.text.TextUtils;
+
+import java.io.File;
+
+public class DownloadCompleteReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+
+        if (downloadId == -1) {
+            return;
+        }
+
+        final DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        final DownloadManager.Query query = new DownloadManager.Query();
+
+        query.setFilterById(downloadId);
+        final Cursor cursor = downloadManager.query(query);
+
+        if (cursor.moveToFirst()) {
+            final int downloadStatus = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
+            final String localUriStr = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+            final String mediaType = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_MEDIA_TYPE));
+            if ((downloadStatus == DownloadManager.STATUS_SUCCESSFUL)
+                    && !TextUtils.isEmpty(localUriStr)) {
+
+                final Uri fileUri = Uri.parse(localUriStr);
+                if ("file".equals(fileUri.getScheme())) {
+
+                    // on some device the uri is "file:///storage/emulated/0/Download/file.png"
+                    // but the real path is "file:///storage/emulated/legacy/Download/file.png"
+                    // Since we already restrict download folder when we were making request to
+                    // DownloadManager, now we only look for the file-name in download folder.
+                    final String fileName = (new File(fileUri.getPath())).getName();
+                    final String type = Environment.DIRECTORY_DOWNLOADS;
+                    final File dir = Environment.getExternalStoragePublicDirectory(type);
+                    final File downloadedFile = new File(dir, fileName);
+                    if (downloadedFile.exists() && downloadedFile.canWrite()) {
+                        RelocateService.startActionMove(context,
+                                downloadId,
+                                downloadedFile,
+                                mediaType);
+                    }
+                }
+            }
+        }
+        cursor.close();
+    }
+}
