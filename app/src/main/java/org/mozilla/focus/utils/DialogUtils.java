ef25457f70c8c8cb5a04f05b106257f31b5a039f
Joseph Yu <joseph.yu@yarboroughtech.com>
2017-09-01 14:23:15 +0800

    Using ScreenshotObserver to monitor screenshot is captured.

--- app/src/main/java/org/mozilla/focus/utils/DialogUtils.java
+++ app/src/main/java/org/mozilla/focus/utils/DialogUtils.java
@@ -1,12 +1,15 @@
 package org.mozilla.focus.utils;
 
-
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import org.mozilla.focus.R;
 import org.mozilla.focus.activity.MainActivity;
@@ -79,5 +82,33 @@ public class DialogUtils {
         dialog.setCanceledOnTouchOutside(true);
         dialog.show();
         Settings.getInstance(context).setShareAppDialogDidShow();
+    }
+
+    public static AlertDialog showScreenshotOnBoardingDialog(Context context) {
+        final AlertDialog dialog = new AlertDialog.Builder(context).create();
+        View dialogView = LayoutInflater.from(context).inflate(R.layout.layout_screenshot_onboarding_dialog, null);
+        
+        TextView textView = (TextView) dialogView.findViewById(R.id.dialog_screenshot_on_boarding_text);
+        ImageSpan imageSpan = new ImageSpan(context, R.drawable.action_capture);
+        String emptyPrefix = context.getString(R.string.screenshot_on_boarding_text_msg_prefix);
+        String emptyPostfix = context.getString(R.string.screenshot_on_boarding_text_msg_postfix);
+        SpannableString spannableString = new SpannableString(emptyPrefix + emptyPostfix);
+
+        int start = emptyPrefix.length();
+        int end = start + 1;
+        spannableString.setSpan(imageSpan, start, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        textView.setText(spannableString);
+
+        dialogView.findViewById(R.id.dialog_screenshot_on_boarding_btn_got_it).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(dialog != null)
+                    dialog.dismiss();
+            }
+        });
+        dialog.setView(dialogView);
+        dialog.show();
+        Settings.getInstance(context).setScreenshotOnBoardingDone();
+        return dialog;
     }
 }
