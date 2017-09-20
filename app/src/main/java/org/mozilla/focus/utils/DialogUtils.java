6a88b0a6fea042a06b63161fbc5acd218b6d7853
Joseph Yu <joseph.yu@yarboroughtech.com>
2017-09-20 16:14:09 +0800

    Update screenshot promotion dialog UI.

--- app/src/main/java/org/mozilla/focus/utils/DialogUtils.java
+++ app/src/main/java/org/mozilla/focus/utils/DialogUtils.java
@@ -1,9 +1,9 @@
 package org.mozilla.focus.utils;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ImageSpan;
@@ -87,17 +87,6 @@ public class DialogUtils {
     public static AlertDialog showScreenshotOnBoardingDialog(Context context) {
         final AlertDialog dialog = new AlertDialog.Builder(context).create();
         View dialogView = LayoutInflater.from(context).inflate(R.layout.layout_screenshot_onboarding_dialog, null);
-        
-        TextView textView = (TextView) dialogView.findViewById(R.id.dialog_screenshot_on_boarding_text);
-        ImageSpan imageSpan = new ImageSpan(context, R.drawable.action_capture);
-        String emptyPrefix = context.getString(R.string.screenshot_on_boarding_text_msg_prefix);
-        String emptyPostfix = context.getString(R.string.screenshot_on_boarding_text_msg_postfix);
-        SpannableString spannableString = new SpannableString(emptyPrefix + emptyPostfix);
-
-        int start = emptyPrefix.length();
-        int end = start + 1;
-        spannableString.setSpan(imageSpan, start, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
-        textView.setText(spannableString);
 
         dialogView.findViewById(R.id.dialog_screenshot_on_boarding_btn_got_it).setOnClickListener(new View.OnClickListener() {
             @Override
