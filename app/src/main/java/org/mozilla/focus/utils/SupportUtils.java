d5fc765c5de5f895eabca64de9889d06e2a56c82
Yung-Pin Lai <ben.yp.lai@gmail.com>
2017-09-28 13:33:38 +0800

    logic: Change URL of Privacy Notice

--- app/src/main/java/org/mozilla/focus/utils/SupportUtils.java
+++ app/src/main/java/org/mozilla/focus/utils/SupportUtils.java
@@ -21,7 +21,7 @@ public class SupportUtils {
     public static final String BLOCKING_HELP_URL = "";
     public static final String DEFAULT_BROWSER_URL = "https://support.mozilla.org/kb/set-firefox-focus-default-browser-android";
     public static final String YOUR_RIGHTS_URI = "file:///android_asset/rights-focus.html";
-    public static final String PRIVACY_URL = "https://www.mozilla.org/privacy/firefox-rocket/";
+    public static final String PRIVACY_URL = "https://github.com/mozilla-tw/Zerda/wiki/Telemetry";
     public static final String ABOUT_URI = "file:///android_res/raw/about.html";
 
     static final String[] SUPPORTED_URLS = new String[]{
