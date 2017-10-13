dad2f438d01f64a204c188afb2b99e0a27ad5383
Joseph Yu <joseph.yu@yarboroughtech.com>
2017-10-13 12:27:40 +0800

    Refine in-app promotion dialog UI. - Closes #1097

--- app/src/main/java/org/mozilla/focus/utils/DialogUtils.java
+++ app/src/main/java/org/mozilla/focus/utils/DialogUtils.java
@@ -21,6 +21,14 @@ public class DialogUtils {
         }
         final AlertDialog dialog = new AlertDialog.Builder(context).create();
         View dialogView = LayoutInflater.from(context).inflate(R.layout.layout_rate_app_dialog, null);
+        dialogView.findViewById(R.id.dialog_rate_app_btn_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
         dialogView.findViewById(R.id.dialog_rate_app_btn_go_rate).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,7 +69,15 @@ public class DialogUtils {
         }
         final AlertDialog dialog = new AlertDialog.Builder(context).create();
         View dialogView = LayoutInflater.from(context).inflate(R.layout.layout_share_app_dialog, null);
-        dialogView.findViewById(R.id.dialog_rate_app_btn_share).setOnClickListener(new View.OnClickListener() {
+        dialogView.findViewById(R.id.dialog_share_app_btn_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        dialogView.findViewById(R.id.dialog_share_app_btn_share).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String appPackageName = context.getPackageName();
