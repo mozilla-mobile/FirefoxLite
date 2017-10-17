7106509ec331b949ad8bec1be7310abc983118a9
Teng-pao Yu <osimpleo@gmail.com>
2017-10-17 16:26:54 +0800

    Change share app's url and pormotion message

--- app/src/main/java/org/mozilla/focus/utils/DialogUtils.java
+++ app/src/main/java/org/mozilla/focus/utils/DialogUtils.java
@@ -80,10 +80,10 @@ public class DialogUtils {
         dialogView.findViewById(R.id.dialog_share_app_btn_share).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String appPackageName = context.getPackageName();
                 Intent sendIntent = new Intent(Intent.ACTION_SEND);
                 sendIntent.setType("text/plain");
-                sendIntent.putExtra(Intent.EXTRA_TEXT, "https://play.google.com/store/apps/details?id=" + appPackageName);
+                sendIntent.putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.app_name));
+                sendIntent.putExtra(Intent.EXTRA_TEXT, context.getString(R.string.share_app_promotion_text));
                 context.startActivity(Intent.createChooser(sendIntent, null));
                 if(dialog != null) {
                     dialog.dismiss();
