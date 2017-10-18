2a32ec3e20c8de6560ce5d7070406ec96398951c
Teng-pao Yu <osimpleo@gmail.com>
2017-10-18 19:57:43 +0800

    Ignore image delete events in ScreenshotObserver

--- app/src/main/java/org/mozilla/focus/screenshot/ScreenshotObserver.java
+++ app/src/main/java/org/mozilla/focus/screenshot/ScreenshotObserver.java
@@ -11,6 +11,10 @@ import org.mozilla.focus.utils.ThreadUtils;
 
 public class ScreenshotObserver {
     public Context context;
+    // The ContentObserver updates when there is an insert but also when there is a delete,
+    // this gives us a wrong guess when an image is deleted and the top one is screenshot.
+    // To filter out deletes, we check if the lastModified time goes back.
+    private long lastModified;
 
     /**
      * Listener for screenshot changes.
@@ -33,8 +37,35 @@ public class ScreenshotObserver {
     private String[] mediaProjections = new String[] {
             MediaStore.Images.ImageColumns.DATA,
             MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME,
-            MediaStore.Images.ImageColumns.TITLE
+            MediaStore.Images.ImageColumns.TITLE,
+            MediaStore.Images.ImageColumns.DATE_MODIFIED
     };
+
+    private void initLastModified() {
+        final ContentResolver cr = context.getContentResolver();
+        ThreadUtils.postToBackgroundThread(new Runnable() {
+            @Override
+            public void run() {
+                // Find the most recent image added to the MediaStore and see if it's a screenshot.
+                final Cursor cursor = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new String[] { MediaStore.Images.ImageColumns.DATE_MODIFIED }, null, null, MediaStore.Images.ImageColumns.DATE_MODIFIED + " DESC LIMIT 1");
+                try {
+                    if (cursor == null) {
+                        return;
+                    }
+
+                    while (cursor.moveToNext()) {
+                        lastModified = cursor.getLong(0);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (cursor != null) {
+                        cursor.close();
+                    }
+                }
+            }
+        });
+    }
 
     /**
      * Start ScreenshotObserver if this device is supported and all required runtime permissions
@@ -48,6 +79,7 @@ public class ScreenshotObserver {
                     if (mediaObserver == null) {
                         mediaObserver = new MediaObserver();
                         context.getContentResolver().registerContentObserver(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, false, mediaObserver);
+                        initLastModified();
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -86,12 +118,16 @@ public class ScreenshotObserver {
                         String data = cursor.getString(0);
                         String album = cursor.getString(1);
                         String title = cursor.getString(2);
-                        if (album != null && album.toLowerCase().contains("screenshot")) {
+                        long modifiedTime = cursor.getLong(3);
+
+                        if (lastModified < modifiedTime && album != null && album.toLowerCase().contains("screenshot")) {
                             if (listener != null) {
                                 listener.onScreenshotTaken(data, title);
                                 break;
                             }
                         }
+
+                        lastModified = modifiedTime;
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
