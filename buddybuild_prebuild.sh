93579e855f06b268a7f5031a0f7f3759240cb376
Teng-pao Yu <osimpleo@gmail.com>
2017-11-17 15:22:31 +0800

    Run 'checkstyle' before buddybuild build
    BB does not run `gradle build` but `gradle assemble`,
    so we want to do this check before the gradle assemble
    since gradle check is never ran.

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+set -e # Exit (and fail) immediately if any command in this scriptfails
 
 # Pre-build script for buddybuild
 # http://docs.buddybuild.com/docs/custom-prebuild-and-postbuild-steps#section-pre-build
 
 python tools/l10n/check_translations.py
+./gradlew checkstyle
