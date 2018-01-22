a14222758fb0ccc38462d502917ff059273ced6a
Julian_Chu <walkingice0204@gmail.com>
2018-01-22 18:23:39 +0800

    tabs: add basic Tabs implementation
    This commit pull in some bacis implementation for multi-tabs.
    It defines model class, some interfaces as callback. Try to bring
    multi-tabs feature to UI in a easier way.
    
    It tries to abstract WebView and hope we can involve other system(ie.
    GeckoView) in the future.
    
    Be carefully. This implementation is still under heavy development
    and might be changed quickly.

---
+++ app/src/main/java/org/mozilla/focus/tabs/TabsChromeListener.java
@@ -0,0 +1,94 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.tabs;
+
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.webkit.GeolocationPermissions;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+/**
+ * An interface for listener which cares about changes of tabs to update browser chrome.
+ * This is similar to android.webkit.WebChromeClient, but also indicate which tab triggered callback.
+ * <p>
+ * Every methods of this listener are supposed to run in main thread.
+ */
+public interface TabsChromeListener {
+
+    /**
+     * @see android.webkit.WebChromeClient#onProgressChanged(WebView, int)
+     */
+    void onProgressChanged(@NonNull Tab tab, int progress);
+
+    /**
+     * Notify the host application a tab becomes 'current tab'. It usually happens when adding,
+     * removing or switching tabs.
+     *
+     * @param tab The tab becomes current tab
+     */
+    void onTabHoist(@NonNull Tab tab);
+
+    /**
+     * Notify the host application the total tab counts changed.
+     *
+     * @param count total tabs count
+     */
+    void onTabCountChanged(int count);
+
+    /**
+     * Notify the host application that long-press happened on a tab
+     *
+     * @param tab       The tab received long press event
+     * @param hitTarget
+     */
+    void onLongPress(@NonNull Tab tab, TabView.HitTarget hitTarget);
+
+    /**
+     * Notify the host application that a tab has entered full screen mode.
+     * <p>
+     * Some TabView implementations may pass a custom View which contains the web contents in
+     * full screen mode.
+     *
+     * @param tab      The tab which entered fullscreen.
+     * @param callback The callback needs to be invoked to request the page to exit full screen mode.
+     */
+    void onEnterFullScreen(@NonNull Tab tab,
+                           @NonNull TabView.FullscreenCallback callback);
+
+    /**
+     * Notify the host application that the a tab has exited full screen mode.
+     * <p>
+     * If a View was passed when the application entered full screen mode then this view must
+     * be hidden now.
+     *
+     * @param tab The tab which which existed fullscreen.
+     */
+    void onExitFullScreen(@NonNull Tab tab);
+
+    /**
+     * Notify the host application to show a file chooser. Usually for file uploading.
+     *
+     * @param tab               The tab which is asking file chooser.
+     * @param webView
+     * @param filePathCallback
+     * @param fileChooserParams
+     * @see android.webkit.WebChromeClient#onShowFileChooser(WebView, ValueCallback, WebChromeClient.FileChooserParams)
+     */
+    boolean onShowFileChooser(@NonNull Tab tab,
+                              WebView webView,
+                              ValueCallback<Uri[]> filePathCallback,
+                              WebChromeClient.FileChooserParams fileChooserParams);
+
+    /**
+     * @see android.webkit.WebChromeClient#onGeolocationPermissionsShowPrompt(String, GeolocationPermissions.Callback)
+     */
+    void onGeolocationPermissionsShowPrompt(@NonNull Tab tab,
+                                            String origin,
+                                            GeolocationPermissions.Callback callback);
+
+}
---
+++ app/src/main/java/org/mozilla/focus/tabs/TabsViewListener.java
@@ -0,0 +1,47 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.tabs;
+
+import android.support.annotation.NonNull;
+import android.webkit.WebView;
+
+/**
+ * An interface for listener to receive notifications and requests from Tabs.
+ * This is similar to android.webkit.WebViewClient, but also indicate which tab triggered callback.
+ * <p>
+ * Every methods of this listener are supposed to run in main thread.
+ */
+public interface TabsViewListener {
+
+    void onTabStarted(@NonNull Tab tab);
+
+    void onTabFinished(@NonNull Tab tab, boolean isSecure);
+
+    void onURLChanged(@NonNull Tab tab, String url);
+
+    /**
+     * Subsequent process after WebViewClient.shouldOverrideUrlLoading. TabView implementation will
+     * decide whether this function be invoke or not.
+     *
+     * @param url External url to handle
+     * @return true if this Listener already handled the external url
+     */
+    boolean handleExternalUrl(String url);
+
+    /**
+     * Subsequent process after WebViewClient.onReceivedError.
+     *
+     * @param url             The url that failed to load.
+     * @param updateFromError To indicate whether this callback is invoked under error. If page started loading, this value would be true.
+     * @return true Return true if the URL was handled, false if we should continue loading the current URL.
+     */
+    void updateFailingUrl(@NonNull Tab tab, String url, boolean updateFromError);
+
+    /**
+     * @see android.webkit.WebChromeClient#onReceivedTitle(WebView, String)
+     */
+    void onReceivedTitle(@NonNull Tab tab, String title);
+}
