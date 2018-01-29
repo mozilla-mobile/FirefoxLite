caf44655686f460740d3a7f5a2a84e467a06f056
Nevin Chen <cnevinchen@gmail.com>
2018-01-29 19:06:03 +0800

    espresso: Add ui test for top sites

---
+++ app/src/androidTest/java/org/mozilla/focus/activity/HomeTest.java
@@ -0,0 +1,99 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.activity;
+
+import android.content.Intent;
+import android.support.annotation.Keep;
+import android.support.test.espresso.IdlingRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mozilla.focus.Inject;
+import org.mozilla.focus.R;
+import org.mozilla.focus.helper.SessionLoadedIdlingResource;
+import org.mozilla.focus.history.model.Site;
+import org.mozilla.focus.utils.AndroidTestUtils;
+import org.mozilla.focus.utils.TopSitesUtils;
+
+import java.util.List;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.mozilla.focus.utils.AndroidTestUtils.atPosition;
+
+@Keep
+@RunWith(AndroidJUnit4.class)
+public class HomeTest {
+
+    // Defer the startup of the activity cause we want to avoid First Run / Share App / Rate App dialogs
+    @Rule
+    public final ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class, true, false);
+    private SessionLoadedIdlingResource loadingIdlingResource;
+
+    @Before
+    public void setUp() {
+        // Set the share preferences and start the activity
+        AndroidTestUtils.beforeTest();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // Remove activity from Recent Apps
+        activityRule.getActivity().finishAndRemoveTask();
+    }
+
+    @Test
+    public void testTopSite() {
+
+        // Now start the activity
+        activityRule.launchActivity(new Intent());
+        loadingIdlingResource = new SessionLoadedIdlingResource(activityRule.getActivity());
+
+        final MainActivity context = activityRule.getActivity();
+
+        try {
+            // Get test top sites
+            final JSONArray jsonDefault = new JSONArray(Inject.getDefaultTopSites(context));
+            final List<Site> defaultSites = TopSitesUtils.paresJsonToList(context, jsonDefault);
+
+            // Check the title of the sample top site is correct
+            onView(withId(R.id.main_list))
+                    .check(matches(atPosition(0, hasDescendant(withText(defaultSites.get(0).getTitle())))));
+
+            // Click and load the sample top site
+            onView(ViewMatchers.withId(R.id.main_list))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+            // After page loading completes
+            IdlingRegistry.getInstance().register(loadingIdlingResource);
+
+            // Check if the url is displayed correctly
+            onView(withText(defaultSites.get(0).getUrl())).check(matches(isDisplayed()));
+
+            // Always remember to unregister idling resource
+            IdlingRegistry.getInstance().unregister(loadingIdlingResource);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            throw new AssertionError("testTopSite failed:", e);
+
+        }
+    }
+}
---
+++ app/src/androidTest/java/org/mozilla/focus/utils/AndroidTestUtils.java
@@ -0,0 +1,65 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+import static android.support.test.internal.util.Checks.checkNotNull;
+import static org.mozilla.focus.fragment.FirstrunFragment.FIRSTRUN_PREF;
+
+
+public final class AndroidTestUtils {
+
+    public static void beforeTest() {
+        final Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        if (context == null) {
+            return;
+        }
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (preferences != null) {
+            final SharedPreferences.Editor editor = preferences.edit();
+            if (editor != null) {
+                editor.putBoolean(FIRSTRUN_PREF, true).commit();
+            }
+        }
+        final Settings settings = Settings.getInstance(context);
+        if (settings != null) {
+            settings.setShareAppDialogDidShow();
+            settings.setRateAppDialogDidShow();
+        }
+    }
+
+    public static Matcher<View> atPosition(final int position, @NonNull final Matcher<View> itemMatcher) {
+        checkNotNull(itemMatcher);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has item at position " + position + ": ");
+                itemMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(final RecyclerView view) {
+                final RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    // has no item on such position
+                    return false;
+                }
+                return itemMatcher.matches(viewHolder.itemView);
+            }
+        };
+    }
+}
