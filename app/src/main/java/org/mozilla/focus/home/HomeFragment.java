1a21444f0ba4420368f731a6d11bf81ec7cbfdbf
Nevin Chen <cnevinchen@gmail.com>
2018-01-29 19:06:03 +0800

    espresso: Add ui test for top sites

--- app/src/main/java/org/mozilla/focus/home/HomeFragment.java
+++ app/src/main/java/org/mozilla/focus/home/HomeFragment.java
@@ -24,6 +24,7 @@ import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.mozilla.focus.Inject;
 import org.mozilla.focus.R;
 import org.mozilla.focus.history.BrowsingHistoryManager;
 import org.mozilla.focus.history.model.Site;
@@ -313,8 +314,8 @@ public class HomeFragment extends LocaleAwareFragment implements TopSitesContrac
     }
 
     private void initDefaultSites() {
-        String obj_sites = PreferenceManager.getDefaultSharedPreferences(getContext())
-                .getString(HomeFragment.TOPSITES_PREF, null);
+        // use different implementation to provide default top sites.
+        String obj_sites = Inject.getDefaultTopSites(getContext());
 
         //if no default sites data in SharedPreferences, load data from assets.
         if (obj_sites == null) {
