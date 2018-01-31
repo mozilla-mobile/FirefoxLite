e36f95a0831066c62a6f01fa0dc9d4c80677b166
Hao-Kuang Tsai <kuang.con@gmail.com>
2018-01-31 17:09:27 +0800

    add pre and post scripts

---
+++ NC_POST_BUILD_SCRIPT.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e # Exit (and fail) immediately if any command in this scriptfails
+
+# Check that our APK files are not bigger than they should be
+python tools/metrics/apk_size.py
+
+# skip CODECOV
+#bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
---
+++ NC_PRE_BUILD_SCRIPT.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e # Exit (and fail) immediately if any command in this scriptfails
+
+python tools/l10n/check_translations.py
