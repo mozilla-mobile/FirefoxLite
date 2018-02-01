b5f1eb0849c1c2a34893d9b6d6f5b66728813ebb
Nevin Chen <cnevinchen@gmail.com>
2018-02-01 17:26:29 +0800

    topsite: Add UI test for top site and page loanding
    Test case:
    1. Click the first top site
    2. Display the web page for it

--- app/src/androidTest/java/org/mozilla/focus/helper/TestUtil.java
+++ app/src/androidTest/java/org/mozilla/focus/helper/TestUtil.java
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with t
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.focus.test;
+package org.mozilla.focus.helper;
 
 import android.support.annotation.NonNull;
 import android.support.test.espresso.matcher.BoundedMatcher;
