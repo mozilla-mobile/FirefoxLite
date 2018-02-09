ae87ed04f7c88808967a51668f2a13a6c81b9a06
Teng-pao Yu <osimpleo@gmail.com>
2018-02-09 11:39:15 +0800

    Nevercode is fixing its bug, let's test via its console

--- NC_POST_BUILD_SCRIPT.sh
+++ NC_POST_BUILD_SCRIPT.sh
@@ -4,8 +4,5 @@ set -e # Exit (and fail) immediately if any command in this scriptfails
 # Check that our APK files are not bigger than they should be
 python tools/metrics/apk_size.py
 
-git reset --hard
-./gradlew app:test -x app:testFocusWebkitReleaseUnitTest
-
 # skip CODECOV
 #bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
