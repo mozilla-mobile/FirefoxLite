12213ebde9905bb8d384a755667cdc753c39266c
Teng-pao Yu <osimpleo@gmail.com>
2018-02-12 14:25:38 +0800

    Fix bug where captureBtn null check is incorrect

--- app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
+++ app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
@@ -405,7 +405,7 @@ public class BrowserFragment extends LocaleAwareFragment implements View.OnClick
         if (menuBtn != null) {
             menuBtn.setOnClickListener(this);
         }
-        if (menuBtn != null) {
+        if (captureBtn != null) {
             captureBtn.setOnClickListener(this);
         }
 
