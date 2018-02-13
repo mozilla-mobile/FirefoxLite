7971f8efd5365ac797aab06824d254ee7b62b21c
Benjamin Cheng <chengwc@gmail.com>
2018-02-13 12:05:33 +0800

    Update screenshot file path

--- tools/screenCapture.sh
+++ tools/screenCapture.sh
@@ -1,5 +1,5 @@
 # Unlock emulator
 # adb shell input keyevent 82
-mkdir -p app/build/outputs/apk/focusWebkit/debug/
-adb shell screencap -p > app/build/outputs/apk/focusWebkit/debug/bootscreen.png
+mkdir -p app/build/outputs/apk/focusWebkit/test/
+adb shell screencap -p > app/build/outputs/apk/focusWebkit/test/app-focus-webkit-test.apk
 # Continue running some more tests
