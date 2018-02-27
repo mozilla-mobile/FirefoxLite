1f32231cc63f6e362fcce9883e397ec114b9836b
Nevin Chen <cnevinchen@gmail.com>
2018-02-28 01:12:29 +0800

    firebase: Add messaging and push notification

--- app/src/main/java/org/mozilla/focus/activity/MainActivity.java
+++ app/src/main/java/org/mozilla/focus/activity/MainActivity.java
@@ -213,7 +213,6 @@ public class MainActivity extends LocaleAwareAppCompatActivity implements Fragme
     @Override
     protected void onPause() {
         super.onPause();
-
         LocalBroadcastManager.getInstance(this).unregisterReceiver(uiMessageReceiver);
 
         safeForFragmentTransactions = false;
@@ -246,6 +245,10 @@ public class MainActivity extends LocaleAwareAppCompatActivity implements Fragme
             // resumed. So just remember this URL and load it in onResumeFragments().
             pendingUrl = intent.getDataString();
             // We don't want to see any menu is visible when processing open url request from Intent.ACTION_VIEW
+            dismissAllMenus();
+            TabTray.dismiss(getSupportFragmentManager());
+        } else if (intent.getStringExtra(NotificationUtil.PUSH_OPEN_URL) != null) {
+            pendingUrl = intent.getStringExtra(NotificationUtil.PUSH_OPEN_URL);
             dismissAllMenus();
             TabTray.dismiss(getSupportFragmentManager());
         }
@@ -347,7 +350,7 @@ public class MainActivity extends LocaleAwareAppCompatActivity implements Fragme
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
-        NotificationCompat.Builder builder = NotificationUtil.generateNotificationBuilder(this, pendingIntent)
+        final NotificationCompat.Builder builder = NotificationUtil.generateNotificationBuilder(this, pendingIntent)
                 .setContentTitle(getString(R.string.survey_notification_title, "\uD83D\uDE4C"))
                 .setContentText(getString(R.string.survey_notification_description))
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(
