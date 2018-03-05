c25e7b7cf28ca9d034f906d3b321d89d3f1c37e2
Hao-Kuang Tsai <kuang.con@gmail.com>
2018-03-05 18:00:04 +0800

    query serial sample use build.gradle to control the versioning

---
+++ tools/build_scripts/query_serial.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import httplib2
+import os
+import argparse
+import time
+
+from googleapiclient import discovery
+from google.oauth2 import service_account
+
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SERVER_SECRET_FILE = '../bitrise-io.json'
+
+def main(args):
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVER_SECRET_FILE, scopes=SCOPES)
+    service = discovery.build('sheets', 'v4',
+                              credentials=credentials)
+
+    spreadsheetId = os.environ['SPREADSHEET_ID']
+    rangeName = 'Sample!A2:D'
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    elif args.c:
+        # consume a new serial number
+        try:
+            cur_max = max([x[1] for x in values if x[0] == args.v and x[3] == args.w])
+        except:
+            cur_max = 0
+        new_max = int(cur_max) + 1
+        value_body = {
+            "majorDimension": "ROWS",
+            "values": [[args.v, new_max, int(time.time()), args.w]]
+        }
+        response = service.spreadsheets().values().append(
+            spreadsheetId=spreadsheetId, range=rangeName, body=value_body, valueInputOption='USER_ENTERED').execute()
+        with open("build.serial", "w") as file:
+            file.write("%04d" % new_max)
+        print("%04d" % new_max)
+        print(response)
+    elif args.q:
+        # print current max
+        try:
+            cur_max = int(max([x[1] for x in values if x[0] == args.v and x[3] == args.w]))
+            with open("build.serial", "w") as file:
+                file.write("%04d" % cur_max)
+            print("%04d" % cur_max)
+        except:
+            print("No data found.")
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Choose parsers')
+    parser.add_argument('-q', action='store_true', default=False, help='Query max serial number')
+    parser.add_argument('-c', action='store_true', default=False, help='consume a new serial number')
+    parser.add_argument('-v', default='2.0.0', help='version constraint')
+    parser.add_argument('-w', default='nightly', help='workflow')
+    return parser
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    print(args)
+    main(args)
