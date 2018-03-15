0d12a852d1756c771fa405b80d54b30eb7275b6a
Nevin Chen <cnevinc@livemail.tw>
2018-03-15 17:10:59 +0800

    test: add TabTrayPresenterTest

---
+++ app/src/test/java/org/mozilla/focus/tabs/tabtray/TabTrayPresenterTest.kt
@@ -0,0 +1,47 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.tabs.tabtray
+
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+import org.mozilla.focus.activity.MainActivity
+import org.mozilla.focus.tabs.TabsSession
+
+class TabTrayPresenterTest {
+
+    private lateinit var tabTrayPresenter: TabTrayPresenter
+
+    @Mock
+    private val mainActivity: MainActivity? = null
+
+    @Mock
+    private val tabTrayContractView: TabTrayContract.View? = null
+
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        val tabsSession = TabsSession(mainActivity!!)
+        val tabsSessionModel = TabsSessionModel(tabsSession)
+
+        tabTrayPresenter = TabTrayPresenter(tabTrayContractView, tabsSessionModel)
+
+    }
+
+
+    @Test
+    fun viewReady_showFocusedTab() {
+        this.tabTrayPresenter.viewReady()
+
+        verify<TabTrayContract.View>(this.tabTrayContractView).showFocusedTab(anyInt())
+    }
+
+}
