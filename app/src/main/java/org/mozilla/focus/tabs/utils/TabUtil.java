8d26adcf3fc23687e0fa41bcfa882e869651450c
Julian_Chu <walkingice0204@gmail.com>
2018-03-16 14:33:35 +0800

    tabs: add util class for TabsSession arguments
    More necessary information should be pass to UI from Session, therefore
    in this commit involves Bundle as a container for arguements.
    
    The bundle will contains dispensable argument on adding tab.

---
+++ app/src/main/java/org/mozilla/focus/tabs/utils/TabUtil.java
@@ -0,0 +1,54 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.mozilla.focus.tabs.utils;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+public final class TabUtil {
+
+    static final String ARG_PARENT_ID = "_tab_parent_";
+    static final String ARG_EXTERNAL = "_tab_external_";
+    static final String ARG_FOCUS = "_tab_focus_";
+
+    private TabUtil() {
+    }
+
+    /**
+     * To create a bundle which contains dispensable arguments for manipulating tabs.
+     *
+     * @param parentId     id of parent tab
+     * @param fromExternal true if the tab is opened by external app
+     * @param toFocus      true if the tab should be focused after creation
+     * @return the @see{android.os.Bundle} contains arguments
+     */
+    public static Bundle argument(@Nullable final String parentId,
+                                  boolean fromExternal,
+                                  boolean toFocus) {
+        final Bundle bundle = new Bundle();
+        if (!TextUtils.isEmpty(parentId)) {
+            bundle.putString(ARG_PARENT_ID, parentId);
+        }
+
+        bundle.putBoolean(ARG_EXTERNAL, fromExternal);
+        bundle.putBoolean(ARG_FOCUS, toFocus);
+        return bundle;
+    }
+
+    @Nullable
+    public static String getParentId(@NonNull final Bundle bundle) {
+        return bundle.getString(ARG_PARENT_ID);
+    }
+
+    public static boolean isFromExternal(@NonNull final Bundle bundle) {
+        return bundle.getBoolean(ARG_EXTERNAL, false);
+    }
+
+    public static boolean toFocus(@NonNull final Bundle bundle) {
+        return bundle.getBoolean(ARG_FOCUS, false);
+    }
+}
---
+++ app/src/test/java/org/mozilla/focus/tabs/utils/TabUtilTest.java
@@ -0,0 +1,34 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.tabs.utils;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class TabUtilTest {
+
+    @Test
+    public void testCreation() {
+        Assert.assertNotNull(TabUtil.argument("parent_id", false, false));
+        Assert.assertNotNull(TabUtil.argument(null, false, false));
+        Assert.assertNotNull(TabUtil.argument(null, false, true));
+        Assert.assertNotNull(TabUtil.argument(null, true, false));
+    }
+
+    @Test
+    public void testReturnValue() {
+        Assert.assertNull(TabUtil.getParentId(TabUtil.argument(null, true, false)));
+        Assert.assertEquals("foobar", TabUtil.getParentId(TabUtil.argument("foobar", true, false)));
+        Assert.assertTrue(TabUtil.isFromExternal(TabUtil.argument(null, true, false)));
+        Assert.assertFalse(TabUtil.isFromExternal(TabUtil.argument(null, false, false)));
+        Assert.assertTrue(TabUtil.toFocus(TabUtil.argument(null, false, true)));
+        Assert.assertFalse(TabUtil.toFocus(TabUtil.argument(null, false, false)));
+    }
+}
