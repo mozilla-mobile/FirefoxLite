babb054ad66a36913d35c70ac75f7f9730529976
Nevin Chen <cnevinchen@gmail.com>
2018-03-16 09:44:34 +0800

    add2home: Update icons and add some fix for pin shortcut #56

--- app/src/main/java/org/mozilla/focus/activity/MainActivity.java
+++ app/src/main/java/org/mozilla/focus/activity/MainActivity.java
@@ -367,6 +367,10 @@ public class MainActivity extends LocaleAwareAppCompatActivity implements Fragme
         refreshIcon = menu.findViewById(R.id.action_refresh);
         stopIcon = menu.findViewById(R.id.action_stop);
         pinShortcut = menu.findViewById(R.id.action_pin_shortcut);
+        final boolean requestPinShortcutSupported = ShortcutManagerCompat.isRequestPinShortcutSupported(this);
+        if (!requestPinShortcutSupported) {
+            pinShortcut.setVisibility(View.GONE);
+        }
         menu.findViewById(R.id.menu_turbomode).setSelected(isTurboEnabled());
         menu.findViewById(R.id.menu_blockimg).setSelected(isBlockingImages());
     }
@@ -394,8 +398,7 @@ public class MainActivity extends LocaleAwareAppCompatActivity implements Fragme
         setEnable(nextButton, canGoForward);
         setLoadingButton(browserFragment);
         setEnable(shareButton, browserFragment != null);
-        final boolean requestPinShortcutSupported = ShortcutManagerCompat.isRequestPinShortcutSupported(this);
-        setEnable(pinShortcut, browserFragment != null && requestPinShortcutSupported);
+        setEnable(pinShortcut, browserFragment != null);
     }
 
     private void showTabTray() {
