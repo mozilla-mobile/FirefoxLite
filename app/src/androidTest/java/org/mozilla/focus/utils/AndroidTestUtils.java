12146afee74b16949b6090c81a140aecbbcdb946
Benjamin Cheng <chengwc@gmail.com>
2018-03-22 11:21:34 +0800

    Add open emtpy tab and open previous tabs UI tests

--- app/src/androidTest/java/org/mozilla/focus/utils/AndroidTestUtils.java
+++ app/src/androidTest/java/org/mozilla/focus/utils/AndroidTestUtils.java
@@ -12,6 +12,7 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 
 import org.mozilla.focus.BuildConfig;
+import org.mozilla.focus.Inject;
 import org.mozilla.focus.R;
 
 import java.io.IOException;
@@ -45,6 +46,9 @@ public final class AndroidTestUtils {
             settings.setShareAppDialogDidShow();
             settings.setRateAppDialogDidShow();
         }
+
+        Inject.getTabsDatabase(null).tabDao().deleteAllTabs();
+        setFocusTabId("");
     }
 
     public static Buffer readTestAsset(String filename) throws IOException {
@@ -67,5 +71,16 @@ public final class AndroidTestUtils {
 
     public static String getResourceId(String id) {
         return String.format("%s:id/%s", BuildConfig.APPLICATION_ID, id);
+    }
+
+    public static void setFocusTabId(String focusTabId) {
+        final Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (preferences != null) {
+            final SharedPreferences.Editor editor = preferences.edit();
+            if (editor != null) {
+                editor.putString("pref_key_focus_tab_id", focusTabId).commit();
+            }
+        }
     }
 }
