6d395661c6054ac9c3bad72d3deaa79ee7370b19
Joseph Yu <joseph.yu@yarboroughtech.com>
2018-03-26 17:04:27 +0800

    Implement grant storage permission test. - permission_006

--- app/src/androidTest/java/org/mozilla/focus/utils/AndroidTestUtils.java
+++ app/src/androidTest/java/org/mozilla/focus/utils/AndroidTestUtils.java
@@ -9,12 +9,21 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.CoordinatesProvider;
+import android.support.test.espresso.action.GeneralClickAction;
+import android.support.test.espresso.action.Press;
+import android.support.test.espresso.action.Tap;
 import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
 
 import org.mozilla.focus.BuildConfig;
 import org.mozilla.focus.Inject;
 import org.mozilla.focus.R;
+import org.mozilla.focus.activity.MainActivity;
 import org.mozilla.focus.fragment.FirstrunFragment;
+import org.mozilla.focus.widget.FragmentListener;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -90,4 +99,36 @@ public final class AndroidTestUtils {
             }
         }
     }
+
+    public static ViewAction clickXY(final int x, final int y, final Tap tap) {
+        return new GeneralClickAction(
+                tap,
+                new CoordinatesProvider() {
+                    @Override
+                    public float[] calculateCoordinates(View view) {
+
+                        final int[] screenPos = new int[2];
+                        view.getLocationOnScreen(screenPos);
+
+                        final float screenX = screenPos[0] + x;
+                        final float screenY = screenPos[1] + y;
+                        float[] coordinates = {screenX, screenY};
+
+                        return coordinates;
+                    }
+                }, Press.FINGER, 0, 0, null);
+    }
+
+    public static void showHomeMenu(ActivityTestRule<MainActivity> activityTestRule) {
+        if (activityTestRule != null) {
+            final MainActivity mainActivity = activityTestRule.getActivity();
+            mainActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mainActivity.onNotified(null, FragmentListener.TYPE.SHOW_MENU, null);
+                }
+            });
+        }
+    }
+
 }
