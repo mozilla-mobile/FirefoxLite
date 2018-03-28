a3dd9a4a44105a3a8d88d28aabccda150ebdb59f
Joseph Yu <joseph.yu@yarboroughtech.com>
2018-03-28 13:15:25 +0800

    Replace UiAutomator with Espresso in suggestion test.

---
+++ app/src/androidTest/java/org/mozilla/focus/helper/GetTextViewMatcher.java
@@ -0,0 +1,36 @@
+package org.mozilla.focus.helper;
+
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.view.View;
+import android.widget.TextView;
+
+import org.hamcrest.Matcher;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+
+public class GetTextViewMatcher {
+
+    public static String getText(final Matcher<View> matcher) {
+        final String stringHolder[] = { null };
+        onView(matcher).perform(new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isAssignableFrom(TextView.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "getting text from a TextView";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                TextView tv = (TextView) view;
+                stringHolder[0] = tv.getText().toString();
+            }
+        });
+        return stringHolder[0];
+    }
+}
