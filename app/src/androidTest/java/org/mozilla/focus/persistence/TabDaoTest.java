2382147b7f8ce6d9a475703245af42a30a64204f
Benjamin Cheng <chengwc@gmail.com>
2018-03-30 11:31:02 +0800

    Add tabs DB migration test from version 1 to 2

--- app/src/androidTest/java/org/mozilla/focus/persistence/TabDaoTest.java
+++ app/src/androidTest/java/org/mozilla/focus/persistence/TabDaoTest.java
@@ -1,20 +1,38 @@
 package org.mozilla.focus.persistence;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.db.framework.FrameworkSQLiteOpenHelperFactory;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.testing.MigrationTestHelper;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.test.InstrumentationRegistry;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.mozilla.focus.Inject;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class TabDaoTest {
 
+    private static final String TEST_DB_NAME = "migration-test";
     private static final TabModel TAB = new TabModel("TEST_ID", "ID_HOME", "Yahoo TW", "https://tw.yahoo.com");
     private static final TabModel TAB_2 = new TabModel("TEST_ID_2", TAB.getId(), "Google", "https://www.google.com");
 
     private TabsDatabase tabsDatabase;
+
+    @Rule
+    public MigrationTestHelper testHelper =
+            new MigrationTestHelper(
+                    InstrumentationRegistry.getInstrumentation(),
+                    TabsDatabase.class.getCanonicalName(),
+                    new FrameworkSQLiteOpenHelperFactory());
 
     @Before
     public void initDb() throws Exception {
@@ -89,10 +107,54 @@ public class TabDaoTest {
         assertEquals(0, dbTabs.size());
     }
 
+    @Test
+    public void migrationFrom1To2_containsCorrectData() throws IOException {
+        // Create the database in version 1
+        SupportSQLiteDatabase db = testHelper.createDatabase(TEST_DB_NAME, 1);
+        // Insert some data
+        insertTabV1(TAB, db);
+        insertTabV1(TAB_2, db);
+        db.close();
+
+        // Re-open the database with version 2 and provide MIGRATION_1_2 as the migration process.
+        testHelper.runMigrationsAndValidate(TEST_DB_NAME, 2, true, TabsDatabase.MIGRATION_1_2);
+
+        // MigrationTestHelper automatically verifies the schema changes, but not the data validity
+        // Validate that the data was migrated properly.
+        List<TabModel> dbTabs = getMigratedRoomDatabase().tabDao().getTabs();
+        assertEquals(2, dbTabs.size());
+        assertTabEquals(TAB, dbTabs.get(0));
+        assertTabEquals(TAB_2, dbTabs.get(1));
+    }
+
+    private void insertTabV1(TabModel tabModel, SupportSQLiteDatabase db) {
+        ContentValues values = new ContentValues();
+        values.put("tab_id", tabModel.getId());
+        values.put("tab_parent_id", tabModel.getParentId());
+        values.put("tab_title", tabModel.getTitle());
+        values.put("tab_url", tabModel.getUrl());
+        values.put("tab_thumbnail_uri", "");
+        values.put("webview_state_uri", "");
+        db.insert("tabs", SQLiteDatabase.CONFLICT_REPLACE, values);
+    }
+
     private void assertTabEquals(TabModel expectedTab, TabModel actualTab) {
         assertEquals(expectedTab.getId(), actualTab.getId());
         assertEquals(expectedTab.getParentId(), actualTab.getParentId());
         assertEquals(expectedTab.getTitle(), actualTab.getTitle());
         assertEquals(expectedTab.getUrl(), actualTab.getUrl());
+    }
+
+    private TabsDatabase getMigratedRoomDatabase() {
+        TabsDatabase database = Room.databaseBuilder(
+                InstrumentationRegistry.getTargetContext(),
+                TabsDatabase.class,
+                TEST_DB_NAME)
+                .fallbackToDestructiveMigration()
+                .addMigrations(TabsDatabase.MIGRATION_1_2)
+                .build();
+
+        testHelper.closeWhenFinished(database);
+        return database;
     }
 }
