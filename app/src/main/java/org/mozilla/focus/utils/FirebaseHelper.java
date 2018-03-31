4e72ec7cbffefd1e0f289c765aaf24e577e6f7c1
Nevin Chen <cnevinc@livemail.tw>
2018-03-31 11:01:54 +0800

    firebase: Inject getRemoteConfigDefault() implementation
    Fix #1725

--- app/src/main/java/org/mozilla/focus/utils/FirebaseHelper.java
+++ app/src/main/java/org/mozilla/focus/utils/FirebaseHelper.java
@@ -15,7 +15,6 @@ import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 import org.mozilla.focus.BuildConfig;
-import org.mozilla.focus.R;
 
 import java.lang.ref.WeakReference;
 import java.util.HashMap;
@@ -23,16 +22,18 @@ import java.util.HashMap;
 /**
  * Provide helper for Firebase functionality
  */
-public class FirebaseHelper {
+final public class FirebaseHelper {
 
-    public static final String RATE_APP_DIALOG_TEXT_TITLE = "rate_app_dialog_text_title";
-    public static final String RATE_APP_DIALOG_TEXT_CONTENT = "rate_app_dialog_text_content";
+    static final String RATE_APP_DIALOG_TEXT_TITLE = "rate_app_dialog_text_title";
+    static final String RATE_APP_DIALOG_TEXT_CONTENT = "rate_app_dialog_text_content";
+
     private static final String TAG = "FirebaseHelper";
 
     private static WeakReference<FirebaseRemoteConfig> REMOTE_CONFIG;
     private static long CACHE_EXPIRATION = 3600; // 1 hour in seconds.
 
-    public static String getString(String key) {
+    // get Remote Config string
+    static String getRcString(String key) {
         final FirebaseRemoteConfig config = REMOTE_CONFIG.get();
         if (config != null) {
             return config.getString(key);
@@ -41,6 +42,7 @@ public class FirebaseHelper {
     }
 
     public static void init(final Context context) {
+        // internalInit() require I/O so I put it in background thread.
         new Thread(new Runnable() {
             public void run() {
                 internalInit(context);
@@ -50,6 +52,7 @@ public class FirebaseHelper {
 
     private static void internalInit(final Context context) {
 
+        // Init remote config
         final FirebaseRemoteConfig config = FirebaseRemoteConfig.getInstance();
         REMOTE_CONFIG = new WeakReference<>(config);
         final FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
@@ -63,11 +66,11 @@ public class FirebaseHelper {
         if (config.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
             CACHE_EXPIRATION = 0;
         }
-        refresh();
+        refreshRemoteConfig();
     }
 
     // Call this method to refresh the value in remote config
-    public static void refresh() {
+    private static void refreshRemoteConfig() {
         final FirebaseRemoteConfig config = REMOTE_CONFIG.get();
         if (config == null) {
             return;
@@ -86,11 +89,8 @@ public class FirebaseHelper {
         });
     }
 
-    public static HashMap<String, Object> getRemoteConfigDefault(Context context) {
+    private static HashMap<String, Object> getRemoteConfigDefault(Context context) {
 
-        final HashMap<String, Object> map = new HashMap<>();
-        map.put(RATE_APP_DIALOG_TEXT_TITLE, context.getString(R.string.rate_app_dialog_text_title));
-        map.put(RATE_APP_DIALOG_TEXT_CONTENT, context.getString(R.string.rate_app_dialog_text_content));
-        return map;
+        return FirebaseHelperInject.getRemoteConfigDefault(context);
     }
 }
