92cd4792e26f404dbfc535a94481ffb2b62ab811
Roger <pharspher@gmail.com>
2018-04-12 11:34:56 +0800

    Add test to TabTrayPresenterTest

--- app/src/test/java/org/mozilla/focus/tabs/tabtray/TabTrayPresenterTest.kt
+++ app/src/test/java/org/mozilla/focus/tabs/tabtray/TabTrayPresenterTest.kt
@@ -5,15 +5,24 @@
 
 package org.mozilla.focus.tabs.tabtray
 
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.os.Message
+import android.view.View
+import junit.framework.Assert
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.*
+import org.mockito.ArgumentMatchers.any
 import org.mockito.ArgumentMatchers.anyInt
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.Mockito.verify
-import org.mockito.MockitoAnnotations
-import org.mozilla.focus.tabs.Tab
+import org.mockito.Mockito.*
+import org.mozilla.focus.tabs.*
+import org.mozilla.focus.tabs.utils.TabUtil
+import org.mozilla.focus.web.DownloadCallback
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class TabTrayPresenterTest {
 
     private lateinit var tabTrayPresenter: TabTrayPresenter
@@ -23,6 +32,9 @@ class TabTrayPresenterTest {
 
     @Mock
     private lateinit var tabsSessionModel: TabsSessionModel
+
+    @Captor
+    private lateinit var tabListCaptor: ArgumentCaptor<List<Tab>>
 
     @Before
     fun setUp() {
@@ -40,5 +52,156 @@ class TabTrayPresenterTest {
         Mockito.`when`(tabsSessionModel.tabs).thenReturn(listOf(Tab(), Tab(), Tab()))
         this.tabTrayPresenter.viewReady()
         verify<TabTrayContract.View>(this.tabTrayContractView).showFocusedTab(anyInt())
+    }
+
+    @Test
+    fun viewReady_tabCountChanged_viewRefreshDataCalled() {
+        val session = TabsSession(DefaultTabViewProvider())
+
+        // Prepare some tabs
+        session.addTab("url", TabUtil.argument(null, false, true))
+        session.addTab("url", TabUtil.argument(null, false, true))
+        session.addTab("url", TabUtil.argument(null, false, true))
+        Assert.assertEquals(3, session.tabs.size)
+
+        val presenter = TabTrayPresenter(tabTrayContractView, TabsSessionModel(session))
+
+        // view is not ready yet, add tab should not trigger refreshData
+        verify<TabTrayContract.View>(this.tabTrayContractView, never()).refreshData(tabListCaptor.capture(), any())
+
+        // OK we are ready
+        presenter.viewReady()
+
+        // Tab count changed
+        session.addTab("url", TabUtil.argument(null, false, true))
+        Assert.assertEquals(4, session.tabs.size)
+
+        // Assert refresh data is called, with new tab list of size=4
+        verify<TabTrayContract.View>(this.tabTrayContractView).refreshData(tabListCaptor.capture(), any())
+        Assert.assertEquals(4, tabListCaptor.value.size)
+    }
+
+    private class DefaultTabViewProvider : TabViewProvider {
+
+        override fun create(): TabView {
+            return DefaultTabView()
+        }
+    }
+
+    private class DefaultTabView : TabView {
+
+        private var url: String? = null
+
+        override fun setContentBlockingEnabled(enabled: Boolean) {
+
+        }
+
+        override fun setImageBlockingEnabled(enabled: Boolean) {
+
+        }
+
+        override fun isBlockingEnabled(): Boolean {
+            return false
+        }
+
+        override fun performExitFullScreen() {
+
+        }
+
+        override fun setViewClient(viewClient: TabViewClient?) {
+
+        }
+
+        override fun setChromeClient(chromeClient: TabChromeClient?) {
+
+        }
+
+        override fun setDownloadCallback(callback: DownloadCallback?) {
+
+        }
+
+        override fun onPause() {
+
+        }
+
+        override fun onResume() {
+
+        }
+
+        override fun destroy() {
+
+        }
+
+        override fun reload() {
+
+        }
+
+        override fun stopLoading() {
+
+        }
+
+        override fun getUrl(): String? {
+            return this.url
+        }
+
+        override fun getTitle(): String? {
+            return null
+        }
+
+        @SiteIdentity.SecurityState
+        override fun getSecurityState(): Int {
+            return SiteIdentity.UNKNOWN
+        }
+
+        override fun loadUrl(url: String) {
+            this.url = url
+        }
+
+        override fun cleanup() {
+
+        }
+
+        override fun goForward() {
+
+        }
+
+        override fun goBack() {
+
+        }
+
+        override fun canGoForward(): Boolean {
+            return false
+        }
+
+        override fun canGoBack(): Boolean {
+            return false
+        }
+
+        override fun restoreViewState(inState: Bundle) {
+
+        }
+
+        override fun saveViewState(outState: Bundle) {
+
+        }
+
+        override fun insertBrowsingHistory() {
+
+        }
+
+        override fun getView(): View? {
+            return null
+        }
+
+        override fun buildDrawingCache(autoScale: Boolean) {
+
+        }
+
+        override fun getDrawingCache(autoScale: Boolean): Bitmap? {
+            return null
+        }
+
+        override fun bindOnNewWindowCreation(msg: Message) {
+        }
     }
 }
