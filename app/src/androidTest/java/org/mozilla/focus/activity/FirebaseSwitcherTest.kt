2f8b40f2329719c3fcbe8dbc7765b76bff2b4c8f
Nevin Chen <cnevinc@livemail.tw>
2018-05-29 00:25:23 +0800

    Closes #1985: Remove unused and not stable tests

--- app/src/androidTest/java/org/mozilla/focus/activity/FirebaseSwitcherTest.kt
+++ app/src/androidTest/java/org/mozilla/focus/activity/FirebaseSwitcherTest.kt
@@ -24,7 +24,6 @@ class FirebaseSwitcherTest {
     @Test
     fun disableFirebase_makeSureSwitchIsOffAfterClick() {
 
-        // SettingRobot().apply {
         setting {
             // check initial state
             prefSendUsageData()
@@ -36,43 +35,6 @@ class FirebaseSwitcherTest {
         }
     }
 
-//
-//    /**
-//     *  This is an integration test (UI test on real component) so no need to use robot
-//     *  It calls bind() multiple times to see if any runnable is created.
-//     *   no actual click was performed. This test case here is not really a UI test.
-//     */
-//    @Test
-//    fun callBindCrazily_OnlyOneRunnableIsCreated() {
-//
-//        val context = InstrumentationRegistry.getInstrumentation().targetContext
-//
-//        // I've added some latency to the enabler, in case it runs too fast
-//        FirebaseHelper.injectEnablerCallback(SettingRobot.Delay())
-//
-//        val enable = TelemetryWrapper.isTelemetryEnabled(context)
-//        // The state is not changed, but we still want keep the same status and call bind again to kick off the enabler.
-//        // I use bind() to simulate the click. This is because calling click can't let get the return value
-//        // of the bind method ( I use it to determine if a new Runnable is created)
-//        var newRunnableCreated = FirebaseHelper.bind(context, enable)
-//        // Only this time will be true
-//        // first time, should get true from bind
-//        assertTrue(newRunnableCreated)
-//
-//        // the successors should return false( not create new runnable)
-//        // assume below three method calls happen very fast
-//        newRunnableCreated = FirebaseHelper.bind(context, enable)
-//        assertFalse(newRunnableCreated)
-//
-//        newRunnableCreated = FirebaseHelper.bind(context, enable)
-//        // second time, should also get false
-//        assertFalse(newRunnableCreated)
-//
-//        newRunnableCreated = FirebaseHelper.bind(context, enable)
-//        // third time, should still get false
-//        assertFalse(newRunnableCreated)
-//
-//    }
 
     @Test
     fun flipPrefCrazily_TheStateIsSynced() {
@@ -92,22 +54,5 @@ class FirebaseSwitcherTest {
         }
     }
 
-    @Test
-    fun flipAndLeave_ShouldHaveNoLeak() {
-
-        // SettingRobot().apply {
-        setting {
-            // check initial state
-            prefSendUsageData()
-            isChecked()
-
-            // now flip crazily and check for leak
-            click() // on -> off
-            click() // off -> on
-            click() // on -> off
-            click() // off -> on
-            restartAndcheckNoLeak()
-        }
-    }
 
 }
