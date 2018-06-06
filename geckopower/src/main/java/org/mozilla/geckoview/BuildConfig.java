1f2c16ed1550d7dc21a9c3c196d5850392266389
Julian_Chu <walkingice0204@gmail.com>
2018-06-07 01:27:04 +0800

    HACK: add generated BuildConfig to build GeckoView
    copy from my own tree:
    
    generated/source/buildConfig/localWithGeckoBinariesMinApi21/debug/org/mozilla/geckoview/BuildConfig.java

---
+++ geckopower/src/main/java/org/mozilla/geckoview/BuildConfig.java
@@ -0,0 +1,39 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.mozilla.geckoview;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.mozilla.geckoview";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "localWithGeckoBinariesMinApi21";
+  public static final int VERSION_CODE = 6200000;
+  public static final String VERSION_NAME = "62.0a1-default";
+  public static final String FLAVOR_audience = "local";
+  public static final String FLAVOR_geckoBinaries = "withGeckoBinaries";
+  public static final String FLAVOR_minApi = "minApi21";
+  // Fields from default config.
+  public static final String ANDROID_CPU_ARCH = "armeabi-v7a";
+  public static final boolean DEBUG_BUILD = false;
+  public static final String GRE_MILESTONE = "62.0a1";
+  public static final int MIN_SDK_VERSION = 16;
+  public static final boolean MOZILLA_OFFICIAL = false;
+  public static final String MOZILLA_VERSION = "62.0a1";
+  public static final String MOZ_APP_ABI = "arm-eabi-gcc3";
+  public static final String MOZ_APP_BASENAME = "Fennec";
+  public static final String MOZ_APP_BUILDID = "20180529190249";
+  public static final String MOZ_APP_DISPLAYNAME = "Fennec jchu";
+  public static final String MOZ_APP_ID = "{aa3c5121-dab2-40e2-81ca-7ea25febc110}";
+  public static final String MOZ_APP_NAME = "fennec";
+  public static final String MOZ_APP_UA_NAME = "Firefox";
+  public static final String MOZ_APP_VENDOR = "Mozilla";
+  public static final String MOZ_APP_VERSION = "62.0a1";
+  public static final boolean MOZ_CRASHREPORTER = false;
+  public static final String MOZ_UPDATE_CHANNEL = "default";
+  public static final boolean NIGHTLY_BUILD = true;
+  public static final String OMNIJAR_NAME = "assets/omni.ja";
+  public static final boolean RELEASE_OR_BETA = false;
+  public static final String USER_AGENT_GECKOVIEW_MOBILE = "Mozilla/5.0 (Android " + android.os.Build.VERSION.RELEASE + "; Mobile; rv: 62.0a1) Gecko/62.0a1 GeckoView/62.0a1";
+  public static final String USER_AGENT_GECKOVIEW_TABLET = "Mozilla/5.0 (Android " + android.os.Build.VERSION.RELEASE + "; Tablet; rv: 62.0a1) Gecko/62.0a1 GeckoView/62.0a1";
+}
