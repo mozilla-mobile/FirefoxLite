eeb3977eeb18a4e0b89f05a635768ccf7be98202
Teng-pao Yu <osimpleo@gmail.com>
2018-06-21 19:39:44 +0800

    Create BookmarksFragment which now only handles empty case

---
+++ app/src/main/java/org/mozilla/focus/fragment/BookmarksFragment.java
@@ -0,0 +1,34 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.mozilla.focus.R;
+
+import javax.annotation.Nonnull;
+
+
+public class BookmarksFragment extends PanelFragment {
+
+    public static BookmarksFragment newInstance() {
+        return new BookmarksFragment();
+    }
+
+    @Override
+    public View onCreateView(@Nonnull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bookmarks, container, false);
+    }
+
+    @Override
+    public void tryLoadMore() {
+
+    }
+}
