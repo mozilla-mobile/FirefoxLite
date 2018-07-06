39b5269df1cf8153d07a2a11eaf9e38f1dd9529d
Joseph Yu <joseph.yu@yarboroughtech.com>
2018-07-06 11:32:38 +0800

    Implement bookmarks test. - 0203001, 0203002, 0203003

--- app/src/androidTest/java/org/mozilla/focus/helper/BeforeTestTask.java
+++ app/src/androidTest/java/org/mozilla/focus/helper/BeforeTestTask.java
@@ -10,6 +10,7 @@ import android.support.test.InstrumentationRegistry;
 
 import org.mozilla.focus.Inject;
 import org.mozilla.focus.history.BrowsingHistoryManager;
+import org.mozilla.focus.persistence.BookmarksDatabase;
 import org.mozilla.focus.utils.AndroidTestUtils;
 import org.mozilla.focus.utils.NewFeatureNotice;
 import org.mozilla.focus.utils.Settings;
@@ -51,6 +52,7 @@ public class BeforeTestTask {
         }
 
         Inject.getTabsDatabase(null).tabDao().deleteAllTabs();
+        BookmarksDatabase.getInstance(context).bookmarkDao().deleteAllBookmarks();
         AndroidTestUtils.setFocusTabId("");
         // Disable theme on boarding
         ThemeManager.dismissOnboarding(context);
