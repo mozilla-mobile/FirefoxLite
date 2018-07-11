6ca3f355b129a1d7cded9c7ad0ed6a172c3eb8cb
Julian_Chu <walkingice0204@gmail.com>
2018-07-11 17:21:06 +0800

    model: move TabModelStore to package persistence
    This class is more like a helper from persistence package to help other
    classes on storing/restoring TabModel.

--- app/src/main/java/org/mozilla/focus/persistence/TabModelStore.java
+++ app/src/main/java/org/mozilla/focus/persistence/TabModelStore.java
@@ -1,4 +1,4 @@
-package org.mozilla.focus.tabs;
+package org.mozilla.focus.persistence;
 
 import android.content.Context;
 import android.os.AsyncTask;
@@ -8,8 +8,6 @@ import android.support.annotation.Nullable;
 
 import org.mozilla.focus.Inject;
 import org.mozilla.focus.R;
-import org.mozilla.focus.persistence.TabEntity;
-import org.mozilla.focus.persistence.TabsDatabase;
 import org.mozilla.focus.utils.FileUtils;
 
 import java.io.File;
