9b0c5a0d138fb63e15fd6614144ef76076a21bb7
Julian_Chu <walkingice0204@gmail.com>
2018-07-16 17:16:50 +0800

    privately: add class PrivateTabViewProvider
    This is the provider to create TabView for private-mode.

---
+++ app/src/main/java/org/mozilla/rocket/privately/PrivateTabViewProvider.kt
@@ -0,0 +1,24 @@
+package org.mozilla.rocket.privately
+
+import android.app.Activity
+import android.webkit.WebSettings
+import org.mozilla.focus.web.WebViewProvider
+import org.mozilla.rocket.tabs.TabView
+import org.mozilla.rocket.tabs.TabViewProvider
+
+class PrivateTabViewProvider(private val host: Activity) : TabViewProvider {
+
+    override fun create(): TabView {
+        return WebViewProvider.create(host, null, WebViewSettingsHook) as TabView
+    }
+
+    object WebViewSettingsHook : WebViewProvider.WebSettingsHook {
+        override fun modify(settings: WebSettings?) {
+            if (settings == null) {
+                return
+            }
+
+            settings.setSupportMultipleWindows(false);
+        }
+    }
+}
