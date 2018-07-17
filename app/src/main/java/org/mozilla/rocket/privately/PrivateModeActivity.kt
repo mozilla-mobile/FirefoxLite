5f86e4f3ea61b703105a080c894e707c17909dcf
Julian_Chu <walkingice0204@gmail.com>
2018-07-17 15:21:04 +0800

    ui: PrivateModeActivity has basic ui operation
    apply navigation to PrivateModeActivity, and let it handle message to
    open/hide url-input-fragment.

--- app/src/main/java/org/mozilla/rocket/privately/PrivateModeActivity.kt
+++ app/src/main/java/org/mozilla/rocket/privately/PrivateModeActivity.kt
@@ -9,11 +9,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.View
+import androidx.navigation.Navigation
 import org.mozilla.focus.R
 import org.mozilla.focus.activity.MainActivity
 import org.mozilla.focus.locale.LocaleAwareAppCompatActivity
+import org.mozilla.focus.urlinput.UrlInputFragment
 import org.mozilla.focus.widget.FragmentListener
-import org.mozilla.rocket.privately.home.PrivateHomeFragment
+import org.mozilla.focus.widget.FragmentListener.TYPE
+import org.mozilla.rocket.privately.browse.BrowserFragment
 
 class PrivateModeActivity : LocaleAwareAppCompatActivity(), FragmentListener {
 
@@ -25,34 +28,37 @@ class PrivateModeActivity : LocaleAwareAppCompatActivity(), FragmentListener {
         // do not overwrite existing value
         visibility = visibility or (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
         window.decorView.systemUiVisibility = visibility
-
-        showHomeScreen()
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        pushToBack()
-    }
+    @Override
+    override fun onSupportNavigateUp() = getNavController().navigateUp()
 
     override fun applyLocale() {}
 
     override fun onNotified(from: Fragment, type: FragmentListener.TYPE, payload: Any?) {
         when (type) {
-            FragmentListener.TYPE.TOGGLE_PRIVATE_MODE -> pushToBack()
+            TYPE.TOGGLE_PRIVATE_MODE -> pushToBack()
+            TYPE.SHOW_URL_INPUT -> showUrlInput(payload)
+            TYPE.DISMISS_URL_INPUT -> dismissUrlInput()
+            TYPE.OPEN_URL_IN_CURRENT_TAB -> openUrl(payload)
+            TYPE.OPEN_URL_IN_NEW_TAB -> openUrl(payload)
             else -> {
             }
         }
     }
 
-    private fun showHomeScreen() {
-        val fragment = this.createHomeFragment()
-        this.supportFragmentManager.beginTransaction()
-                .add(R.id.container, fragment, PrivateHomeFragment.FRAGMENT_TAG)
-                .commit()
-    }
-
-    private fun createHomeFragment(): PrivateHomeFragment {
-        return PrivateHomeFragment.create()
+    override fun onBackPressed() {
+        val cnt = supportFragmentManager.backStackEntryCount
+        if (cnt != 0) {
+            supportFragmentManager.popBackStack()
+        } else {
+            val controller = getNavController()
+            if (controller.currentDestination.id == R.id.fragment_private_home_screen) {
+                super.onBackPressed()
+            } else {
+                getNavController().navigateUp()
+            }
+        }
     }
 
     private fun pushToBack() {
@@ -61,4 +67,46 @@ class PrivateModeActivity : LocaleAwareAppCompatActivity(), FragmentListener {
         startActivity(intent)
         overridePendingTransition(0, R.anim.pb_exit)
     }
+
+    private fun showUrlInput(payload: Any?) {
+        val url = payload?.toString() ?: ""
+
+        var frgMgr = supportFragmentManager
+
+        if (isUrlInputDisplaying()) {
+            // We are already showing an URL input fragment. This might have been a double click on the
+            // fake URL bar. Just ignore it.
+            return
+        }
+        val urlFragment: UrlInputFragment = UrlInputFragment.create(url, null)
+        val transaction = frgMgr.beginTransaction()
+        transaction.add(R.id.private_nav_host_fragment, urlFragment, UrlInputFragment.FRAGMENT_TAG)
+                .addToBackStack(UrlInputFragment.FRAGMENT_TAG)
+                .commit()
+    }
+
+    private fun dismissUrlInput() {
+        if (isUrlInputDisplaying()) {
+            supportFragmentManager.popBackStack()
+        }
+    }
+
+    private fun openUrl(payload: Any?) {
+        val url = payload?.toString() ?: ""
+        dismissUrlInput()
+        val controller = getNavController()
+        if (controller.currentDestination.id == R.id.fragment_private_home_screen) {
+            controller.navigate(R.id.action_private_home_to_browser,
+                    BrowserFragment.createArgument(url))
+        } else {
+            // TODO: notify BrowserFragment
+        }
+    }
+
+    private fun isUrlInputDisplaying(): Boolean {
+        val frg = supportFragmentManager.findFragmentByTag(UrlInputFragment.FRAGMENT_TAG)
+        return ((frg != null) && frg.isAdded && !frg.isRemoving)
+    }
+
+    private fun getNavController() = Navigation.findNavController(this, R.id.private_nav_host_fragment)
 }
