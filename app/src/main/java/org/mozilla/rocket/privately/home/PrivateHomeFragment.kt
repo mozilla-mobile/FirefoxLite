208c4837bcee8536e1609634c507c358475ed189
Julian_Chu <walkingice0204@gmail.com>
2018-07-18 11:52:41 +0800

    ui: let PrivateHomeFragemnt could open UrlInputFragment

--- app/src/main/java/org/mozilla/rocket/privately/home/PrivateHomeFragment.kt
+++ app/src/main/java/org/mozilla/rocket/privately/home/PrivateHomeFragment.kt
@@ -13,13 +13,15 @@ import android.view.animation.TranslateAnimation
 import org.mozilla.focus.R
 import org.mozilla.focus.locale.LocaleAwareFragment
 import org.mozilla.focus.widget.FragmentListener
+import org.mozilla.focus.widget.FragmentListener.TYPE.SHOW_URL_INPUT
 import org.mozilla.focus.widget.FragmentListener.TYPE.TOGGLE_PRIVATE_MODE
 import org.mozilla.rocket.widget.BetterBounceInterpolator
 
 class PrivateHomeFragment : LocaleAwareFragment() {
 
-    lateinit var btnBack: View
-    lateinit var logoMan: View;
+    private lateinit var btnBack: View
+    private lateinit var logoMan: View
+    private lateinit var fakeInput: View
 
     @Override
     override fun onCreate(bundle: Bundle?) {
@@ -31,12 +33,22 @@ class PrivateHomeFragment : LocaleAwareFragment() {
         val view = inflater.inflate(R.layout.fragment_private_homescreen, container, false)
         btnBack = view.findViewById(R.id.btn_tab_tray)
         logoMan = view.findViewById(R.id.logo_man)
+        fakeInput = view.findViewById(R.id.home_fragment_fake_input)
 
-        btnBack.setOnClickListener { v ->
-            // TODO: we should use Navigator
-            val listener = activity as FragmentListener
-            listener.onNotified(PrivateHomeFragment@ this, TOGGLE_PRIVATE_MODE, null)
+        btnBack.setOnClickListener {
+            var parent = activity
+            if (parent is FragmentListener) {
+                parent.onNotified(PrivateHomeFragment@ this, TOGGLE_PRIVATE_MODE, null)
+            }
         }
+
+        fakeInput.setOnClickListener {
+            var parent = activity
+            if (parent is FragmentListener) {
+                parent.onNotified(PrivateHomeFragment@ this, SHOW_URL_INPUT, null)
+            }
+        }
+
         return view
     }
 
