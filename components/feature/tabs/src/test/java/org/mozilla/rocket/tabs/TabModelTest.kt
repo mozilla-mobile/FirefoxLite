2eb3a3e659bcb183bba4a4f52111c8581a963c2c
Julian_Chu <walkingice0204@gmail.com>
2018-07-21 14:20:30 +0800

    tabs: rewrite method TabModel.isValid
    if url is null, old implementation will trigger kotlin exception.
    
    to resolve #2189

---
+++ components/feature/tabs/src/test/java/org/mozilla/rocket/tabs/TabModelTest.kt
@@ -0,0 +1,53 @@
+package org.mozilla.rocket.tabs
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.robolectric.annotation.Config
+import java.util.UUID
+
+@Config(manifest = Config.NONE)
+class TabModelTest {
+
+    @Test
+    fun testIsValid() {
+        TabModel(UUID.randomUUID().toString(),
+                "parent_id",
+                "title",
+                "https://mozilla.org").let { model ->
+            assertEquals(true, model.isValid())
+        }
+
+        TabModel(UUID.randomUUID().toString(),
+                "",
+                "",
+                "https://mozilla.org").let { model ->
+            assertEquals(true, model.isValid())
+        }
+
+        TabModel(UUID.randomUUID().toString(),
+                null,
+                null,
+                "https://mozilla.org").let { model ->
+            assertEquals(true, model.isValid())
+        }
+
+        // no id, invalid
+        TabModel("", "", "", "https://mozilla.org").let { model ->
+            assertEquals(false, model.isValid())
+        }
+
+        TabModel("", "", "", "").let { model ->
+            assertEquals(false, model.isValid())
+        }
+
+        // no url, invalid
+        TabModel(UUID.randomUUID().toString(), "", "", "").let { model ->
+            assertEquals(false, model.isValid())
+        }
+
+        TabModel(UUID.randomUUID().toString(), "", "", null).let { model ->
+            assertEquals(false, model.isValid())
+        }
+
+    }
+}
