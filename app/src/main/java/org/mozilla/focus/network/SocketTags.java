33030449fc370298502a1e48afc002eebe3970ce
Teng-pao Yu <osimpleo@gmail.com>
2018-08-01 19:01:06 +0800

    Load banner configuration from Firebase

--- app/src/main/java/org/mozilla/focus/network/SocketTags.java
+++ app/src/main/java/org/mozilla/focus/network/SocketTags.java
@@ -3,4 +3,5 @@ package org.mozilla.focus.network;
 public class SocketTags {
     public static final int SEARCH_SUGGESTION = 10000;
     public static final int DOWNLOADS = 10001;
+    public static final int BANNER = 10002;
 }
