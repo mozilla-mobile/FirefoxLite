1e583909dd1a46ce8629420039e64b8e2f318fb5
Joseph Yu <joseph.yu@yarboroughtech.com>
2018-08-08 15:00:36 +0800

    WIP: Using html color inversion for web page night mode support.

--- app/src/webkit/java/org/mozilla/focus/webkit/FocusWebViewClient.java
+++ app/src/webkit/java/org/mozilla/focus/webkit/FocusWebViewClient.java
@@ -9,13 +9,16 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.net.http.SslError;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatDelegate;
 import android.text.TextUtils;
+import android.view.View;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import org.mozilla.focus.R;
 import org.mozilla.focus.utils.AppConstants;
 import org.mozilla.focus.utils.Settings;
 import org.mozilla.focus.utils.SupportUtils;
@@ -35,9 +38,10 @@ import org.mozilla.rocket.tabs.TabViewClient;
 
     private WebViewDebugOverlay debugOverlay;
     private ErrorPageDelegate errorPageDelegate;
-
+    private int nightThemeBackgroundColor;
     FocusWebViewClient(Context context) {
         super(context);
+        nightThemeBackgroundColor = context.getResources().getColor(R.color.browserFragmentBackground);
     }
 
     public void setViewClient(TabViewClient callback) {
@@ -77,6 +81,9 @@ import org.mozilla.rocket.tabs.TabViewClient;
 
     @Override
     public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            view.setVisibility(View.INVISIBLE);
+        }
 
         if (viewClient != null) {
             viewClient.updateFailingUrl(url, false);
@@ -93,6 +100,30 @@ import org.mozilla.rocket.tabs.TabViewClient;
 
     @Override
     public void onPageFinished(WebView view, final String url) {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            view.setBackgroundColor(nightThemeBackgroundColor);
+            String js = "javascript: ("
+                    + "function () { "
+                    + "var css = 'html {-webkit-filter: hue-rotate(180deg) invert(100%) !important;}'+"
+                    + "          'html {background:#222222 !important;}'+"
+                    + "          'img,video {-webkit-filter: brightness(80%) invert(100%) hue-rotate(180deg) !important;}',"
+                    + "head = document.getElementsByTagName('head')[0],"
+                    + "style = document.createElement('style');"
+                    + "style.type = 'text/css';"
+                    + "if (style.styleSheet){"
+                    + "style.styleSheet.cssText = css;"
+                    + "} else {"
+                    + "style.appendChild(document.createTextNode(css));"
+                    + "}"
+                    //injecting the css to the head
+                    + "head.appendChild(style);"
+                    + "}());";
+
+            //view.evaluateJavascript("(function() { css = document.createElement('link'); css.id = 'moz_rocket'; css.rel = 'stylesheet'; css.href = 'data:text/css,html,link,textarea,select,button,menu,aside,img,strong, body,header,div,a,span,table,tr,td,th,tbody,p,form,input,ul,ol,li,dl,dt,dd,section,footer,nav,h1,h2,h3,h4,h5,h6,em,pre{background: #333 !important;color:#616161!important;border-color:#454530!important;text-shadow:0!important;-webkit-text-fill-color : none!important;}html a,html a *{color:#5a8498!important;text-decoration:underline!important;}html a:visited,html a:visited *,html a:active,html a:active *{color:#505f64!important;}html a:hover,html a:hover *{color:#cef!important;}html input,html select,html button,html textarea{background:#4d4c40!important;border:1px solid #5c5a46!important;border-top-color:#494533!important;border-bottom-color:#494533!important;}html input[type=button],html input[type=submit],html input[type=reset],html input[type=image],html button{border-top-color:#494533!important;border-bottom-color:#494533!important;}html input:focus,html select:focus,html option:focus,html button:focus,html textarea:focus{background:#5c5b3e!important;color:#fff!important;border-color:#494100 #635d00 #474531!important;outline:1px solid #041d29!important;}html input[type=button]:focus,html input[type=submit]:focus,html input[type=reset]:focus,html input[type=image]:focus,html button:focus{border-color:#494533 #635d00 #474100!important;}html input[type=radio]{background:none!important;border-color:#333!important;border-width:0!important;}html img[src],html input[type=image]{opacity:.5;}html img[src]:hover,html input[type=image]:hover{opacity:1;}html,html body {scrollbar-base-color: #4d4c40 !important;scrollbar-face-color: #5a5b3c !important;scrollbar-shadow-color: #5a5b3c !important;scrollbar-highlight-color: #5c5b3c !important;scrollbar-dlight-color: #5c5b3c !important;scrollbar-darkshadow-color: #474531 !important;scrollbar-track-color: #4d4c40 !important;scrollbar-arrow-color: #000 !important;scrollbar-3dlight-color: #6a6957 !important;}dt a{background-color: #333 !important;}'; document.getElementsByTagName('head')[0].appendChild(css);})(); ",null);
+            view.evaluateJavascript(js, null);
+            view.setVisibility(View.VISIBLE);
+        }
+
         if (viewClient != null) {
             viewClient.onPageFinished(view.getCertificate() != null);
         }
