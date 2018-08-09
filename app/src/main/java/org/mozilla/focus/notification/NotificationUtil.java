8cd29a89efdb1385f8bc905e0780f52b5b70b5b4
cnevinc <--global>
2018-08-09 16:16:27 +0800

    Expected to not have both vibration and notification. Closes #2238

--- app/src/main/java/org/mozilla/focus/notification/NotificationUtil.java
+++ app/src/main/java/org/mozilla/focus/notification/NotificationUtil.java
@@ -9,16 +9,24 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Build;
+import android.support.annotation.CheckResult;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
 
 import org.mozilla.focus.R;
 
 public class NotificationUtil {
 
+    public enum Channel {
+        IMPORTANT,
+        PRIVATE
+    }
+
     private static final String DEFAULT_CHANNEL_ID = "default_channel_id";
+    private static final String PRIVATE_MODE_CHANNEL_ID = "private_mode_channel_id";
 
     /**
      * To ensure we can generate a Notification Builder with same style
@@ -26,8 +34,14 @@ public class NotificationUtil {
      * @param context
      * @return
      */
-    public static NotificationCompat.Builder baseBuilder(Context context) {
-        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context, DEFAULT_CHANNEL_ID)
+    public static NotificationCompat.Builder baseBuilder(Context context, Channel channel) {
+
+        final String channelId = getChannelD(channel);
+        if (TextUtils.isEmpty(channelId)) {
+            throw new IllegalStateException("No such channel");
+        }
+
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
                 .setSmallIcon(R.drawable.ic_notification)
                 .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
                 .setColor(ContextCompat.getColor(context, R.color.surveyNotificationAccent))
@@ -43,7 +57,7 @@ public class NotificationUtil {
     // DEFAULT_VIBRATE makes notifications can show heads-up for Android 7 and below
     public static NotificationCompat.Builder importantBuilder(Context context) {
 
-        final NotificationCompat.Builder builder = baseBuilder(context)
+        final NotificationCompat.Builder builder = baseBuilder(context, Channel.IMPORTANT)
                 .setDefaults(NotificationCompat.DEFAULT_VIBRATE)
                 .setPriority(NotificationCompat.PRIORITY_HIGH);
 
@@ -64,21 +78,40 @@ public class NotificationUtil {
     // Configure the default notification channel if needed
     // See: https://developer.android.com/training/notify-user/channels#CreateChannel
     public static void init(Context context) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return;
+        }
 
+        createNotificationChannel(context, DEFAULT_CHANNEL_ID,
+                R.string.app_name,
+                NotificationManager.IMPORTANCE_HIGH);
+
+        createNotificationChannel(context, PRIVATE_MODE_CHANNEL_ID,
+                R.string.private_browsing_title,
+                NotificationManager.IMPORTANCE_LOW);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private static void createNotificationChannel(Context context, String channelId, int channelNameRes, int importance) {
         final NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        if (notificationManager != null) {
-            // NotificationChannel API is only available for Android O and above, so we need to add the check here so IDE won't complain
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                final String channelName = context.getString(R.string.app_name);
-                // IMPORTANCE_HIGH makes notifications can show heads-up for Android 8 and above
-                final NotificationChannel notificationChannel = new NotificationChannel(DEFAULT_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_HIGH);
-                notificationChannel.enableLights(true);
-                notificationChannel.setLightColor(Color.RED);
-                notificationChannel.setImportance(NotificationManager.IMPORTANCE_HIGH);
 
-                notificationManager.createNotificationChannel(notificationChannel);
-            }
+        if (notificationManager == null) {
+            return;
         }
+
+        final NotificationChannel channel = new NotificationChannel(channelId, context.getString(channelNameRes), importance);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    @CheckResult
+    private static String getChannelD(Channel channel) {
+        switch (channel) {
+            case IMPORTANT:
+                return DEFAULT_CHANNEL_ID;
+            case PRIVATE:
+                return PRIVATE_MODE_CHANNEL_ID;
+        }
+        return "";
     }
 }
