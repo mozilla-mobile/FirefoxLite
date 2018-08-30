2b4d0b9f995b71a188fd59218ae5e61530fed7ef
Julian_Chu <walkingice0204@gmail.com>
2018-08-30 10:40:38 +0800

    pref: add Development category to preference
    This category contains some preference only for development purpose,
    hence it is only visible under dev-build.

--- app/src/main/java/org/mozilla/focus/settings/SettingsFragment.java
+++ app/src/main/java/org/mozilla/focus/settings/SettingsFragment.java
@@ -42,10 +42,10 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.settings);
-        if (AppConstants.isReleaseBuild() || AppConstants.isBetaBuild()) {
+        if (!AppConstants.isDevBuild() && !AppConstants.isFirebaseBuild()) {
             PreferenceScreen rootPreferences = (PreferenceScreen) findPreference(PREF_KEY_ROOT);
-            Preference prefPrivateMode = findPreference(PrivateMode.PREF_KEY_PRIVATE_MODE_ENABLED);
-            rootPreferences.removePreference(prefPrivateMode);
+            Preference category = findPreference(getString(R.string.pref_key_category_development));
+            rootPreferences.removePreference(category);
         }
     }
 
