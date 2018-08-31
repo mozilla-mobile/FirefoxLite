baff606dd70eeec3336edc02242fb65d58abb4bb
cnevinc <--global>
2018-08-31 11:35:11 +0800

    Make LaunchIntentDispatcher the entry point of the app. #closes 1983
    This includes
    1. Pin Shortcut
    2. Launcher acitivty for background notification message
    3. AppLaunchMode parsing
    
    AppLaunchMethod use enums to replace if-else check. When the scneraios
    getting more and more, there'll be too many boilerplate code. So I
    switch to the old-school way: if-then-else.

--- app/src/main/java/org/mozilla/focus/notification/NotificationActionBroadcastReceiver.java
+++ app/src/main/java/org/mozilla/focus/notification/NotificationActionBroadcastReceiver.java
@@ -15,11 +15,11 @@ import android.util.Log;
 
 import org.mozilla.focus.R;
 import org.mozilla.focus.activity.InfoActivity;
-import org.mozilla.focus.activity.MainActivity;
 import org.mozilla.focus.telemetry.TelemetryWrapper;
 import org.mozilla.focus.utils.IntentUtils;
 import org.mozilla.focus.utils.Settings;
 import org.mozilla.focus.utils.SupportUtils;
+import org.mozilla.rocket.component.RocketLauncherActivity;
 
 // This class handles all click/actions users performed on a notification.
 // This ensures that all telemetry works for action/click are in one place.
@@ -75,7 +75,7 @@ public class NotificationActionBroadcastReceiver extends BroadcastReceiver {
             NotificationManagerCompat.from(context).cancel(NotificationId.DEFAULT_BROWSER);
 
         } else if (bundle.getBoolean(IntentUtils.EXTRA_NOTIFICATION_CLICK_LOVE_FIREFOX)) {
-            nexStep = new Intent(context, MainActivity.class);
+            nexStep = new Intent(context, RocketLauncherActivity.class);
             nexStep.putExtra(IntentUtils.EXTRA_SHOW_RATE_DIALOG, true);
 
             TelemetryWrapper.clickRateAppNotification();
--- app/src/main/java/org/mozilla/rocket/component/LaunchIntentDispatcher.kt
+++ app/src/main/java/org/mozilla/rocket/component/LaunchIntentDispatcher.kt
@@ -9,10 +9,15 @@ import org.mozilla.focus.activity.SettingsActivity
 import org.mozilla.focus.notification.RocketMessagingService
 import org.mozilla.focus.utils.IntentUtils
 import org.mozilla.focus.utils.SupportUtils
+import org.mozilla.focus.telemetry.TelemetryWrapper
 import org.mozilla.focus.widget.DefaultBrowserPreference
-import org.mozilla.rocket.component.LaunchIntentDispatcher.Command.SET_DEFAULT
 
 class LaunchIntentDispatcher {
+
+    enum class LaunchMethod(val value: String) {
+        EXTRA_BOOL_TEXT_SELECTION("text_selection"),
+        EXTRA_BOOL_HOME_SCREEN_SHORTCUT("shortcut")
+    }
 
     enum class Command(val value: String) {
         SET_DEFAULT("SET_DEFAULT")
@@ -23,15 +28,37 @@ class LaunchIntentDispatcher {
     }
 
     companion object {
-
+        /**
+         * Now [LaunchIntentDispatcher.dispatch] is the universal entry to our app. All starting method should be analyzed here.
+         * */
         @JvmStatic
         @CheckResult
         fun dispatch(context: Context, intent: Intent): Action? {
+
+
+            /**
+             * This extra is passed when we click our icon in mobile launcher
+             * */
+            if (intent.getBooleanExtra(LaunchMethod.EXTRA_BOOL_HOME_SCREEN_SHORTCUT.value, false)) {
+                TelemetryWrapper.launchByHomeScreenShortcutEvent()
+                return Action.NORMAL
+            }
+
+            /**
+             * This extra is passed when we long click on some text and click "Search in Firefox Rocket"
+             * */
+            if (intent.getBooleanExtra(LaunchMethod.EXTRA_BOOL_TEXT_SELECTION.value, false)) {
+                TelemetryWrapper.launchByTextSelectionSearchEvent()
+                return Action.NORMAL
+            }
+
             /**
              *  This intent is used when we want to set default browser on Android L, see [DefaultBrowserPreference]
+             *  Called by the internal app, doesn't count as a launch event
              * */
             if (intent.getBooleanExtra(DefaultBrowserPreference.EXTRA_RESOLVE_BROWSER, false)) {
                 context.startActivity(Intent(context, SettingsActivity::class.java))
+                // called by internal app, doesn't count as a launch event
                 return Action.HANDLED
             }
             /**
@@ -46,10 +73,13 @@ class LaunchIntentDispatcher {
                 intent.putExtra(IntentUtils.EXTRA_OPEN_NEW_TAB, true)
             }
 
-            /** This extra is passed by the Notification (either [RocketMessagingService.onRemoteMessage] or System tray*/
+            /**
+             * This extra is passed by the Notification (either [RocketMessagingService.onRemoteMessage] or System tray
+             *  Called by the internal app, doesn't count as a launch event
+             * */
             intent.getStringExtra(RocketMessagingService.PUSH_COMMAND)?.apply {
                 when (this) {
-                    SET_DEFAULT.value -> {
+                    Command.SET_DEFAULT.value -> {
                         if (!IntentUtils.openDefaultAppsSettings(context)) {
                             intent.action = Intent.ACTION_VIEW
                             intent.data = Uri.parse(SupportUtils.getSumoURLForTopic(context, "rocket-default"))
@@ -60,6 +90,22 @@ class LaunchIntentDispatcher {
                     }
                 }
             }
+
+            /**
+             *  When notification received in bg, the intent 's action is also ACTION_MAIN.(https://goo.gl/sMLZhZ )
+             * We've return early in previous check so they won't reach this line.
+             * Their actions are recorded in [org.mozilla.focus.notification.NotificationActionBroadcastReceiver]
+             * */
+            if (Intent.ACTION_MAIN == intent.action) {
+                TelemetryWrapper.launchByAppLauncherEvent()
+                return Action.NORMAL
+            }
+
+            if (Intent.ACTION_VIEW == intent.action) {
+                TelemetryWrapper.launchByExternalAppEvent()
+                return Action.NORMAL
+            }
+
             return Action.NORMAL
         }
     }
