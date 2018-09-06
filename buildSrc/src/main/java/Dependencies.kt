298b0fd7f5bdabd342479d7e8cb9dc0b7bce08c8
Teng-pao Yu <git@mtwtm.org>
2018-09-06 12:59:17 +0800

    Bump version name to 3.2.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -4,7 +4,7 @@ object Versions {
     const val compile_sdk = 27
     const val build_tools = "27.0.3"
     const val version_code = 1
-    const val version_name = "3.1.0"
+    const val version_name = "3.2.0"
     const val android_gradle_plugin = "3.1.3"
     const val dicedmelon_jacoco_plugin = "0.1.3"
     const val gms_oss_licenses_plugin = "0.9.1"
