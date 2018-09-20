6d358a06871546d17ee490af4586a6ebc98e8963
Julian_Chu <walkingice0204@gmail.com>
2018-09-20 15:54:11 +0800

    refactor: suppress using-deprecated-class warning
    We are using some deprecated classes, and we cannot get rid of them due
    to it effects our data. We need more discussion for those data, to
    ensure we are ready to replace those deprecated class.
    
    Suppress the warning for now, to keep using Kotlin.

--- app/src/main/java/org/mozilla/focus/telemetry/TelemetryWrapper.kt
+++ app/src/main/java/org/mozilla/focus/telemetry/TelemetryWrapper.kt
@@ -3,6 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// Some deprecated class (ie. TelemetryEventPingBuilder) should be replaced, however the replace
+// process takes time. To suppress the warning until we are ready to replace.
+// Please refer to github issue: #2660
+@file:Suppress("DEPRECATION")
+
 package org.mozilla.focus.telemetry
 
 import android.content.Context
