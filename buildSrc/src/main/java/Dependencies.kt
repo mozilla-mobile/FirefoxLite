67128ecc40a682bc674af7d6d50a65c5e5d3db83
maliu <max@mxli.us>
2018-11-02 11:46:51 +0800

    Upgrade oss-licenses-plugin to avoid licenses list checkin to source tree

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
     const val version_name = "1.0.1"
     const val android_gradle_plugin = "3.2.0"
     const val dicedmelon_jacoco_plugin = "0.1.3"
-    const val gms_oss_licenses_plugin = "0.9.1"
+    const val gms_oss_licenses_plugin = "0.9.3"
     const val support = "28.0.0"
     const val constraint = "1.1.2"
     const val arch_core = "1.1.1"
