70b4729f03c6038dcd2d371cc91d8ac30d2a0e07
Nevin Chen <cnevinchen@gmail.com>
2019-01-11 17:52:57 +0800

    Use SessionManager.Observer and Session.Obsever to determine if the loading is done.
    Related #3006
    Previously we use BrowserFragment's state as the indicator. Now we should use the callback in SessionManager's
    and Session's Observer.
    
    I'll land this first so I can continue the refactoring.

--- app/src/androidTest/java/org/mozilla/focus/helper/PrivateSessionLoadedIdlingResource.java
+++ app/src/androidTest/java/org/mozilla/focus/helper/PrivateSessionLoadedIdlingResource.java
@@ -4,21 +4,36 @@
 
 package org.mozilla.focus.helper;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.test.espresso.IdlingResource;
 
-import org.mozilla.focus.fragment.BrowserFragment;
+import android.view.View;
+import android.webkit.GeolocationPermissions;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import mozilla.components.browser.session.Download;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.mozilla.rocket.privately.PrivateModeActivity;
+import org.mozilla.rocket.tabs.Session;
+import org.mozilla.rocket.tabs.SessionManager;
+import org.mozilla.rocket.tabs.TabView;
+import org.mozilla.rocket.tabs.TabViewEngineSession;
 
 /**
  * An IdlingResource implementation that waits until the BrowserFragment is not loading anymore.
  * Only after loading has completed further actions will be performed.
  */
-public class PrivateSessionLoadedIdlingResource implements IdlingResource, BrowserFragment.LoadStateListener {
+public class PrivateSessionLoadedIdlingResource implements IdlingResource {
     private ResourceCallback resourceCallback;
     private boolean isCompleted;
+    private Observer observer = new Observer();
 
     public PrivateSessionLoadedIdlingResource(PrivateModeActivity activity) {
-        activity.getBrowserFragment().setIsLoadingListener(this);
+        final SessionManager sessionManager = activity.getSessionManager();
+        sessionManager.register(observer);
     }
 
     @Override
@@ -42,12 +57,104 @@ public class PrivateSessionLoadedIdlingResource implements IdlingResource, Brows
         this.resourceCallback = callback;
     }
 
-    @Override
-    public void isLoadingChanged(boolean isLoading) {
-        if (!isLoading) {
-            isCompleted = true;
-            invokeCallback();
+
+    class Observer implements SessionManager.Observer, Session.Observer {
+
+        @Override
+        public void updateFailingUrl(@Nullable String url, boolean updateFromError) {
+
         }
 
+        @Override
+        public boolean handleExternalUrl(@Nullable String url) {
+            return false;
+        }
+
+        @Override
+        public boolean onShowFileChooser(@NotNull TabViewEngineSession es, @Nullable ValueCallback<Uri[]> filePathCallback, @Nullable WebChromeClient.FileChooserParams fileChooserParams) {
+            return false;
+        }
+
+        @Override
+        public void onLoadingStateChanged(@NotNull Session session, boolean loading) {
+            isCompleted = !loading;
+            if (isCompleted) {
+                invokeCallback();
+            }
+        }
+
+        @Override
+        public void onNavigationStateChanged(@NotNull Session session, boolean canGoBack, boolean canGoForward) {
+
+        }
+
+        @Override
+        public void onSecurityChanged(@NotNull Session session, boolean isSecure) {
+
+        }
+
+        @Override
+        public void onUrlChanged(@NotNull Session session, @Nullable String url) {
+
+        }
+
+        @Override
+        public void onProgress(@NotNull Session session, int progress) {
+
+        }
+
+        @Override
+        public void onTitleChanged(@NotNull Session session, @Nullable String title) {
+
+        }
+
+        @Override
+        public void onReceivedIcon(@Nullable Bitmap icon) {
+
+        }
+
+        @Override
+        public void onLongPress(@NotNull Session session, @NotNull TabView.HitTarget hitTarget) {
+
+        }
+
+        @Override
+        public boolean onDownload(@NotNull Session session, @NotNull Download download) {
+            return false;
+        }
+
+        @Override
+        public void onFindResult(@NotNull Session session, @NotNull mozilla.components.browser.session.Session.FindResult result) {
+
+        }
+
+        @Override
+        public void onEnterFullScreen(@NotNull TabView.FullscreenCallback callback, @Nullable View view) {
+
+        }
+
+        @Override
+        public void onExitFullScreen() {
+
+        }
+
+        @Override
+        public void onGeolocationPermissionsShowPrompt(@NotNull String origin, @Nullable GeolocationPermissions.Callback callback) {
+
+        }
+
+        @Override
+        public void onFocusChanged(@Nullable Session session, @NotNull SessionManager.Factor factor) {
+        }
+
+        @Override
+        public void onSessionAdded(@NotNull Session session, @Nullable Bundle arguments) {
+            session.register(observer);
+        }
+
+        @Override
+        public void onSessionCountChanged(int count) {
+        }
     }
+
 }
