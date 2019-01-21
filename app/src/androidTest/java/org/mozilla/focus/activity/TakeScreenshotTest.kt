712198769064dde8297e274894d807fa8f99e58f
daisy <pliu@mozilla.com>
2019-01-21 18:13:06 +0800

    [TC0054]open web of screenshot [TC0055] edit screenshot [TC0056]share screenshot [TC0057] view info of screenshot

--- app/src/androidTest/java/org/mozilla/focus/activity/TakeScreenshotTest.kt
+++ app/src/androidTest/java/org/mozilla/focus/activity/TakeScreenshotTest.kt
@@ -8,11 +8,10 @@ package org.mozilla.focus.activity
 import android.Manifest
 import android.content.Intent
 import android.support.annotation.Keep
-import android.support.test.rule.ActivityTestRule
+import android.support.test.espresso.intent.rule.IntentsTestRule
 import android.support.test.rule.GrantPermissionRule
 import android.support.test.runner.AndroidJUnit4
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,8 +19,8 @@ import org.mozilla.focus.autobot.screenshot
 import org.mozilla.focus.autobot.session
 import org.mozilla.focus.utils.AndroidTestUtils
 
+
 @Keep
-@Ignore
 @RunWith(AndroidJUnit4::class)
 class TakeScreenshotTest {
 
@@ -31,12 +30,20 @@ class TakeScreenshotTest {
 
     @JvmField
     @Rule
-    val activityTestRule = ActivityTestRule(MainActivity::class.java, true, false)
+    val activityTestRule = IntentsTestRule(MainActivity::class.java, true, false)
 
     @Before
     fun setUp() {
         AndroidTestUtils.beforeTest()
         activityTestRule.launchActivity(Intent())
+
+        // Make pre-existing screenshot
+        session {
+            loadPageFromHomeSearchField(activityTestRule.activity, TARGET_URL_SITE)
+            clickCaptureScreen(activityTestRule.activity)
+            clickBrowserMenu()
+            clickMenuMyShots()
+        }
     }
 
     companion object {
@@ -45,31 +52,96 @@ class TakeScreenshotTest {
 
     /**
      * Test case no: TC0058
-     * Test case name: Take Screenshot
+     * Test case name: Take Screenshot and delete
      * Steps:
-     * 1. Launch app
-     * 2. Tap screenshot capture button
-     * 3. Tap toast message displays "Screenshot saved"
-     * 4. Tap menu
-     * 5. Tap my shots viewer
-     * 6. Tap first screenshot
-     * 7. Tap delete
-     * 8. Tap confirm delete
-     * 9. Check it goes back to my shots viewer
+     * 1. Given pre-existing screenshot
+     * 2. Tap menu -> my shots viewer -> first screenshot
+     * 3. Tap delete
+     * 4. Tap confirm delete
+     * 5. Check it goes back to my shots viewer
      * */
     @Test
-    fun takeScreenshot_screenshotIsCaptured() {
-
-        session {
-            loadPageFromHomeSearchField(activityTestRule.activity, TARGET_URL_SITE)
-            clickCaptureScreen(activityTestRule.activity)
-            clickBrowserMenu()
-            clickMenuMyShots()
-        }
+    fun takeScreenshot_deleteScreenShot() {
 
         screenshot {
             clickFirstItemInMyShotsAndOpen()
-            longClickAndDeleteTheFirstItemInMyShots()
+            clickAndDeleteTheFirstItemInMyShots()
+        }
+    }
+
+    /**
+     * Test case no: TC0057
+     * Test case name: Take Screenshot and view info
+     * Steps:
+     * 1. Given pre-existing screenshot
+     * 2. Tap menu -> my shots viewer -> first screenshot
+     * 3. Click info
+     * 4. Check image dialog is displayed
+     * 5. press back
+     * 6. Check image info dialog is dismissed
+     * */
+    @Test
+    fun takeScreenshot_viewInfo() {
+
+        screenshot {
+            clickFirstItemInMyShotsAndOpen()
+            clickInfoTheFirstItemInMyShots()
+            pressBack()
+            checkScreenshotInfoDialogDimissed()
+        }
+    }
+
+    /**
+     * Test case no: TC0054
+     * Test case name: Take Screenshot and open the web
+     * Steps:
+     * 1. Given preexisting screenshot
+     * 2. Tap menu -> my shots viewer -> first screenshot
+     * 3. Click open the web
+     * 5. Check url matches
+     * */
+    @Test
+    fun takeScreenshot_openWeb() {
+
+        screenshot {
+            clickFirstItemInMyShotsAndOpen()
+            clickOpenWebInScreenshotViewer(TARGET_URL_SITE)
+        }
+    }
+
+    /**
+     * Test case no: TC0054
+     * Test case name: Take Screenshot and share
+     * Steps:
+     * 1. Given preexisting screenshot
+     * 2. Tap menu -> my shots viewer -> first screenshot
+     * 3. Click share
+     * 5. Check intent is sent
+     * */
+    @Test
+    fun takeScreenshot_share() {
+
+        screenshot {
+            clickFirstItemInMyShotsAndOpen()
+            clickShareInScreenshotViewer(TARGET_URL_SITE)
+        }
+    }
+
+    /**
+     * Test case no: TC0055
+     * Test case name: Take Screenshot and edit
+     * Steps:
+     * 1. Given preexisting screenshot
+     * 2. Tap menu -> my shots viewer -> first screenshot
+     * 3. Click edit
+     * 5. Check intent is sent
+     * */
+    @Test
+    fun takeScreenshot_Edit() {
+
+        screenshot {
+            clickFirstItemInMyShotsAndOpen()
+            clickEditInScreenshotViewer()
         }
     }
 }
