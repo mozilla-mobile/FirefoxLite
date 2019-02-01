73335746c364951d54d2e13c3c9faccafeed9f1c
Nevin Chen <cnevinchen@gmail.com>
2019-02-01 17:51:57 +0800

    Turns out my guessing may be correct. I tried 1. Make org.mozilla.components:ui-autocomplete open (use api) 2. Make base module load MainDispatcherFactory first
    I guess this problem only happens in local(base.apk path changes.)
    otherwise it doesn't explain why this issue never happens in produciton before
    
    I make it version 3 and will deploy to play store to test.

--- app/src/main/java/org/mozilla/rocket/component/RocketLauncherActivity.kt
+++ app/src/main/java/org/mozilla/rocket/component/RocketLauncherActivity.kt
@@ -3,12 +3,22 @@ package org.mozilla.rocket.component
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.mozilla.focus.activity.MainActivity
+import kotlin.coroutines.CoroutineContext
 
-class RocketLauncherActivity : AppCompatActivity() {
+class RocketLauncherActivity : AppCompatActivity(), CoroutineScope {
+    private lateinit var job: Job
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        job = Job()
 
         val action = LaunchIntentDispatcher.dispatch(this, intent)
         when (action) {
@@ -21,11 +31,14 @@ class RocketLauncherActivity : AppCompatActivity() {
      * Launch the browser activity.
      */
     private fun dispatchNormalIntent() {
-        val intent = Intent(intent)
-        intent.setClass(applicationContext, MainActivity::class.java)
-        filterFlags(intent)
-        startActivity(intent)
-        finish()
+        this.launch {
+            val intent = Intent(intent)
+            intent.setClass(applicationContext, MainActivity::class.java)
+            filterFlags(intent)
+            startActivity(intent)
+            finish()
+        }
+
     }
 
     private fun filterFlags(intent: Intent) {
@@ -33,5 +46,10 @@ class RocketLauncherActivity : AppCompatActivity() {
         // task activity and we never want to start a second task here). See bug 1280112.
         intent.flags = intent.flags and Intent.FLAG_ACTIVITY_NEW_TASK.inv()
         intent.flags = intent.flags and Intent.FLAG_ACTIVITY_CLEAR_TASK.inv()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
