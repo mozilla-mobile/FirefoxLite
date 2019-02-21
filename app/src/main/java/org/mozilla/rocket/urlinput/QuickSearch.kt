48f20895e67b6012a633bc12de3b93cbed3203cb
Joseph Yu <joseph.yu@yarboroughtech.com>
2019-02-21 14:18:59 +0800

    Handling searches with space on instagram. Closes #3202

--- app/src/main/java/org/mozilla/rocket/urlinput/QuickSearch.kt
+++ app/src/main/java/org/mozilla/rocket/urlinput/QuickSearch.kt
@@ -6,6 +6,12 @@ package org.mozilla.rocket.urlinput
 
 import android.net.Uri
 
+/**
+ * The class holds the Quick Search setting.
+ * The order of param is important cause we'll operate them in the same order.
+ * If one day the order needs to be different for each Quick Search setting, we may need
+ * a more complex data structure.
+ */
 data class QuickSearch(
     var name: String,
     var icon: String,
@@ -13,13 +19,21 @@ data class QuickSearch(
     var homeUrl: String,
     var urlPrefix: String = "",
     var urlSuffix: String = "",
-    var patternEncode: Boolean = false
+    var patternEncode: Boolean = false,
+    var permitSpace: Boolean = true
 ) {
     fun generateLink(keyword: String): String {
-        var url = if (patternEncode) {
-            Uri.encode(String.format(searchUrlPattern, keyword))
+        var newKeyword = keyword
+
+        // Remove all spaces in keyword
+        if (!permitSpace) {
+            newKeyword = keyword.replace("\\s".toRegex(), "")
+        }
+
+        val url = if (patternEncode) {
+            Uri.encode(String.format(searchUrlPattern, newKeyword))
         } else {
-            String.format(searchUrlPattern, Uri.encode(keyword))
+            String.format(searchUrlPattern, Uri.encode(newKeyword))
         }
         return urlPrefix + url + urlSuffix
     }
