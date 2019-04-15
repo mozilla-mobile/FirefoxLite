bd3ea5b0031dcf4e0c87415c1d2afbcf85a29de7
Teng-pao Yu <git@mtwtm.org>
2019-04-15 18:20:17 +0800

    New module strictmodeviolator

---
+++ components/utils/strictmodeviolator/src/main/java/org/mozilla/strictmodeviolator/StrictModeViolation.kt
@@ -0,0 +1,16 @@
+package org.mozilla.strictmodeviolator
+
+import android.os.StrictMode
+
+object StrictModeViolation {
+    @JvmStatic
+    fun <T> tempGrant(tempChange: (StrictMode.ThreadPolicy.Builder) -> StrictMode.ThreadPolicy.Builder, lambda: () -> T): T {
+        val cachedPolicy = StrictMode.getThreadPolicy()
+        StrictMode.setThreadPolicy(tempChange(StrictMode.ThreadPolicy.Builder(cachedPolicy)).build())
+        try {
+            return lambda()
+        } finally {
+            StrictMode.setThreadPolicy(cachedPolicy)
+        }
+    }
+}
