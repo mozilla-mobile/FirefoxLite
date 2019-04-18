92d830ee89b8a33751edbe2f72634f23e2c6c110
Benjamin Cheng <chengwc@gmail.com>
2019-04-18 16:27:17 +0800

    Issue #3428 - Add data parsing

---
+++ app/src/main/java/org/mozilla/rocket/content/data/Coupon.kt
@@ -0,0 +1,13 @@
+package org.mozilla.rocket.content.data
+
+object CouponKey {
+    const val KEY_ID = "id"
+    const val KEY_CATEGORY = "category"
+    const val KEY_SUBCATEGORY = "subcategory"
+    const val KEY_FEED = "feed"
+    const val KEY_START = "start"
+    const val KEY_END = "end"
+    const val KEY_ACTIVE = "active"
+}
+
+data class Coupon(val id: String, val category: String, val subcategory: String, val feed: String, val start: Long, val end: Long, val active: Boolean, val link: ShoppingLink)
