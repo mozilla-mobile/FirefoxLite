cc9a8b75a2e21da5cf262799c486fe71bc2a5d4c
Roger <pharspher@gmail.com>
2019-04-19 12:46:41 +0800

    Implement tracker-blocked popup UI

---
+++ app/src/main/java/org/mozilla/rocket/privately/browse/TrackerPopup.kt
@@ -0,0 +1,100 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.rocket.privately.browse
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
+import android.support.v4.content.ContextCompat
+import android.support.v4.graphics.drawable.DrawableCompat
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupWindow
+import kotlinx.android.synthetic.main.view_tracker_popup.view.*
+import org.mozilla.focus.R
+
+class TrackerPopup(context: Context) : PopupWindow(
+        View.inflate(
+                context,
+                R.layout.view_tracker_popup,
+                null
+        ),
+        ViewGroup.LayoutParams.MATCH_PARENT,
+        ViewGroup.LayoutParams.WRAP_CONTENT,
+        true
+) {
+
+    companion object {
+        private const val COUNT_INFINITY = "∞"
+        private const val COUNT_DISABLED = "-"
+    }
+
+    private val counterColorEnabled = ContextCompat.getColor(context, R.color.palettePurple100)
+    private val counterColorDisabled = ContextCompat.getColor(context, R.color.paletteWhite100)
+
+    var onSwitchToggled: ((Boolean) -> Unit)? = null
+
+    var blockedCount: Int = 0
+        set(value) {
+            if (contentView.tracker_switch.isChecked) {
+                contentView.tracker_count.text = if (value < 99) {
+                    value.toString()
+                } else {
+                    COUNT_INFINITY
+                }
+            }
+            field = value
+        }
+
+    init {
+        val view = contentView
+
+        val switchView = contentView.tracker_switch
+        DrawableCompat.setTintList(
+                switchView.thumbDrawable,
+                ContextCompat.getColorStateList(context, R.color.switch_thumb_dark)
+        )
+        DrawableCompat.setTintList(
+                switchView.trackDrawable,
+                ContextCompat.getColorStateList(context, R.color.switch_track_dark)
+        )
+
+        val counterBkg = DrawableCompat.wrap(view.tracker_count_container.background).mutate()
+        view.tracker_count_container.background = counterBkg
+
+        val counterView = view.tracker_count
+        switchView.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                counterView.text = "0"
+                DrawableCompat.setTint(counterBkg, counterColorEnabled)
+            } else {
+                view.tracker_count.text = COUNT_DISABLED
+                DrawableCompat.setTint(counterBkg, counterColorDisabled)
+            }
+
+            onSwitchToggled?.invoke(isChecked)
+        }
+    }
+
+    fun show(parentView: View) {
+        val res = parentView.resources
+        val margin: Int = (res.getDimension(R.dimen.fixed_menu_height) +
+                res.getDimension(R.dimen.browser_tracker_popup_bottom_margin)).toInt()
+
+        this.width = (parentView.measuredWidth * 0.8f).toInt()
+
+        val offsetY = (parentView.context as Activity).window.decorView.let { decorView ->
+            val contentHeight = Rect().apply {
+                decorView.getWindowVisibleDisplayFrame(this)
+            }.bottom
+            val winHeight = decorView.height
+            winHeight - contentHeight
+        }
+
+        showAtLocation(parentView, Gravity.BOTTOM, 0, margin + offsetY)
+    }
+}
