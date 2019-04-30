6e98fe3568cbae2e672caa257a1c7164da019e31
Conjuror <kuang.con@gmail.com>
2019-04-30 11:25:09 +0800

    fix indent to 4 and refact some code

--- tools/metrics/amplitude_csv2schema.py
+++ tools/metrics/amplitude_csv2schema.py
@@ -5,72 +5,64 @@ import json
 
 
 BASE_SCHEMA = {
-  "source": "telemetry",
-  "eventGroups": [
-    {
-      "eventGroupName": "Rocket",
-      "events": []
+    "source": "telemetry",
+    "eventGroups": [
+        {
+            "eventGroupName": "Rocket",
+            "events": []
+        }
+    ],
+    "filters": {
+        "docType": [
+            "focus-event"
+        ],
+        "appName": [
+            "Zerda",
+            "OTHER"
+        ],
+        "os": [
+            "Android"
+        ]
     }
-  ],
-  "filters": {
-    "docType": [
-      "focus-event"
-    ],
-    "appName": [
-      "Zerda",
-      "OTHER"
-    ],
-    "os": [
-      "Android"
-    ]
-  }
 }
 
 def get_event(name, categories, methods, objects, values, amplitude_props, descr=""):
     props = {
-      "timestamp": {
-        "type": "number",
-        "minimum": 0
-      },
-      "category": {
-        "type": "string",
-        "enum": categories
-      },
-      "method": {
-        "type": "string",
-        "enum": methods
-      },
-      "object": {
-        "type": "string",
-        "enum": objects
-      }
+        "timestamp": {
+            "type": "number",
+            "minimum": 0
+        },
+        "category": {
+            "type": "string",
+            "enum": categories
+        },
+        "method": {
+            "type": "string",
+            "enum": methods
+        },
+        "object": {
+            "type": "string",
+            "enum": objects
+        }
     }
 
-    if values:
+    if values != [""]:
         props["value"] = {
-            "type": "string",
-            "enum": values
+                "type": "string",
+                "enum": values
         }
 
     return {
-      "name": name,
-      "description": descr,
-      "amplitudeProperties": amplitude_props,
-      "schema": {
-        "$schema": "http://json-schema.org/schema#",
-        "type": "object",
-        "properties": props,
-        "required": list(props.keys())
-      }
+        "name": name,
+        "description": descr,
+        "amplitudeProperties": amplitude_props,
+        "schema": {
+            "$schema": "http://json-schema.org/schema#",
+            "type": "object",
+            "properties": props,
+            "required": list(props.keys())
+        }
     },
-def refactorNull(a):
-  for i, s in enumerate(a):
-    if s == "null":
-      a[i] = None
-  return a
-
-def sep(val):
-    return [e.strip() for e in val.split(",") if e.strip() if e.replace('"',"")]
 
 def convert(filename):
     default = BASE_SCHEMA
@@ -81,18 +73,16 @@ def convert(filename):
     for e in events:
         name, cs, ms, os, vs, extra, aps = e
 
+        vs = [x if x != "null" else None for x in vs.strip('\"').split(',')]
         amplitude_props = {}
         if extra:
-            separated = sep(extra)
+            separated = extra.strip('\"').split(',')
             for e in separated:
                 if '=' in e:
                     k, v = e.split("=")
-                    amplitude_props[k] = "extra."+k
-  
+                    amplitude_props[k] = "extra." + k
 
-        default["eventGroups"][0]["events"]+=get_event(name,sep(cs),sep(ms),sep(os),refactorNull(sep(vs)),amplitude_props)
-        #default["eventGroups"][0]["events"]+=get_event(name,sep(cs),sep(ms),sep(os),sep(vs),amplitude_props)
-
+        default["eventGroups"][0]["events"] += get_event(name, [cs], [ms], [os], vs, amplitude_props)
     return default
 
 
@@ -101,12 +91,9 @@ if __name__ == "__main__":
         description="CSV to Schema converter for Events to Amplitude",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-
     parser.add_argument(
-        "filename",
-        help = "CSV file with the data to convert"
+            "filename",
+            help = "CSV file with the data to convert"
     )
-
     args = parser.parse_args()
-
     print json.dumps(convert(args.filename), sort_keys = True, indent=4)
