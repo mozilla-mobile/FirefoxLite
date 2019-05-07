194891b536da9a8fc425d525d31068fe8c464972
Roger <pharspher@gmail.com>
2019-05-07 14:51:23 +0800

    Add orientation test

---
+++ app/src/androidTest/java/org/mozilla/focus/activity/ScreenOrientationTest.kt
@@ -0,0 +1,172 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.activity
+
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.support.annotation.Keep
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.IdlingRegistry
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.replaceText
+import android.support.test.espresso.action.ViewActions.pressKey
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.view.KeyEvent
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mozilla.focus.R
+import org.mozilla.focus.helper.BeforeTestTask
+import org.mozilla.focus.helper.SessionLoadedIdlingResource
+import org.mozilla.focus.utils.AndroidTestUtils
+
+@Keep
+@RunWith(AndroidJUnit4::class)
+class ScreenOrientationTest {
+    companion object {
+        private const val TARGET_URL_SITE_1 = "file:///android_asset/gpl.html"
+    }
+
+    @Rule @JvmField
+    val activityTestRule = ActivityTestRule(
+            MainActivity::class.java,
+            true,
+            false
+    )
+
+    private var sessionLoadedIdlingResource: SessionLoadedIdlingResource? = null
+
+    @Before
+    fun setUp() {
+        BeforeTestTask.Builder().clearBrowsingHistory().build().execute()
+        activityTestRule.launchActivity(Intent())
+    }
+
+    @After
+    fun tearDown() {
+        sessionLoadedIdlingResource?.let {
+            IdlingRegistry.getInstance().unregister(it)
+        }
+    }
+
+    @Test
+    fun testBrowserScreen() {
+        // Prepare
+        gotoBrowserScreen()
+
+        // Test - Should be SCREEN_ORIENTATION_USER when in browser screen
+        assertEquals(ActivityInfo.SCREEN_ORIENTATION_USER, activityTestRule.activity.requestedOrientation)
+    }
+
+    @Test
+    fun testFirstLevelMenu() {
+        // Prepare
+        gotoBrowserScreen()
+        AndroidTestUtils.tapBrowserMenuButton()
+
+        // Test - Should be SCREEN_ORIENTATION_PORTRAIT when the first level menu is opened
+        assertEquals(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, activityTestRule.activity.requestedOrientation)
+
+        Espresso.pressBack()
+
+        assertEquals(ActivityInfo.SCREEN_ORIENTATION_USER, activityTestRule.activity.requestedOrientation)
+
+        assertBackToHomeOrientation()
+    }
+
+    @Test
+    fun testSecondLevelMenu() {
+        // Prepare
+        gotoBrowserScreen()
+        AndroidTestUtils.tapBrowserMenuButton()
+        onView(withId(R.id.menu_history)).perform(click())
+
+        // Test - Should be SCREEN_ORIENTATION_PORTRAIT when the second level menu is opened
+        assertEquals(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, activityTestRule.activity.requestedOrientation)
+
+        Espresso.pressBack()
+
+        assertEquals(ActivityInfo.SCREEN_ORIENTATION_USER, activityTestRule.activity.requestedOrientation)
+
+        assertBackToHomeOrientation()
+    }
+
+    @Test
+    fun testFindInPage() {
+        // Prepare
+        sessionLoadedIdlingResource = SessionLoadedIdlingResource(activityTestRule.activity)
+        gotoBrowserScreen()
+        IdlingRegistry.getInstance().register(sessionLoadedIdlingResource)
+        AndroidTestUtils.tapBrowserMenuButton()
+        IdlingRegistry.getInstance().unregister(sessionLoadedIdlingResource)
+
+        onView(withId(R.id.menu_find_in_page)).perform(click())
+
+        // Test - Should be SCREEN_ORIENTATION_PORTRAIT when in find-in-page mode
+        assertEquals(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, activityTestRule.activity.requestedOrientation)
+
+        Espresso.pressBack()
+
+        assertEquals(ActivityInfo.SCREEN_ORIENTATION_USER, activityTestRule.activity.requestedOrientation)
+
+        assertBackToHomeOrientation()
+    }
+
+    @Test
+    fun testTabTray() {
+        // Prepare
+        gotoBrowserScreen()
+        onView(withId(R.id.btn_tab_tray)).perform(click())
+
+        // Test - Should be SCREEN_ORIENTATION_PORTRAIT when the second level menu is opened
+        assertEquals(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, activityTestRule.activity.requestedOrientation)
+
+        Espresso.pressBack()
+
+        assertEquals(ActivityInfo.SCREEN_ORIENTATION_USER, activityTestRule.activity.requestedOrientation)
+
+        assertBackToHomeOrientation()
+    }
+
+    @Test
+    fun testUrlInput() {
+        // Prepare
+        gotoBrowserScreen()
+
+        onView(withId(R.id.display_url)).perform(click())
+
+        // Test - Should be SCREEN_ORIENTATION_PORTRAIT when the url fragment is presented
+        assertEquals(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, activityTestRule.activity.requestedOrientation)
+
+        Espresso.pressBack()
+        Espresso.pressBack()
+
+        assertEquals(ActivityInfo.SCREEN_ORIENTATION_USER, activityTestRule.activity.requestedOrientation)
+
+        assertBackToHomeOrientation()
+    }
+
+    private fun assertBackToHomeOrientation() {
+        Espresso.pressBack()
+        assertEquals(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, activityTestRule.activity.requestedOrientation)
+    }
+
+    private fun gotoBrowserScreen() {
+        onView(withId(R.id.home_fragment_fake_input))
+                .perform(click())
+        onView(withId(R.id.url_edit)).check(matches(isDisplayed()))
+        onView(withId(R.id.url_edit)).perform(replaceText(TARGET_URL_SITE_1))
+        onView(withId(R.id.url_edit)).perform(pressKey(KeyEvent.KEYCODE_ENTER))
+    }
+}
