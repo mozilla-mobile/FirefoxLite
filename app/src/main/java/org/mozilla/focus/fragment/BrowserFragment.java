523152c7ac9a6ad0e5bb545aefa6e73bcbb94fef
Roger <pharspher@gmail.com>
2019-05-08 19:05:38 +0800

    Avoid potential class-cast exception in production version

--- app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
+++ app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
@@ -54,6 +54,7 @@ import android.widget.Toast;
 import com.airbnb.lottie.LottieAnimationView;
 
 import org.jetbrains.annotations.NotNull;
+import org.mozilla.focus.BuildConfig;
 import org.mozilla.focus.Inject;
 import org.mozilla.focus.R;
 import org.mozilla.focus.activity.MainActivity;
@@ -1120,13 +1121,21 @@ public class BrowserFragment extends LocaleAwareFragment implements View.OnClick
         }
     }
 
-    private PortraitStateModel getPortraitStateModel() throws ClassCastException {
+    private PortraitStateModel getPortraitStateModel() {
         Activity activity = getActivity();
         if (activity == null) {
             return null;
         }
 
-        return ((MainActivity) activity).getPortraitStateModel();
+        if (activity instanceof MainActivity) {
+            return ((MainActivity) activity).getPortraitStateModel();
+        } else {
+            if (BuildConfig.DEBUG) {
+                throw new IllegalStateException("Only MainActivity has portrait state model");
+            } else {
+                return null;
+            }
+        }
     }
 
     private void hideFindInPage() {
