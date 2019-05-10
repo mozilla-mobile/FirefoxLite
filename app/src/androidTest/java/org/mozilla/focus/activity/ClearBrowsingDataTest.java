e16204018beb72e9df69110bafa1e1f624dda63a
Conjuror <kuang.con@gmail.com>
2019-05-10 11:14:44 +0800

    remove clear browsing data test to observe if clear cache test passes without toast blocker

--- app/src/androidTest/java/org/mozilla/focus/activity/ClearBrowsingDataTest.java
+++ app/src/androidTest/java/org/mozilla/focus/activity/ClearBrowsingDataTest.java
@@ -6,6 +6,7 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -50,6 +51,7 @@ public class ClearBrowsingDataTest {
      * 6. Tap clear data button
      * 7. Check toast message "browsing data cleared" displayed
      */
+    @Ignore
     @Test
     public void clearBrowsingData() {
 
