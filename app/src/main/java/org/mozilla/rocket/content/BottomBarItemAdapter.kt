6b4cff6f1dc8c81425923dc8c5fe5192b4d71772
Evan Lin <yanghsin.lin@gmail.com>
2019-05-16 19:58:55 +0800

    Revert "fix Kotlin JVM 1.8 compatible issue"
    This reverts commit 8d62f9f815ca1946e04719c75ecc09ba160272e7.

--- app/src/main/java/org/mozilla/rocket/content/BottomBarItemAdapter.kt
+++ app/src/main/java/org/mozilla/rocket/content/BottomBarItemAdapter.kt
@@ -211,10 +211,7 @@ class BottomBarItemAdapter(
         object DARK : Theme(buttonColorResId = R.color.home_bottom_button)
     }
 
-    data class ItemData(val type: Int) {
-        // TODO: workaround to fix the kotlin and JVM 1.8 compatible issue: https://youtrack.jetbrains.com/issue/KT-31027
-        override fun hashCode(): Int = type
-    }
+    data class ItemData(val type: Int)
 
     companion object {
         const val TYPE_TAB_COUNTER = 0
