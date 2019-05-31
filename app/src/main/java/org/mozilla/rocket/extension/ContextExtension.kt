0432c22ab0ec7de926761fe310c6ae403643a932
proevan <yanghsin.lin@gmail.com>
2019-05-31 18:42:10 +0800

    Created MenuDialog that set up menu bottom bar and display menu items

---
+++ app/src/main/java/org/mozilla/rocket/extension/ContextExtension.kt
@@ -0,0 +1,22 @@
+package org.mozilla.rocket.extension
+
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.support.v4.app.FragmentActivity
+
+
+fun Context.toActivity(): Activity = when {
+    this is Activity -> this
+    this is ContextWrapper -> this.baseContext.toActivity()
+    else -> error("context is not a Activity")
+}
+
+fun Context.toFragmentActivity(): FragmentActivity =
+        this.toActivity().let {
+            if (it is FragmentActivity) {
+                it
+            } else {
+                error("context is not a FragmentActivity")
+            }
+        }
