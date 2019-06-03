f5b7e1c8c9164048c2625096acc63829926bf5c3
Benjamin Cheng <chengwc@gmail.com>
2019-06-03 16:55:07 +0800

    Remove default news language setting from UI layer

--- app/src/main/java/org/mozilla/rocket/content/news/NewsViewModel.kt
+++ app/src/main/java/org/mozilla/rocket/content/news/NewsViewModel.kt
@@ -8,19 +8,12 @@ import org.mozilla.lite.partner.NewsItem
 import org.mozilla.lite.partner.Repository
 import org.mozilla.rocket.content.Result
 import org.mozilla.rocket.content.news.data.NewsCategory
-import org.mozilla.rocket.content.news.data.NewsLanguage
 import org.mozilla.rocket.content.news.data.NewsSettingsRepository
 import javax.inject.Inject
 
 class NewsViewModel @Inject constructor(
     private val loadNewsCategoryUseCase: LoadNewsCategoryUseCase
 ) : ViewModel() {
-
-    companion object {
-        private const val DEFAULT_LANGUAGE = "english"
-    }
-
-    var language: String = DEFAULT_LANGUAGE
 
     private var newCategoryResult: MediatorLiveData<Result<LoadNewsCategoryByLangResult>> = loadNewsCategoryUseCase.observe()
 
@@ -34,11 +27,11 @@ class NewsViewModel @Inject constructor(
     lateinit var newsSettingsRepository: NewsSettingsRepository
 
     init {
-        updateCategory("english")
+        updateCategory()
     }
 
-    private fun updateCategory(input: String) {
-        val param = LoadNewsCategoryByLangParameter(input)
+    private fun updateCategory() {
+        val param = LoadNewsCategoryByLangParameter()
         loadNewsCategoryUseCase.execute(param)
     }
 
@@ -67,26 +60,6 @@ class NewsViewModel @Inject constructor(
     }
 
     fun loadMore(category: String) {
-        useCaseMap[category]?.execute(LoadNewsParameter(category, language))
-    }
-
-    fun getSupportLanguages(): LiveData<List<NewsLanguage>> {
-        return newsSettingsRepository.getLanguages()
-    }
-
-    fun getUserPreferenceLanguage(): LiveData<NewsLanguage> {
-        return newsSettingsRepository.getUserPreferenceLanguage()
-    }
-
-    fun setUserPreferenceLanguage(language: NewsLanguage) {
-        newsSettingsRepository.setUserPreferenceLanguage(language)
-    }
-
-    fun getCategoriesByLanguage(language: String): LiveData<List<NewsCategory>> {
-        return newsSettingsRepository.getCategoriesByLanguage(language)
-    }
-
-    fun setUserPreferenceCategories(language: String, userPreferenceCategories: List<NewsCategory>) {
-        newsSettingsRepository.setUserPreferenceCategories(language, userPreferenceCategories)
+        useCaseMap[category]?.execute(LoadNewsParameter(category))
     }
 }
