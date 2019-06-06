794e3d0dd287fa112acded4f02752ae026cdb473
Nevin Chen <cnevinchen@gmail.com>
2019-06-06 16:58:54 +0800

    Add unit test for content portal

--- app/src/test/java/org/mozilla/rocket/content/ContentPortalViewTest.kt
+++ app/src/test/java/org/mozilla/rocket/content/ContentPortalViewTest.kt
@@ -1,46 +1,74 @@
 package org.mozilla.rocket.content
 
+import android.support.v4.app.FragmentActivity
+import android.view.LayoutInflater
+import android.view.View
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.mozilla.focus.R
+import org.mozilla.rocket.content.portal.ContentFeature
+import org.mozilla.rocket.content.portal.ContentPortalView
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+@Config(manifest = Config.NONE)
+@RunWith(RobolectricTestRunner::class)
 class ContentPortalViewTest {
+
+    private lateinit var contentPortalView: ContentPortalView
+
+    private lateinit var activity: FragmentActivity
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        activity = Robolectric.buildActivity(FragmentActivity::class.java).create().start().resume().get()
+        contentPortalView =
+            LayoutInflater.from(activity).inflate(R.layout.content_portal, null) as ContentPortalView
+        activity.setContentView(contentPortalView)
+    }
 
     @Test
     fun `Swipe up or tap the arrow to show content portal panel`() {
+
+        assert(contentPortalView.visibility == View.GONE)
+
+        contentPortalView.show(false)
+        assert(contentPortalView.visibility == View.VISIBLE)
+        assert(ContentPortalViewState.isOpened())
+
+        contentPortalView.hide()
+        assert(contentPortalView.visibility == View.GONE)
+        assert(!ContentPortalViewState.isOpened())
     }
 
     @Test
-    fun `Load 30 the latest news when open the panel`() {
+    fun `when there's News in RemoteConfig, we show NewsTabFragment`() {
+        val mockFeature = mock(ContentFeature::class.java)
+        contentPortalView.contentFeature = mockFeature
+        `when`(mockFeature.hasNews()).thenReturn(true)
+        contentPortalView.onAttachedToWindow()
+        assert(activity.supportFragmentManager.findFragmentByTag(ContentPortalView.TAG_NEWS_FRAGMENT) != null)
     }
 
     @Test
-    fun `Load more automatically at the bottom of the list`() {
+    fun `when there's no News in RemoteConfig, we show EcTabFragment`() {
+        contentPortalView.onAttachedToWindow()
+        assert(activity.supportFragmentManager.findFragmentByTag(ContentPortalView.TAG_CONTENT_FRAGMENT) != null)
     }
 
     @Test
-    fun `Open a new tab & back to previous page`() {
-    }
+    fun `Remember to re-open Content Portal once opend`() {
+        ContentPortalViewState.lastOpened()
+        contentPortalView.onResume()
+        assert(contentPortalView.visibility == View.VISIBLE)
 
-    @Test
-    fun `(No any content in cache)`() {
-    }
-
-    @Test
-    fun `Open panel if having content in cache & check new news`() {
-    }
-
-    @Test
-    fun `Refresh the list automatically (Clear old news and show new news)`() {
-    }
-
-    @Test
-    fun `When at the top of the list, swipe down   the list then show half panel`() {
-    }
-
-    @Test
-    fun `No support half panel when there is no content`() {
-    }
-
-    @Test
-    fun `Press back then close the panel`() {
+        ContentPortalViewState.reset()
+        contentPortalView.onResume()
+        assert(contentPortalView.visibility == View.GONE)
     }
 }
