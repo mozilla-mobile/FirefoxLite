39268011549c75754b2d0a0fca82938aaa472544
Nevin Chen <cnevinchen@gmail.com>
2019-06-13 14:59:57 +0800

    Add Dagger

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -37,6 +37,7 @@ object Versions {
     const val fabric_plugin = "1.25.1"
     const val fastlane_screengrab = "1.2.0"
     const val jraska_falcon = "2.0.1"
+    const val dagger = "2.17"
 }
 
 object SystemEnv {
