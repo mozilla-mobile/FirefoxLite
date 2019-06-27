e18a5a0b4aa0cdd6e3bec9b35bc28bb3be6a2ccc
Roger <pharspher@gmail.com>
2019-06-27 15:07:37 +0800

    [InAppUpdate] Add notification channel for low priority notification

--- app/src/main/java/org/mozilla/focus/notification/NotificationId.java
+++ app/src/main/java/org/mozilla/focus/notification/NotificationId.java
@@ -10,6 +10,7 @@ public class NotificationId {
     public static final int LOVE_FIREFOX = 1001;            // in app promotion: Love Firefox
     public static final int DEFAULT_BROWSER = 1002;         // in app promotion: Set Default Browser
     public static final int PRIVACY_POLICY_UPDATE = 1003;   // in app notification
+    public static final int IN_APP_UPDATE = 1004;           // in app update progress
     public static final int RELOCATE_SERVICE = 2000;        // For file download
     public static final int FIREBASE_AD_HOC = 3000;         // For push notification
     public static final int PRIVATE_MODE = 4000;            // Keeps PrivateModeActivity alive
