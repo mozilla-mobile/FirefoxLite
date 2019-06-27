8788b237b54eb7191f2da185ff6fb4af15047f3b
Roger <pharspher@gmail.com>
2019-06-27 14:06:55 +0800

    Update string resources

--- app/src/main/java/org/mozilla/rocket/appupdate/InAppUpdateViewDelegate.kt
+++ app/src/main/java/org/mozilla/rocket/appupdate/InAppUpdateViewDelegate.kt
@@ -9,6 +9,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AlertDialog
 import android.view.View
 import android.widget.Toast
+import org.mozilla.focus.R
 import org.mozilla.focus.activity.MainActivity
 
 class InAppUpdateViewDelegate(
@@ -35,15 +36,21 @@ class InAppUpdateViewDelegate(
     }
 
     override fun showInAppUpdateInstallPrompt(action: () -> Unit) {
-        // TODO: String resource needed
-        Snackbar.make(snackBarAnchor, "Update downloaded", Snackbar.LENGTH_LONG)
-                .setAction("Update") { action.invoke() }
-                .show()
+        Snackbar.make(
+                snackBarAnchor,
+                activity.getString(R.string.update_to_latest_app_snack_bar_message),
+                Snackbar.LENGTH_LONG
+        ).setAction(activity.getString(R.string.update_to_latest_app_snack_bar_update)) {
+            action.invoke()
+        }.show()
     }
 
     override fun showInAppUpdateDownloadStartHint() {
-        // TODO: String resource needed
-        Toast.makeText(activity, "Download start", Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+                activity,
+                activity.getString(R.string.update_to_latest_app_toast),
+                Toast.LENGTH_SHORT
+        ).show()
     }
 
     override fun closeOnInAppUpdateDenied() {
