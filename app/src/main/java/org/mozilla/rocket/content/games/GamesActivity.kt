0b5dcafe7baa502b69e01ab7ace12633a11f6ead
proevan <yanghsin.lin@gmail.com>
2019-07-31 17:19:40 +0800

    [Games Groundwork] add GamesActivity with ViewPager's FragmentPagerAdapter

---
+++ app/src/main/java/org/mozilla/rocket/content/games/GamesActivity.kt
@@ -0,0 +1,41 @@
+package org.mozilla.rocket.content.games
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import kotlinx.android.synthetic.main.activity_games.games_tabs
+import kotlinx.android.synthetic.main.activity_games.view_pager
+import org.mozilla.focus.R
+import org.mozilla.rocket.content.games.adapter.GameTabsAdapter
+
+class GamesActivity : FragmentActivity() {
+
+    private lateinit var adapter: GameTabsAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_games)
+        initViewPager()
+        initTabLayout()
+    }
+
+    private fun initViewPager() {
+        adapter = GameTabsAdapter(supportFragmentManager, this)
+        view_pager.apply {
+            adapter = this@GamesActivity.adapter
+        }
+    }
+
+    private fun initTabLayout() {
+        games_tabs.setupWithViewPager(view_pager)
+    }
+
+    class BrowserGamesFragment : Fragment()
+    class PremiumGamesFragment : Fragment()
+
+    companion object {
+        fun getStartIntent(context: Context) = Intent(context, GamesActivity::class.java)
+    }
+}
---
+++ app/src/main/java/org/mozilla/rocket/content/games/adapter/GameTabsAdapter.kt
@@ -0,0 +1,37 @@
+package org.mozilla.rocket.content.games.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import org.mozilla.focus.R
+import org.mozilla.rocket.content.games.GamesActivity
+
+class GameTabsAdapter(fm: FragmentManager, activity: FragmentActivity) : FragmentPagerAdapter(fm) {
+
+    private val resource = activity.resources
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> GamesActivity.BrowserGamesFragment()
+            1 -> GamesActivity.PremiumGamesFragment()
+            else -> throw IndexOutOfBoundsException("position: $position")
+        }
+    }
+
+    override fun getCount(): Int = GAMES_TAB_COUNT
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        val resId = when (position) {
+            0 -> R.string.games_tab_title_browser_games
+            1 -> R.string.games_tab_title_premium_games
+            else -> throw IndexOutOfBoundsException("position: $position")
+        }
+
+        return resource.getString(resId)
+    }
+
+    companion object {
+        const val GAMES_TAB_COUNT = 2
+    }
+}
