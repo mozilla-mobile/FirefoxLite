022c863f0433e29e5ee3712115c10a9885b1744f
proevan <yanghsin.lin@gmail.com>
2019-07-31 17:45:56 +0800

    [Games Groundwork] bind browser games data from GamesRepo with view

--- app/src/main/java/org/mozilla/rocket/content/games/BrowserGamesFragment.kt
+++ app/src/main/java/org/mozilla/rocket/content/games/BrowserGamesFragment.kt
@@ -5,15 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_games.list
 import org.mozilla.focus.R
 import org.mozilla.rocket.adapter.AdapterDelegatesManager
 import org.mozilla.rocket.adapter.DelegateAdapter
+import org.mozilla.rocket.content.games.adapter.CarouselBanner
+import org.mozilla.rocket.content.games.adapter.CarouselBannerAdapterDelegate
+import org.mozilla.rocket.content.games.adapter.GameCategory
+import org.mozilla.rocket.content.games.adapter.GameCategoryAdapterDelegate
 
 class BrowserGamesFragment : Fragment() {
 
+    private lateinit var gamesViewModel: GamesViewModel
     private lateinit var adapter: DelegateAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        gamesViewModel = ViewModelProviders.of(requireActivity(), GamesViewModelFactory.INSTANCE).get(GamesViewModel::class.java)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_games, container, false)
@@ -22,15 +34,25 @@ class BrowserGamesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRecyclerView()
+        bindListData()
     }
 
     private fun initRecyclerView() {
         adapter = DelegateAdapter(
-            AdapterDelegatesManager()
+            AdapterDelegatesManager().apply {
+                add(CarouselBanner::class, R.layout.item_carousel_banner, CarouselBannerAdapterDelegate())
+                add(GameCategory::class, R.layout.item_game_category, GameCategoryAdapterDelegate())
+            }
         )
         list.apply {
             adapter = this@BrowserGamesFragment.adapter
             layoutManager = LinearLayoutManager(context)
         }
+    }
+
+    private fun bindListData() {
+        gamesViewModel.browserGamesItems.observe(this@BrowserGamesFragment, Observer {
+            adapter.setData(it)
+        })
     }
 }
--- app/src/main/java/org/mozilla/rocket/content/games/GamesViewModel.kt
+++ app/src/main/java/org/mozilla/rocket/content/games/GamesViewModel.kt
@@ -2,12 +2,41 @@ package org.mozilla.rocket.content.games
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.mozilla.rocket.adapter.DelegateAdapter
+import org.mozilla.rocket.content.games.data.GamesRepo
 
-class GamesViewModel : ViewModel() {
+class GamesViewModel(
+    private val gamesRepo: GamesRepo
+) : ViewModel() {
 
     val browserGamesState = MutableLiveData<State>().apply { value = State.Idle }
     val browserGamesItems = MutableLiveData<List<DelegateAdapter.UIModel>>()
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        launchDataLoad {
+            browserGamesItems.value = gamesRepo.getFakeData()
+        }
+    }
+
+    private fun launchDataLoad(block: suspend () -> Unit): Job {
+        return viewModelScope.launch {
+            try {
+                browserGamesState.value = State.Loading
+                block()
+            } catch (t: Throwable) {
+                browserGamesState.value = State.Error(t)
+            } finally {
+                browserGamesState.value = State.Idle
+            }
+        }
+    }
 
     sealed class State {
         object Idle : State()
