fba947484a345af70a366082034efd186c48d76c
proevan <yanghsin.lin@gmail.com>
2019-07-31 17:02:46 +0800

    [Games Groundwork] create DelegateAdapter for multiple viewTypes usage

---
+++ app/src/main/java/org/mozilla/rocket/adapter/DelegateAdapter.kt
@@ -0,0 +1,49 @@
+package org.mozilla.rocket.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
+
+class DelegateAdapter(
+    private val delegatesManager: AdapterDelegatesManager
+) : RecyclerView.Adapter<DelegateAdapter.ViewHolder>() {
+
+    private var data = mutableListOf<UIModel>()
+
+    fun setData(data: List<UIModel>) {
+        this.data.clear()
+        this.data.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+            delegatesManager.onCreateViewHolder(parent, viewType)
+
+    override fun getItemViewType(position: Int): Int =
+            delegatesManager.getItemViewType(data[position])
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        delegatesManager.onBindViewHolder(holder, data[position])
+    }
+
+    abstract class ViewHolder(view: View) : RecyclerView.ViewHolder(view), LayoutContainer {
+        abstract fun bind(uiModel: UIModel)
+    }
+
+    abstract class UIModel
+}
+
+interface AdapterDelegate {
+    fun inflateView(parent: ViewGroup, layoutId: Int): View =
+            LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
+
+    fun onCreateViewHolder(view: View): DelegateAdapter.ViewHolder
+
+    fun onBindViewHolder(uiModel: DelegateAdapter.UIModel, position: Int, holder: DelegateAdapter.ViewHolder) {
+        holder.bind(uiModel)
+    }
+}
