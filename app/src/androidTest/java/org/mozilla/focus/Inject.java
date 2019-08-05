955405da3d45ceb770b1eb1536e9cffdf266065b
proevan <yanghsin.lin@gmail.com>
2019-08-05 14:18:46 +0800

    [Remove Inject.java] remove unused methods from AndroidTest's Inject.java

--- app/src/androidTest/java/org/mozilla/focus/Inject.java
+++ app/src/androidTest/java/org/mozilla/focus/Inject.java
@@ -5,50 +5,22 @@
 
 package org.mozilla.focus;
 
-import androidx.lifecycle.ViewModelProviders;
-import androidx.room.Room;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
-import androidx.test.InstrumentationRegistry;
-import androidx.fragment.app.FragmentActivity;
 import android.view.View;
 import android.view.animation.Animation;
 
-import org.mozilla.focus.persistence.BookmarksDatabase;
+import androidx.room.Room;
+import androidx.test.InstrumentationRegistry;
+
 import org.mozilla.focus.persistence.TabsDatabase;
-import org.mozilla.focus.repository.BookmarkRepository;
 import org.mozilla.focus.utils.AppConstants;
-import org.mozilla.focus.utils.Browsers;
 import org.mozilla.focus.utils.RemoteConfigConstants;
-import org.mozilla.focus.utils.Settings;
-import org.mozilla.rocket.chrome.BottomBarViewModel;
-import org.mozilla.rocket.chrome.BottomBarViewModelFactory;
-import org.mozilla.rocket.chrome.ChromeViewModel;
-import org.mozilla.rocket.chrome.ChromeViewModelFactory;
-import org.mozilla.rocket.chrome.MenuViewModel;
-import org.mozilla.rocket.chrome.MenuViewModelFactory;
-import org.mozilla.rocket.chrome.PrivateBottomBarViewModel;
-import org.mozilla.rocket.chrome.PrivateBottomBarViewModelFactory;
-import org.mozilla.rocket.content.news.NewsViewModel;
-import org.mozilla.rocket.content.news.NewsViewModelFactory;
-import org.mozilla.rocket.download.DownloadIndicatorViewModel;
-import org.mozilla.rocket.download.DownloadInfoRepository;
-import org.mozilla.rocket.download.DownloadInfoViewModel;
-import org.mozilla.rocket.download.DownloadViewModelFactory;
-import org.mozilla.rocket.helper.StorageHelper;
-import org.mozilla.rocket.privately.PrivateMode;
-import org.mozilla.rocket.urlinput.GlobalDataSource;
-import org.mozilla.rocket.urlinput.LocaleDataSource;
-import org.mozilla.rocket.urlinput.QuickSearchRepository;
-import org.mozilla.rocket.urlinput.QuickSearchViewModel;
-import org.mozilla.rocket.urlinput.QuickSearchViewModelFactory;
 
 public class Inject {
-
-    private static boolean sIsNewCreated = true;
 
     private static final String TOP_SITES = "[{\"id\":-1,\"url\":\"file:\\/\\/\\/android_asset\\/gpl.html\",\"title\":\"Sample Top Site\",\"favicon\":\"ic_youtube.png\",\"viewCount\":20,\"lastViewTimestamp\":1517196818119},{\"id\":-3,\"url\":\"http:\\/\\/m.tribunnews.com\\/\",\"title\":\"Tribunnews\",\"favicon\":\"ic_tribunnews.png\",\"viewCount\":18,\"lastViewTimestamp\":1517196818119},{\"id\":-5,\"url\":\"https:\\/\\/m.tokopedia.com\\/\",\"title\":\"Tokopedia\",\"favicon\":\"ic_tokopedia.png\",\"viewCount\":16,\"lastViewTimestamp\":1517196818119},{\"id\":-4,\"url\":\"https:\\/\\/m.facebook.com\\/\",\"title\":\"Facebook\",\"favicon\":\"ic_facebook.png\",\"viewCount\":14,\"lastViewTimestamp\":1517196818119},{\"id\":-8,\"url\":\"https:\\/\\/m.bukalapak.com\\/\",\"title\":\"Bukalapak\",\"favicon\":\"ic_bukalapak.png\",\"viewCount\":12,\"lastViewTimestamp\":1517196818119},{\"id\":-6,\"url\":\"http:\\/\\/m.liputan6.com\\/\",\"title\":\"Liputan6\",\"favicon\":\"ic_liputan6.png\",\"viewCount\":10,\"lastViewTimestamp\":1517196818119},{\"id\":-7,\"url\":\"http:\\/\\/www.kompas.com\\/\",\"title\":\"Kompas\",\"favicon\":\"ic_kompas.png\",\"viewCount\":8,\"lastViewTimestamp\":1517196818119},{\"id\":-9,\"url\":\"https:\\/\\/m.kapanlagi.com\\/\",\"title\":\"Kapanlagi\",\"favicon\":\"ic_kapanlagi.png\",\"viewCount\":6,\"lastViewTimestamp\":1517196818119}]";
 
@@ -105,14 +77,6 @@ public class Inject {
         StrictMode.setVmPolicy(vmPolicyBuilder.build());
     }
 
-    public static final boolean getActivityNewlyCreatedFlag() {
-        return sIsNewCreated;
-    }
-
-    public static void setActivityNewlyCreatedFlag() {
-        // Do nothing in testing
-    }
-
     public static boolean isUnderEspressoTest() {
         return true;
     }
@@ -120,56 +84,6 @@ public class Inject {
 
     public static RemoteConfigConstants.SURVEY getDefaultFeatureSurvey() {
         return RemoteConfigConstants.SURVEY.VPN_RECOMMENDER;
-    }
-
-    public static DownloadInfoRepository provideDownloadInfoRepository() {
-        //TODO inject data source, ex production DB or mock DB here
-        return DownloadInfoRepository.getInstance();
-    }
-
-    public static DownloadIndicatorViewModel obtainDownloadIndicatorViewModel(FragmentActivity activity) {
-        DownloadViewModelFactory factory = DownloadViewModelFactory.getInstance();
-        return ViewModelProviders.of(activity, factory).get(DownloadIndicatorViewModel.class);
-    }
-
-    public static DownloadInfoViewModel obtainDownloadInfoViewModel(FragmentActivity activity) {
-        DownloadViewModelFactory factory = DownloadViewModelFactory.getInstance();
-        return ViewModelProviders.of(activity, factory).get(DownloadInfoViewModel.class);
-    }
-
-    private static QuickSearchRepository provideQuickSearchRepository(Context context) {
-        // TODO add mock data source
-        return QuickSearchRepository.getInstance(GlobalDataSource.getInstance(context), LocaleDataSource.getInstance(context));
-    }
-
-    public static QuickSearchViewModel obtainQuickSearchViewModel(FragmentActivity activity) {
-        QuickSearchViewModelFactory factory = new QuickSearchViewModelFactory(provideQuickSearchRepository(activity.getApplicationContext()));
-        return ViewModelProviders.of(activity, factory).get(QuickSearchViewModel.class);
-    }
-
-    public static BottomBarViewModel obtainBottomBarViewModel(FragmentActivity activity) {
-        BottomBarViewModelFactory factory = BottomBarViewModelFactory.getInstance();
-        return ViewModelProviders.of(activity, factory).get(BottomBarViewModel.class);
-    }
-
-    public static MenuViewModel obtainMenuViewModel(FragmentActivity activity) {
-        MenuViewModelFactory factory = MenuViewModelFactory.getInstance();
-        return ViewModelProviders.of(activity, factory).get(MenuViewModel.class);
-    }
-
-    public static PrivateBottomBarViewModel obtainPrivateBottomBarViewModel(FragmentActivity activity) {
-        PrivateBottomBarViewModelFactory factory = PrivateBottomBarViewModelFactory.getInstance();
-        return ViewModelProviders.of(activity, factory).get(PrivateBottomBarViewModel.class);
-    }
-
-    public static ChromeViewModel obtainChromeViewModel(FragmentActivity activity) {
-        Settings settings = Settings.getInstance(activity);
-        BookmarkRepository bookmarkRepo = BookmarkRepository.getInstance(BookmarksDatabase.getInstance(activity));
-        PrivateMode privateMode = PrivateMode.getInstance(activity);
-        Browsers Browsers = new Browsers(activity, "http://mozilla.org");
-        StorageHelper storageHelper = new StorageHelper(activity);
-        ChromeViewModelFactory factory = ChromeViewModelFactory.getInstance(settings, bookmarkRepo, privateMode, Browsers, storageHelper);
-        return ViewModelProviders.of(activity, factory).get(ChromeViewModel.class);
     }
 
     public static void startAnimation(View view, Animation animation) {
