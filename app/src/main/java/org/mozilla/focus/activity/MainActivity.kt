c1ba6c837dfa5f59f98a09207ff837ab5366a43e
proevan <yanghsin.lin@gmail.com>
2019-08-05 13:51:51 +0800

    [Remove Inject.java] remove sIsNewCreated from Inject.java

--- app/src/main/java/org/mozilla/focus/activity/MainActivity.kt
+++ app/src/main/java/org/mozilla/focus/activity/MainActivity.kt
@@ -33,7 +33,6 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import kotlinx.android.synthetic.main.activity_main.container
-import org.mozilla.focus.Inject
 import org.mozilla.focus.R
 import org.mozilla.focus.download.DownloadInfoManager
 import org.mozilla.focus.fragment.BrowserFragment
@@ -231,8 +230,8 @@ class MainActivity : BaseActivity(),
     }
 
     private fun checkAndRunPromotion(promotionModel: PromotionModel) {
-        if (Inject.getActivityNewlyCreatedFlag()) {
-            Inject.setActivityNewlyCreatedFlag()
+        if (shouldRunPromotion) {
+            shouldRunPromotion = false
             PromotionPresenter.runPromotion(this, promotionModel)
         }
     }
@@ -751,5 +750,8 @@ class MainActivity : BaseActivity(),
 
         const val REQUEST_CODE_IN_APP_UPDATE = 1024
         const val ACTION_INSTALL_IN_APP_UPDATE = "action_install_in_app_update"
+
+        @JvmField
+        var shouldRunPromotion = true
     }
 }
