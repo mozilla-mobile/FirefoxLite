f3bbec066572ed0bc0283218e956a19f16ca836b
Benjamin Cheng <chengwc@gmail.com>
2019-08-07 10:59:33 +0800

    Add shopping search result page

---
+++ app/src/main/java/org/mozilla/rocket/shopping/search/ui/ShoppingSearchActivity.kt
@@ -0,0 +1,24 @@
+package org.mozilla.rocket.shopping.search.ui
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.mozilla.focus.R
+
+class ShoppingSearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_search)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, ShoppingSearchResultTabFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+    companion object {
+        fun getStartIntent(context: Context) = Intent(context, ShoppingSearchActivity::class.java)
+    }
+}
