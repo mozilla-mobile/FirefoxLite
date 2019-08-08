897086f37187ed495583d90314a779bd69af7f3c
Benjamin Cheng <chengwc@gmail.com>
2019-08-08 18:08:44 +0800

    Integrate the shopping search result sites with mock data

--- app/src/main/java/org/mozilla/rocket/shopping/search/ui/ShoppingSearchResultTabFragment.kt
+++ app/src/main/java/org/mozilla/rocket/shopping/search/ui/ShoppingSearchResultTabFragment.kt
@@ -5,13 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_shopping_search_result_tab.shopping_search_tabs
 import kotlinx.android.synthetic.main.fragment_shopping_search_result_tab.view_pager
 import org.mozilla.focus.R
+import org.mozilla.rocket.shopping.search.data.ShoppingSearchSiteRepository
+import org.mozilla.rocket.shopping.search.ui.ShoppingSearchTabsAdapter.TabItem
 
 class ShoppingSearchResultTabFragment : Fragment() {
 
+    private val viewModelFactory: ShoppingSearchResultViewModel.Factory by lazy {
+        ShoppingSearchResultViewModel.Factory(ShoppingSearchSiteRepository())
+    }
+    private val viewModel by lazy {
+        ViewModelProviders.of(this, viewModelFactory).get(ShoppingSearchResultViewModel::class.java)
+    }
     private val safeArgs: ShoppingSearchResultTabFragmentArgs by navArgs()
     private val searchKeyword by lazy { safeArgs.searchKeyword }
 
@@ -23,10 +33,20 @@ class ShoppingSearchResultTabFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         initViewPager()
         initTabLayout()
+
+        viewModel.search(searchKeyword)
     }
 
     private fun initViewPager() {
-        view_pager.adapter = ShoppingSearchTabsAdapter(childFragmentManager)
+        viewModel.uiModel.observe(this, Observer { uiModel ->
+            val tabItems = uiModel.sites.map { site ->
+                TabItem(
+                    ShoppingSearchResultContentFragment.newInstance(site.searchUrl),
+                    site.title
+                )
+            }
+            view_pager.adapter = ShoppingSearchTabsAdapter(childFragmentManager, tabItems)
+        })
     }
 
     private fun initTabLayout() {
