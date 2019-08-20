aa3fb824964c57ac6e5bca769e4626d92505d972
Benjamin Cheng <chengwc@gmail.com>
2019-08-20 17:03:12 +0800

    Move the bottom bar from the content tab fragment to its own activity

--- app/src/main/java/org/mozilla/rocket/content/common/ui/ContentTabActivity.kt
+++ app/src/main/java/org/mozilla/rocket/content/common/ui/ContentTabActivity.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.LiveData
@@ -17,10 +18,15 @@ import org.mozilla.focus.download.DownloadInfoManager
 import org.mozilla.focus.navigation.ScreenNavigator
 import org.mozilla.focus.urlinput.UrlInputFragment
 import org.mozilla.focus.utils.Constants
+import org.mozilla.focus.utils.IntentUtils
 import org.mozilla.focus.utils.SafeIntent
+import org.mozilla.rocket.chrome.BottomBarItemAdapter
 import org.mozilla.rocket.chrome.ChromeViewModel
 import org.mozilla.rocket.content.appComponent
 import org.mozilla.rocket.content.getViewModel
+import org.mozilla.rocket.content.view.BottomBar
+import org.mozilla.rocket.extension.nonNullObserve
+import org.mozilla.rocket.extension.switchFrom
 import org.mozilla.rocket.landing.NavigationModel
 import org.mozilla.rocket.landing.OrientationState
 import org.mozilla.rocket.landing.PortraitStateModel
@@ -38,12 +44,17 @@ class ContentTabActivity : BaseActivity(),
     @Inject
     lateinit var chromeViewModelCreator: Lazy<ChromeViewModel>
 
+    @Inject
+    lateinit var bottomBarViewModelCreator: Lazy<ContentTabBottomBarViewModel>
+
     private var sessionManager: SessionManager? = null
     private val portraitStateModel = PortraitStateModel()
     private lateinit var chromeViewModel: ChromeViewModel
     private lateinit var tabViewProvider: PrivateTabViewProvider
     private lateinit var screenNavigator: ScreenNavigator
     private lateinit var uiMessageReceiver: BroadcastReceiver
+    private lateinit var bottomBarItemAdapter: BottomBarItemAdapter
+    private lateinit var bottomBar: BottomBar
     private lateinit var snackBarContainer: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,6 +70,8 @@ class ContentTabActivity : BaseActivity(),
         setContentView(R.layout.activity_content_tab)
         snackBarContainer = findViewById(R.id.container)
         makeStatusBarTransparent()
+        bottomBar = findViewById(R.id.bottom_bar)
+        setupBottomBar(bottomBar)
 
         initBroadcastReceivers()
 
@@ -91,6 +104,16 @@ class ContentTabActivity : BaseActivity(),
     override fun onDestroy() {
         super.onDestroy()
         sessionManager?.destroy()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            bottomBar.visibility = View.GONE
+        } else {
+            bottomBar.visibility = View.VISIBLE
+        }
     }
 
     override fun applyLocale() {}
@@ -157,6 +180,38 @@ class ContentTabActivity : BaseActivity(),
             throw RuntimeException("ContentTabActivity should never show Redeem")
         }
         TODO("ContentTabActivity should never show Redeem")
+    }
+
+    private fun setupBottomBar(bottomBar: BottomBar) {
+        bottomBar.setOnItemClickListener(object : BottomBar.OnItemClickListener {
+            override fun onItemClick(type: Int, position: Int) {
+                when (type) {
+                    BottomBarItemAdapter.TYPE_BACK -> onBackPressed()
+                    BottomBarItemAdapter.TYPE_REFRESH -> chromeViewModel.refreshOrStop.call()
+                    BottomBarItemAdapter.TYPE_SHARE -> chromeViewModel.share.call()
+                    BottomBarItemAdapter.TYPE_OPEN_IN_NEW_TAB -> {
+                        startActivity(
+                            IntentUtils.createInternalOpenUrlIntent(
+                                this@ContentTabActivity,
+                                chromeViewModel.currentUrl.value,
+                                true
+                            )
+                        )
+                    }
+                    else -> throw IllegalArgumentException("Unhandled bottom bar item, type: $type")
+                }
+            }
+        })
+        bottomBarItemAdapter = BottomBarItemAdapter(bottomBar, BottomBarItemAdapter.Theme.PrivateMode)
+        val bottomBarViewModel = getViewModel(bottomBarViewModelCreator)
+        bottomBarViewModel.items.nonNullObserve(this) {
+            bottomBarItemAdapter.setItems(it)
+        }
+
+        chromeViewModel.isRefreshing.switchFrom(bottomBarViewModel.items)
+            .observe(this, Observer { bottomBarItemAdapter.setRefreshing(it == true) })
+        chromeViewModel.canGoForward.switchFrom(bottomBarViewModel.items)
+            .observe(this, Observer { bottomBarItemAdapter.setCanGoForward(it == true) })
     }
 
     private fun handleIntent(intent: Intent?) {
