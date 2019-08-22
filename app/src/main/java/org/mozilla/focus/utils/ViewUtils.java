6f1e4196401f88263b73485a50a06613c6f8e7f1
maliu <max@mxli.us>
2019-08-22 14:21:53 +0800

    [Bug #144] Apply ime learning flag only in private mode

--- app/src/main/java/org/mozilla/focus/utils/ViewUtils.java
+++ app/src/main/java/org/mozilla/focus/utils/ViewUtils.java
@@ -7,7 +7,6 @@ package org.mozilla.focus.utils;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.pm.ActivityInfo;
 import android.graphics.Rect;
 import android.os.Build;
 import android.support.annotation.IdRes;
@@ -15,6 +14,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
+import android.support.v13.view.inputmethod.EditorInfoCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
 import android.view.Gravity;
@@ -31,7 +31,7 @@ public class ViewUtils {
 
     public final static int SYSTEM_UI_VISIBILITY_NONE = -1;
 
-    public static final int IME_FLAG_NO_PERSONALIZED_LEARNING = 0x01000000;
+    public static final int IME_FLAG_NO_PERSONALIZED_LEARNING = EditorInfoCompat.IME_FLAG_NO_PERSONALIZED_LEARNING;
 
 
     public static void showKeyboard(View view) {
