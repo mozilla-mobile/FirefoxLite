bb28d26302ce7bec79af4496bce348439bf6e621
Kami <yichen@mozilla.com>
2019-08-23 17:08:52 +0800

    [ShoppingTab] Add Deal list page

--- app/src/main/java/org/mozilla/rocket/content/ecommerce/DealFragment.kt
+++ app/src/main/java/org/mozilla/rocket/content/ecommerce/DealFragment.kt
@@ -8,7 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.Lazy
-import kotlinx.android.synthetic.main.fragment_coupon.*
+import kotlinx.android.synthetic.main.fragment_coupon.spinner
+import kotlinx.android.synthetic.main.fragment_deal.*
 import org.mozilla.focus.R
 import org.mozilla.rocket.adapter.AdapterDelegatesManager
 import org.mozilla.rocket.adapter.DelegateAdapter
@@ -20,13 +21,13 @@ import org.mozilla.rocket.content.ecommerce.adapter.RunwayAdapterDelegate
 import org.mozilla.rocket.content.getActivityViewModel
 import javax.inject.Inject
 
-class CouponFragment : Fragment() {
+class DealFragment : Fragment() {
 
     @Inject
     lateinit var shoppingViewModelCreator: Lazy<ShoppingViewModel>
 
     private lateinit var shoppingViewModel: ShoppingViewModel
-    private lateinit var couponAdapter: DelegateAdapter
+    private lateinit var dealAdapter: DelegateAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         appComponent().inject(this)
@@ -35,37 +36,37 @@ class CouponFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_coupon, container, false)
+        return inflater.inflate(R.layout.fragment_deal, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initCoupons()
+        initDeals()
         bindListData()
         bindPageState()
     }
 
-    private fun initCoupons() {
-        couponAdapter = DelegateAdapter(
+    private fun initDeals() {
+        dealAdapter = DelegateAdapter(
             AdapterDelegatesManager().apply {
                 add(Runway::class, R.layout.item_runway_list, RunwayAdapterDelegate(shoppingViewModel))
                 add(Coupon::class, R.layout.item_coupon, CouponAdapterDelegate(shoppingViewModel))
             }
         )
-        content_coupons.apply {
-            adapter = couponAdapter
+        content_deals.apply {
+            adapter = dealAdapter
             layoutManager = LinearLayoutManager(context)
         }
     }
 
     private fun bindListData() {
-        shoppingViewModel.couponItems.observe(this@CouponFragment, Observer {
-            couponAdapter.setData(it)
+        shoppingViewModel.couponItems.observe(this@DealFragment, Observer {
+            dealAdapter.setData(it)
         })
     }
 
     private fun bindPageState() {
-        shoppingViewModel.isDataLoading.observe(this@CouponFragment, Observer {
+        shoppingViewModel.isDataLoading.observe(this@DealFragment, Observer {
             spinner.visibility = if (it) View.VISIBLE else View.GONE
         })
     }
