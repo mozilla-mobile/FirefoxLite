c975ce9356fc8199c0025fbbc54b9950ab32c79d
Benjamin Cheng <chengwc@gmail.com>
2019-08-23 16:46:36 +0800

    Move AppBar to ContentTabActivity

--- app/src/main/java/org/mozilla/rocket/content/common/ui/ContentTabFragment.kt
+++ app/src/main/java/org/mozilla/rocket/content/common/ui/ContentTabFragment.kt
@@ -1,144 +1,33 @@
 package org.mozilla.rocket.content.common.ui
 
-import android.Manifest
-import android.content.Context
-import android.content.pm.PackageManager
-import android.content.res.Configuration
-import android.net.Uri
 import android.os.Bundle
-import android.os.Parcelable
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.ValueCallback
-import android.webkit.WebChromeClient
 import android.webkit.WebView
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
-import com.google.android.material.snackbar.Snackbar
 import dagger.Lazy
 import org.mozilla.focus.R
-import org.mozilla.focus.download.EnqueueDownloadTask
 import org.mozilla.focus.locale.LocaleAwareFragment
-import org.mozilla.focus.menu.WebContextMenu
-import org.mozilla.focus.utils.ViewUtils
-import org.mozilla.focus.web.HttpAuthenticationDialogBuilder
-import org.mozilla.focus.widget.AnimatedProgressBar
 import org.mozilla.focus.widget.BackKeyHandleable
-import org.mozilla.permissionhandler.PermissionHandle
-import org.mozilla.permissionhandler.PermissionHandler
 import org.mozilla.rocket.chrome.ChromeViewModel
 import org.mozilla.rocket.content.appComponent
 import org.mozilla.rocket.content.getActivityViewModel
 import org.mozilla.rocket.tabs.Session
 import org.mozilla.rocket.tabs.SessionManager
-import org.mozilla.rocket.tabs.TabView
-import org.mozilla.rocket.tabs.TabViewClient
-import org.mozilla.rocket.tabs.TabViewEngineSession
 import org.mozilla.rocket.tabs.TabsSessionProvider
 import org.mozilla.rocket.tabs.utils.TabUtil
-import org.mozilla.rocket.tabs.web.Download
-import org.mozilla.urlutils.UrlUtils
 import javax.inject.Inject
-
-private const val SITE_GLOBE = 0
-private const val SITE_LOCK = 1
-private const val ACTION_DOWNLOAD = 0
 
 class ContentTabFragment : LocaleAwareFragment(), BackKeyHandleable {
 
     @Inject
     lateinit var chromeViewModelCreator: Lazy<ChromeViewModel>
 
-    private lateinit var permissionHandler: PermissionHandler
     private lateinit var sessionManager: SessionManager
     private lateinit var tabSession: Session
-    private lateinit var observer: Observer
     private lateinit var chromeViewModel: ChromeViewModel
-
-    private lateinit var browserContainer: ViewGroup
-    private lateinit var videoContainer: ViewGroup
     private lateinit var tabViewSlot: ViewGroup
-    private lateinit var displayUrlView: TextView
-    private lateinit var progressView: AnimatedProgressBar
-    private lateinit var siteIdentity: ImageView
-    private lateinit var toolbarRoot: ViewGroup
-
-    private var systemVisibility = ViewUtils.SYSTEM_UI_VISIBILITY_NONE
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        permissionHandler = PermissionHandler(object : PermissionHandle {
-            override fun doActionDirect(permission: String?, actionId: Int, params: Parcelable?) {
-
-                this@ContentTabFragment.context?.also {
-                    val download = params as Download
-
-                    if (PackageManager.PERMISSION_GRANTED ==
-                        ContextCompat.checkSelfPermission(it, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    ) {
-                        // We do have the permission to write to the external storage. Proceed with the download.
-                        queueDownload(download)
-                    }
-                } ?: run {
-                    Log.e("ContentTabFragment", "No context to use, abort callback onDownloadStart")
-                }
-            }
-
-            fun actionDownloadGranted(parcelable: Parcelable?) {
-                val download = parcelable as Download
-                queueDownload(download)
-            }
-
-            override fun doActionGranted(permission: String?, actionId: Int, params: Parcelable?) {
-                actionDownloadGranted(params)
-            }
-
-            override fun doActionSetting(permission: String?, actionId: Int, params: Parcelable?) {
-                actionDownloadGranted(params)
-            }
-
-            override fun doActionNoPermission(
-                permission: String?,
-                actionId: Int,
-                params: Parcelable?
-            ) {
-            }
-
-            override fun makeAskAgainSnackBar(actionId: Int): Snackbar {
-                activity?.also {
-                    return PermissionHandler.makeAskAgainSnackBar(
-                        this@ContentTabFragment,
-                        it.findViewById(R.id.container),
-                        R.string.permission_toast_storage
-                    )
-                }
-                throw IllegalStateException("No Activity to show Snackbar.")
-            }
-
-            override fun permissionDeniedToast(actionId: Int) {
-                Toast.makeText(getContext(), R.string.permission_toast_storage_deny, Toast.LENGTH_LONG).show()
-            }
-
-            override fun requestPermissions(actionId: Int) {
-                this@ContentTabFragment.requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), actionId)
-            }
-
-            private fun queueDownload(download: Download?) {
-                activity?.let { activity ->
-                    download?.let {
-                        EnqueueDownloadTask(activity, it, displayUrlView.text.toString()).execute()
-                    }
-                }
-            }
-        })
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         appComponent().inject(this)
@@ -153,19 +42,7 @@ class ContentTabFragment : LocaleAwareFragment(), BackKeyHandleable {
     override fun onViewCreated(view: View, savedState: Bundle?) {
         super.onViewCreated(view, savedState)
 
-        displayUrlView = view.findViewById(R.id.display_url)
-        siteIdentity = view.findViewById(R.id.site_identity)
-        browserContainer = view.findViewById(R.id.browser_container)
-        videoContainer = view.findViewById(R.id.video_container)
         tabViewSlot = view.findViewById(R.id.tab_view_slot)
-        progressView = view.findViewById(R.id.progress)
-
-        view.findViewById<View>(R.id.appbar).setOnApplyWindowInsetsListener { v, insets ->
-            (v.layoutParams as LinearLayout.LayoutParams).topMargin = insets.systemWindowInsetTop
-            insets
-        }
-
-        toolbarRoot = view.findViewById(R.id.toolbar_root)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -183,10 +60,6 @@ class ContentTabFragment : LocaleAwareFragment(), BackKeyHandleable {
             }
         }
 
-        observer = Observer(this)
-        sessionManager.register(observer)
-        tabSession.register(observer)
-
         observeChromeAction()
 
         arguments?.getString(EXTRA_URL)?.apply { loadUrl(this) }
@@ -202,35 +75,11 @@ class ContentTabFragment : LocaleAwareFragment(), BackKeyHandleable {
         sessionManager.pause()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        tabSession.unregister(observer)
-        sessionManager.unregister(observer)
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            toolbarRoot.visibility = View.GONE
-        } else {
-            toolbarRoot.visibility = View.VISIBLE
-        }
-    }
-
     override fun applyLocale() {
         // We create and destroy a new WebView here to force the internal state of WebView to know
         // about the new language. See issue #666.
         val unneeded = WebView(context)
         unneeded.destroy()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        permissionHandler.onRequestPermissionsResult(context, requestCode, permissions, grantResults)
     }
 
     override fun onBackPressed(): Boolean {
@@ -270,8 +119,6 @@ class ContentTabFragment : LocaleAwareFragment(), BackKeyHandleable {
 
     private fun loadUrl(url: String) {
         if (url.isNotBlank()) {
-            displayUrlView.text = url
-
             tabSession.engineSession?.tabView?.apply {
                 loadUrl(url)
             }
@@ -290,191 +137,6 @@ class ContentTabFragment : LocaleAwareFragment(), BackKeyHandleable {
             return ContentTabFragment().apply {
                 arguments = args
             }
-        }
-    }
-
-    class Observer(val fragment: ContentTabFragment) : SessionManager.Observer, Session.Observer {
-        override fun updateFailingUrl(url: String?, updateFromError: Boolean) {
-            // do nothing, exist for interface compatibility only.
-        }
-
-        override fun handleExternalUrl(url: String?): Boolean {
-            // do nothing, exist for interface compatibility only.
-            return false
-        }
-
-        override fun onShowFileChooser(
-            es: TabViewEngineSession,
-            filePathCallback: ValueCallback<Array<Uri>>?,
-            fileChooserParams: WebChromeClient.FileChooserParams?
-        ): Boolean {
-            // do nothing, exist for interface compatibility only.
-            return false
-        }
-
-        var callback: TabView.FullscreenCallback? = null
-        var session: Session? = null
-
-        override fun onSessionAdded(session: Session, arguments: Bundle?) {
-        }
-
-        override fun onProgress(session: Session, progress: Int) {
-            fragment.progressView.progress = progress
-        }
-
-        override fun onTitleChanged(session: Session, title: String?) {
-            fragment.chromeViewModel.onFocusedTitleChanged(title)
-            session.let {
-                if (fragment.displayUrlView.text.toString() != it.url) {
-                    fragment.displayUrlView.text = it.url
-                }
-            }
-        }
-
-        override fun onLongPress(session: Session, hitTarget: TabView.HitTarget) {
-            fragment.activity?.let {
-                WebContextMenu.show(true,
-                    it,
-                    DownloadCallback(fragment, session.url),
-                    hitTarget)
-            }
-        }
-
-        override fun onEnterFullScreen(callback: TabView.FullscreenCallback, view: View?) {
-            with(fragment) {
-                browserContainer.visibility = View.INVISIBLE
-                videoContainer.visibility = View.VISIBLE
-                videoContainer.addView(view)
-
-                // Switch to immersive mode: Hide system bars other UI controls
-                systemVisibility = ViewUtils.switchToImmersiveMode(activity)
-            }
-        }
-
-        override fun onExitFullScreen() {
-            with(fragment) {
-                browserContainer.visibility = View.VISIBLE
-                videoContainer.visibility = View.INVISIBLE
-                videoContainer.removeAllViews()
-
-                if (systemVisibility != ViewUtils.SYSTEM_UI_VISIBILITY_NONE) {
-                    ViewUtils.exitImmersiveMode(systemVisibility, activity)
-                }
-            }
-
-            callback?.fullScreenExited()
-            callback = null
-
-            // WebView gets focus, but unable to open the keyboard after exit Fullscreen for Android 7.0+
-            // We guess some component in WebView might lock focus
-            // So when user touches the input text box on Webview, it will not trigger to open the keyboard
-            // It may be a WebView bug.
-            // The workaround is clearing WebView focus
-            // The WebView will be normal when it gets focus again.
-            // If android change behavior after, can remove this.
-            session?.engineSession?.tabView?.let { if (it is WebView) it.clearFocus() }
-        }
-
-        override fun onUrlChanged(session: Session, url: String?) {
-            fragment.chromeViewModel.onFocusedUrlChanged(url)
-            if (!UrlUtils.isInternalErrorURL(url)) {
-                fragment.displayUrlView.text = url
-            }
-        }
-
-        override fun onLoadingStateChanged(session: Session, loading: Boolean) {
-            if (loading) {
-                fragment.chromeViewModel.onPageLoadingStarted()
-            } else {
-                fragment.chromeViewModel.onPageLoadingStopped()
-            }
-        }
-
-        override fun onSecurityChanged(session: Session, isSecure: Boolean) {
-            val level = if (isSecure) SITE_LOCK else SITE_GLOBE
-            fragment.siteIdentity.setImageLevel(level)
-        }
-
-        override fun onSessionCountChanged(count: Int) {
-            fragment.chromeViewModel.onTabCountChanged(count)
-            if (count == 0) {
-                session?.unregister(this)
-            } else {
-                session = fragment.sessionManager.focusSession
-                session?.register(this)
-            }
-        }
-
-        override fun onDownload(
-            session: Session,
-            download: mozilla.components.browser.session.Download
-        ): Boolean {
-            val activity = fragment.activity
-            if (activity == null || !activity.lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
-                return false
-            }
-
-            val d = Download(
-                download.url,
-                download.fileName,
-                download.userAgent,
-                "",
-                download.contentType,
-                download.contentLength!!,
-                false
-            )
-            fragment.permissionHandler.tryAction(
-                fragment,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                ACTION_DOWNLOAD,
-                d
-            )
-            return true
-        }
-
-        override fun onHttpAuthRequest(
-            callback: TabViewClient.HttpAuthCallback,
-            host: String?,
-            realm: String?
-        ) {
-            val builder = HttpAuthenticationDialogBuilder.Builder(fragment.activity, host, realm)
-                .setOkListener { _, _, username, password -> callback.proceed(username, password) }
-                .setCancelListener { callback.cancel() }
-                .build()
-
-            builder.createDialog()
-            builder.show()
-        }
-
-        override fun onNavigationStateChanged(session: Session, canGoBack: Boolean, canGoForward: Boolean) {
-            fragment.chromeViewModel.onNavigationStateChanged(canGoBack, canGoForward)
-        }
-
-        override fun onFocusChanged(session: Session?, factor: SessionManager.Factor) {
-            fragment.chromeViewModel.onFocusedUrlChanged(session?.url)
-            fragment.chromeViewModel.onFocusedTitleChanged(session?.title)
-            if (session != null) {
-                val canGoBack = fragment.sessionManager.focusSession?.canGoBack ?: false
-                val canGoForward = fragment.sessionManager.focusSession?.canGoForward ?: false
-                fragment.chromeViewModel.onNavigationStateChanged(canGoBack, canGoForward)
-            }
-        }
-    }
-
-    private class DownloadCallback(val fragment: ContentTabFragment, val refererUrl: String?) : org.mozilla.rocket.tabs.web.DownloadCallback {
-        override fun onDownloadStart(download: Download) {
-            fragment.activity?.let {
-                if (!it.lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
-                    return
-                }
-            }
-
-            fragment.permissionHandler.tryAction(
-                fragment,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                ACTION_DOWNLOAD,
-                download
-            )
         }
     }
 }
