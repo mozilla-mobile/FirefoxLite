638137613c6dd157857ee38aaae0ddb1c36d6d62
Benjamin Cheng <chengwc@gmail.com>
2019-08-25 22:25:02 +0800

    Apply ContentTab in the shopping search

---
+++ app/src/main/java/org/mozilla/rocket/shopping/search/ui/ShoppingSearchBottomBarViewModel.kt
@@ -0,0 +1,34 @@
+package org.mozilla.rocket.shopping.search.ui
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.mozilla.rocket.chrome.BottomBarItemAdapter
+import org.mozilla.rocket.chrome.BottomBarItemAdapter.ItemData
+
+class ShoppingSearchBottomBarViewModel : ViewModel() {
+    val items = MutableLiveData<List<ItemData>>()
+
+    init {
+        refresh()
+    }
+
+    fun refresh() {
+        val configuredItems = DEFAULT_CONTENT_TAB_BOTTOM_BAR_ITEMS
+        items.value.let { currentValue ->
+            if (configuredItems != currentValue) {
+                items.value = configuredItems
+            }
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        val DEFAULT_CONTENT_TAB_BOTTOM_BAR_ITEMS = listOf(
+            ItemData(BottomBarItemAdapter.TYPE_PRIVATE_HOME),
+            ItemData(BottomBarItemAdapter.TYPE_REFRESH),
+            ItemData(BottomBarItemAdapter.TYPE_DELETE),
+            ItemData(BottomBarItemAdapter.TYPE_NEXT),
+            ItemData(BottomBarItemAdapter.TYPE_SHARE)
+        )
+    }
+}
