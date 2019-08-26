33a38e17946870b8c975de6108afc0fc0427122f
mxlius <441664+mxlius@users.noreply.github.com>
2019-08-26 17:19:52 +0800

    Merge pull request #3824 from mozilla-tw/max/bug/mango/144
    [Bug #144] Apply ime learning flag only in private mode

--- app/src/main/java/org/mozilla/focus/urlinput/UrlInputFragment.kt
+++ app/src/main/java/org/mozilla/focus/urlinput/UrlInputFragment.kt
@@ -96,7 +96,6 @@ class UrlInputFragment : Fragment(), UrlInputContract.View, View.OnClickListener
         }
 
         urlView.setOnFilterListener(::onFilter)
-        urlView.imeOptions = urlView.imeOptions or ViewUtils.IME_FLAG_NO_PERSONALIZED_LEARNING
 
         initByArguments()
 
@@ -171,6 +170,9 @@ class UrlInputFragment : Fragment(), UrlInputContract.View, View.OnClickListener
             clearView.visibility = if (TextUtils.isEmpty(url)) View.GONE else View.VISIBLE
         }
         allowSuggestion = args?.getBoolean(ARGUMENT_ALLOW_SUGGESTION, true) ?: false
+        if (args?.getBoolean(ARGUMENT_BOOLEAN_PRIVATE_MODE, false) == true) {
+            urlView.imeOptions = urlView.imeOptions or ViewUtils.IME_FLAG_NO_PERSONALIZED_LEARNING
+        }
     }
 
     private fun onSuggestionClicked(tag: CharSequence) {
@@ -320,6 +322,7 @@ class UrlInputFragment : Fragment(), UrlInputContract.View, View.OnClickListener
         private const val ARGUMENT_URL = "url"
         private const val ARGUMENT_PARENT_FRAGMENT = "parent_frag_tag"
         private const val ARGUMENT_ALLOW_SUGGESTION = "allow_suggestion"
+        private const val ARGUMENT_BOOLEAN_PRIVATE_MODE = "boolean_private_mode"
         private const val REQUEST_THROTTLE_THRESHOLD = 300
 
         /**
@@ -327,11 +330,12 @@ class UrlInputFragment : Fragment(), UrlInputContract.View, View.OnClickListener
          * fake url bar view.
          */
         @JvmStatic
-        fun create(url: String?, parentFragmentTag: String?, allowSuggestion: Boolean): UrlInputFragment {
+        fun create(url: String?, parentFragmentTag: String?, allowSuggestion: Boolean, privateMode: Boolean = false): UrlInputFragment {
             val arguments = Bundle()
             arguments.putString(ARGUMENT_URL, url)
             arguments.putString(ARGUMENT_PARENT_FRAGMENT, parentFragmentTag)
             arguments.putBoolean(ARGUMENT_ALLOW_SUGGESTION, allowSuggestion)
+            arguments.putBoolean(ARGUMENT_BOOLEAN_PRIVATE_MODE, privateMode)
 
             val fragment = UrlInputFragment()
             fragment.arguments = arguments
