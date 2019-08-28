81b941ed7b6993cdd979c34140385b0f3faa08ec
Benjamin Cheng <chengwc@gmail.com>
2019-08-28 16:06:34 +0800

    Apply ContentTabHelper into game mode

--- app/src/main/java/org/mozilla/rocket/content/games/ui/GameModeActivity.kt
+++ app/src/main/java/org/mozilla/rocket/content/games/ui/GameModeActivity.kt
@@ -1,43 +1,104 @@
 package org.mozilla.rocket.content.games.ui
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.webkit.WebView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import dagger.Lazy
+import kotlinx.android.synthetic.main.activity_game_mode.*
 import org.mozilla.focus.R
 import org.mozilla.focus.activity.BaseActivity
-import org.mozilla.focus.utils.SafeIntent
+import org.mozilla.rocket.chrome.ChromeViewModel
+import org.mozilla.rocket.content.appComponent
+import org.mozilla.rocket.content.common.ui.ContentTabFragment
+import org.mozilla.rocket.content.common.ui.ContentTabHelper
+import org.mozilla.rocket.content.common.ui.ContentTabViewContract
+import org.mozilla.rocket.content.getViewModel
+import org.mozilla.rocket.privately.PrivateTabViewProvider
+import org.mozilla.rocket.tabs.Session
+import org.mozilla.rocket.tabs.SessionManager
+import org.mozilla.rocket.tabs.TabViewProvider
+import org.mozilla.rocket.tabs.TabsSessionProvider
+import javax.inject.Inject
 
-class GameModeActivity : BaseActivity() {
+class GameModeActivity : BaseActivity(), TabsSessionProvider.SessionHost, ContentTabViewContract {
 
-    private lateinit var webview: WebView
+    @Inject
+    lateinit var chromeViewModelCreator: Lazy<ChromeViewModel>
 
-    @SuppressLint("SetJavaScriptEnabled")
+    private lateinit var chromeViewModel: ChromeViewModel
+    private lateinit var tabViewProvider: TabViewProvider
+    private lateinit var sessionManager: SessionManager
+    private lateinit var contentTabHelper: ContentTabHelper
+    private lateinit var contentTabObserver: ContentTabHelper.Observer
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        appComponent().inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_mode)
 
-        webview = findViewById(R.id.webview_game)
-        webview.settings.javaScriptEnabled = true
-        webview.settings.useWideViewPort = true
-        webview.settings.loadWithOverviewMode = true
+        chromeViewModel = getViewModel(chromeViewModelCreator)
+        tabViewProvider = PrivateTabViewProvider(this)
+        sessionManager = SessionManager(tabViewProvider)
 
-        handleIntent(intent)
+        contentTabHelper = ContentTabHelper(this)
+        contentTabHelper.initPermissionHandler()
+
+        contentTabObserver = contentTabHelper.getObserver()
+        sessionManager.register(contentTabObserver)
+
+        chromeViewModel.showUrlInput.value = chromeViewModel.currentUrl.value
+
+        if (savedInstanceState == null) {
+            val url = intent?.extras?.getString(EXTRA_URL) ?: ""
+            val enableTurboMode = intent?.extras?.getBoolean(EXTRA_ENABLE_TURBO_MODE) ?: true
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ContentTabFragment.newInstance(url, enableTurboMode))
+                .commit()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sessionManager.unregister(contentTabObserver)
+        sessionManager.destroy()
     }
 
     override fun applyLocale() = Unit
 
-    private fun handleIntent(intent: Intent?) {
-        val safeIntent = intent?.let { SafeIntent(it) } ?: return
-        webview.loadUrl(safeIntent.getStringExtra(Intent.EXTRA_TEXT))
-    }
+    override fun getSessionManager(): SessionManager = sessionManager
+
+    override fun getHostActivity(): AppCompatActivity = this
+
+    override fun getCurrentSession(): Session? = sessionManager.focusSession
+
+    override fun getChromeViewModel(): ChromeViewModel = chromeViewModel
+
+    override fun getSiteIdentity(): ImageView? = null
+
+    override fun getDisplayUrlView(): TextView? = null
+
+    override fun getProgressBar(): ProgressBar? = null
+
+    override fun getFullScreenGoneViews(): List<View> = emptyList()
+
+    override fun getFullScreenInvisibleViews(): List<View> = listOf(fragment_container)
+
+    override fun getFullScreenContainerView(): ViewGroup = video_container
 
     companion object {
-        fun create(context: Context, linkUrl: String): Intent {
-            val intent = Intent(context, GameModeActivity::class.java)
-            intent.putExtra(Intent.EXTRA_TEXT, linkUrl)
-            return intent
-        }
+        private const val EXTRA_URL = "url"
+        private const val EXTRA_ENABLE_TURBO_MODE = "enable_turbo_mode"
+
+        fun getStartIntent(context: Context, url: String, enableTurboMode: Boolean = false) =
+            Intent(context, GameModeActivity::class.java).also {
+                it.putExtra(EXTRA_URL, url)
+                it.putExtra(EXTRA_ENABLE_TURBO_MODE, enableTurboMode)
+            }
     }
 }
