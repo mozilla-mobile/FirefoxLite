1ce6dc3aa2b27f6df1cd521b59e27dcc432c5404
proevan <yanghsin.lin@gmail.com>
2019-08-28 15:42:11 +0800

    [Fix UI Tests] convert HomeTest.java to HomeTest.kt

--- app/src/androidTest/java/org/mozilla/focus/activity/HomeTest.kt
+++ app/src/androidTest/java/org/mozilla/focus/activity/HomeTest.kt
@@ -3,94 +3,93 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.focus.activity;
+package org.mozilla.focus.activity
 
-import android.content.Context;
-import android.content.Intent;
-import android.util.DisplayMetrics;
+import android.content.Context
+import android.content.Intent
+import android.util.DisplayMetrics
 
-import androidx.annotation.Keep;
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.espresso.action.Tap;
-import androidx.test.espresso.contrib.RecyclerViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.FlakyTest;
-import androidx.test.rule.ActivityTestRule;
+import androidx.annotation.Keep
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.Tap
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.FlakyTest
+import androidx.test.rule.ActivityTestRule
 
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mozilla.focus.R;
-import org.mozilla.focus.helper.SessionLoadedIdlingResource;
-import org.mozilla.focus.history.model.Site;
-import org.mozilla.focus.utils.AndroidTestUtils;
-import org.mozilla.rocket.content.ExtentionKt;
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mozilla.focus.R
+import org.mozilla.focus.helper.SessionLoadedIdlingResource
+import org.mozilla.focus.history.model.Site
+import org.mozilla.focus.utils.AndroidTestUtils
+import org.mozilla.rocket.content.*
 
-import java.util.List;
-
-import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.pressBack;
-import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.mozilla.focus.utils.RecyclerViewTestUtils.atPosition;
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.pressBack
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.hamcrest.Matchers.allOf
+import org.mozilla.focus.utils.RecyclerViewTestUtils.atPosition
 
 @Keep
-@RunWith(AndroidJUnit4.class)
-public class HomeTest {
+@RunWith(AndroidJUnit4::class)
+class HomeTest {
 
     // Defer the startup of the activity cause we want to avoid First Run / Share App / Rate App dialogs
-    @Rule
-    public final ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class, true, false);
-    private SessionLoadedIdlingResource loadingIdlingResource;
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java, true, false)
+    private var loadingIdlingResource: SessionLoadedIdlingResource? = null
 
     @Before
-    public void setUp() {
+    fun setUp() {
         // Set the share preferences and start the activity
-        AndroidTestUtils.beforeTest();
+        AndroidTestUtils.beforeTest()
     }
 
     @Test
     @Ignore("fix this after top sites implemented")
-    public void clickTopSite_loadTopSite() {
+    fun clickTopSite_loadTopSite() {
 
         // Now start the activity
-        activityRule.launchActivity(new Intent());
-        loadingIdlingResource = new SessionLoadedIdlingResource(activityRule.getActivity());
+        activityRule.launchActivity(Intent())
+        loadingIdlingResource = SessionLoadedIdlingResource(activityRule.activity)
 
         // Get test top sites
-        final List<Site> defaultSites = ExtentionKt.appComponent((Context) getApplicationContext())
-                .topSitesRepo().getDefaultSites();
+        val defaultSites = (getApplicationContext() as Context).appComponent()
+                .topSitesRepo().getDefaultSites()
 
         // Check the title of the sample top site is correct
         onView(withId(R.id.main_list))
-                .check(matches(atPosition(0, hasDescendant(withText(defaultSites.get(0).getTitle())))));
+                .check(matches(atPosition(0, hasDescendant(withText(defaultSites!![0].title)))))
 
         // Click and load the sample top site
         // Some intermittent issues happens when performing a single click event, we add a rollback action in case of a long click action
         // is triggered unexpectedly here. i.e. pressBack() can dismiss the popup menu.
         onView(ViewMatchers.withId(R.id.main_list))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click(pressBack())));
+                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click(pressBack())))
 
         // After page loading completes
-        IdlingRegistry.getInstance().register(loadingIdlingResource);
+        IdlingRegistry.getInstance().register(loadingIdlingResource!!)
 
         // Check if the url is displayed correctly
         onView(withId(R.id.display_url))
-                .check(matches(allOf(withText(defaultSites.get(0).getUrl()), isDisplayed())));
+                .check(matches(allOf(withText(defaultSites[0].url), isDisplayed())))
 
         // Always remember to unregister idling resource
-        IdlingRegistry.getInstance().unregister(loadingIdlingResource);
+        IdlingRegistry.getInstance().unregister(loadingIdlingResource!!)
     }
 
     /**
@@ -102,23 +101,23 @@ public class HomeTest {
      */
     @Test
     @Ignore("waiting for coroutine to finish in order to show main_list")
-    public void checkBasicHomeComponents_allAreVisible() {
-        activityRule.launchActivity(new Intent());
+    fun checkBasicHomeComponents_allAreVisible() {
+        activityRule.launchActivity(Intent())
 
         // Check if App Logo is visible
-        onView(withId(R.id.home_fragment_title)).check(matches(isDisplayed()));
+        onView(withId(R.id.home_fragment_title)).check(matches(isDisplayed()))
 
         // Check if home search field is visible
-        onView(withId(R.id.home_fragment_fake_input)).check(matches(isDisplayed()));
+        onView(withId(R.id.home_fragment_fake_input)).check(matches(isDisplayed()))
 
         // Check if top site list is visible
-        onView(withId(R.id.main_list)).check(matches(isDisplayed()));
+        onView(withId(R.id.main_list)).check(matches(isDisplayed()))
 
         // Check if menu button is visible
-        onView(withId(R.id.home_fragment_menu_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.home_fragment_menu_button)).check(matches(isDisplayed()))
 
         // Check if tab tray button is visible
-        onView(withId(R.id.home_fragment_tab_counter)).check(matches(isDisplayed()));
+        onView(withId(R.id.home_fragment_tab_counter)).check(matches(isDisplayed()))
     }
 
     /**
@@ -135,28 +134,28 @@ public class HomeTest {
      */
     @FlakyTest
     @Test
-    public void dismissMenu() {
-        activityRule.launchActivity(new Intent());
+    fun dismissMenu() {
+        activityRule.launchActivity(Intent())
 
         // Tap menu
-        AndroidTestUtils.tapHomeMenuButton();
+        AndroidTestUtils.tapHomeMenuButton()
 
         // Press back
-        Espresso.pressBack();
+        Espresso.pressBack()
 
         // Check menu panel not exist
-        onView(withId(R.id.menu)).check(doesNotExist());
+        onView(withId(R.id.menu)).check(doesNotExist())
 
         // Tap menu
-        AndroidTestUtils.tapHomeMenuButton();
+        AndroidTestUtils.tapHomeMenuButton()
 
         // Tap on blank space above menu
-        final DisplayMetrics displayMetrics = activityRule.getActivity().getResources().getDisplayMetrics();
-        final int displayWidth = displayMetrics.widthPixels;
-        final int displayHeight = displayMetrics.heightPixels;
-        onView(withId(R.id.menu)).perform(AndroidTestUtils.clickXY(displayWidth / 2, -displayHeight / 2, Tap.SINGLE));
+        val displayMetrics = activityRule.activity.resources.displayMetrics
+        val displayWidth = displayMetrics.widthPixels
+        val displayHeight = displayMetrics.heightPixels
+        onView(withId(R.id.menu)).perform(AndroidTestUtils.clickXY(displayWidth / 2, -displayHeight / 2, Tap.SINGLE))
 
         //  Check menu panel not exist
-        onView(withId(R.id.menu)).check(doesNotExist());
+        onView(withId(R.id.menu)).check(doesNotExist())
     }
 }
