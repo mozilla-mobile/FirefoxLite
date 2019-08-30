5f0385b159ea39bce82ea32f54a98811c100da46
Evan Lin <yanghsin.lin@gmail.com>
2019-08-30 18:12:45 +0800

    Implement FxToast for showing custom toast (#173)

---
+++ app/src/main/java/org/mozilla/rocket/widget/FxToast.kt
@@ -0,0 +1,48 @@
+package org.mozilla.rocket.widget
+
+import android.app.Service
+import android.content.Context
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fx_toast.view.text
+import org.mozilla.focus.R
+import java.lang.ref.WeakReference
+
+object FxToast {
+    private var toastWeak: WeakReference<Toast?> = WeakReference(null)
+    private var inflater: LayoutInflater? = null
+
+    fun show(appContext: Context, msg: String, duration: Int = Toast.LENGTH_SHORT) {
+        val lastToast = toastWeak.get()
+        val newToast = if (lastToast != null) {
+            createToast(appContext, duration, customView = lastToast.view.apply { text.text = msg })
+        } else {
+            createToast(appContext, duration, customView = inflateCustomView(appContext, msg))
+        }
+        toastWeak = WeakReference(newToast)
+        lastToast?.cancel()
+        newToast.show()
+    }
+
+    private fun inflateCustomView(appContext: Context, msg: String): View =
+            getLayoutInflater(appContext).inflate(R.layout.fx_toast, null).apply {
+                text.text = msg
+            }
+
+    private fun createToast(appContext: Context, duration: Int, customView: View) = Toast(appContext)
+            .apply {
+                setGravity(Gravity.FILL_HORIZONTAL or Gravity.BOTTOM, 0, 0)
+                view = customView
+                this.duration = duration
+            }
+
+    private fun getLayoutInflater(appContext: Context): LayoutInflater {
+        if (inflater == null) {
+            inflater = appContext.getSystemService(Service.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        }
+
+        return requireNotNull(inflater)
+    }
+}
