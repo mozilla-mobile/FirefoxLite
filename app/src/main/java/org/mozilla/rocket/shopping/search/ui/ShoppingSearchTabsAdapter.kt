f8a4eedcd769a9f4592cc8cee488b00e673c0252
Benjamin Cheng <chengwc@gmail.com>
2019-09-17 16:20:52 +0800

    Fix potential fragment leakage

--- app/src/main/java/org/mozilla/rocket/shopping/search/ui/ShoppingSearchTabsAdapter.kt
+++ app/src/main/java/org/mozilla/rocket/shopping/search/ui/ShoppingSearchTabsAdapter.kt
@@ -1,24 +1,35 @@
 package org.mozilla.rocket.shopping.search.ui
 
-import android.annotation.SuppressLint
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import org.mozilla.rocket.content.common.ui.ContentTabFragment
 
-@SuppressLint("WrongConstant")
+@Suppress("DEPRECATION")
 class ShoppingSearchTabsAdapter(
     fm: FragmentManager,
     private val items: List<TabItem>
-) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+) : FragmentPagerAdapter(fm) {
 
-    override fun getItem(position: Int): Fragment = items[position].fragment
+    override fun getItem(position: Int): Fragment = ContentTabFragment.newInstance(items[position].searchUrl)
 
     override fun getCount(): Int = items.size
 
     override fun getPageTitle(position: Int): CharSequence? = items[position].title
 
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val fragment = super.instantiateItem(container, position) as ContentTabFragment
+        // Force to update the search url since Viewpager may reuse the fragment instance previously instantiated.
+        fragment.arguments?.apply {
+            val item = items[position]
+            putString(ContentTabFragment.EXTRA_URL, item.searchUrl)
+        }
+        return fragment
+    }
+
     data class TabItem(
-        val fragment: Fragment,
-        val title: String
+        val title: String,
+        val searchUrl: String
     )
 }
