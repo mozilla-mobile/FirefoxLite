6897591a809863b0af88918ffb5b2212c258d9c4
Roger <pharspher@gmail.com>
2019-09-20 11:36:12 +0800

    Implement mission-related repository interface

--- app/src/main/java/org/mozilla/rocket/msrp/data/Mission.kt
+++ app/src/main/java/org/mozilla/rocket/msrp/data/Mission.kt
@@ -5,12 +5,13 @@ data class Mission(
     val mid: String,
     val title: String,
     val description: String,
-    val expireDate: Long, // if the mission is expired, it's not showing
     val events: List<String>,
     val important: Boolean,
     val status: Int, // 0:new, 1:joined, 2:redeem-able 3:redeemed
     val endpoint: String, // Use this API to request to join the mission
     val redeem: String?, // Use this API to retrieve the reward
+    val minVersion: Int,
+    val missionType: String,
 
     val missionProgress: MissionProgress
 
@@ -20,9 +21,10 @@ data class Mission(
  *
  * */
 sealed class MissionProgress {
-    class TypeDaily(
+    data class TypeDaily(
         val joinDate: Long?, // the date the user join this mission
         val currentDay: Int?, // number of the total days accomplished
-        val totalDays: Int? // number of the total days needed
+        val totalDays: Int?, // number of the total days needed
+        val message: String?
     ) : MissionProgress()
 }
--- app/src/main/java/org/mozilla/rocket/msrp/domain/LoadMissionsUseCase.kt
+++ app/src/main/java/org/mozilla/rocket/msrp/domain/LoadMissionsUseCase.kt
@@ -1,23 +1,23 @@
 package org.mozilla.rocket.msrp.domain
 
-import org.mozilla.rocket.msrp.data.Mission
 import org.mozilla.rocket.msrp.data.MissionRepository
-import org.mozilla.rocket.msrp.data.RewardServiceException
+import org.mozilla.rocket.msrp.data.UserRepository
+import org.mozilla.rocket.msrp.data.LoadMissionsResult
+import org.mozilla.rocket.msrp.data.Mission
 
-class LoadMissionsUseCase(private val missionRepository: MissionRepository) :
-    UseCase<LoadMissionsUseCaseParameter, LoadMissionsUseCaseResult>() {
+class LoadMissionsUseCase(
+    private val missionRepository: MissionRepository,
+    private val userRepository: UserRepository
+) : UseCase<LoadMissionsUseCaseParameter, LoadMissionsResult>() {
 
     /**
      * Fetch a list of [Mission]s from the server
      *
      * @param [LoadMissionsUseCaseParameter] the mission group endpoint from Firebase Remote Config
      * @return [LoadMissionsUseCaseResult] a list of [Mission]s the user can join or has joined
-     * @throws RewardServiceException.ServerErrorException when there's a server error
-     * @throws RewardServiceException.AuthorizationException when there's a something wrong with authorization
      */
-    override suspend fun execute(parameters: LoadMissionsUseCaseParameter): LoadMissionsUseCaseResult {
-        val missions = missionRepository.fetchMission(parameters.missionGroupURI)
-        return LoadMissionsUseCaseResult(missions)
+    override suspend fun execute(parameters: LoadMissionsUseCaseParameter): LoadMissionsResult {
+        return missionRepository.fetchMission(userRepository.getUserToken())
     }
 }
 
@@ -25,6 +25,4 @@ data class LoadMissionsUseCaseResult(
     val items: List<Mission>
 )
 
-data class LoadMissionsUseCaseParameter(
-    val missionGroupURI: String
-)
+class LoadMissionsUseCaseParameter
