0124b5c8dbd07a524cac888b6d5244f361ddac95
Benjamin Cheng <chengwc@gmail.com>
2019-10-04 14:45:27 +0800

    Do not keep fragment instances in a list and reuse in tab adapter

--- app/src/main/java/org/mozilla/rocket/content/ecommerce/ui/adapter/ShoppingTabsAdapter.kt
+++ app/src/main/java/org/mozilla/rocket/content/ecommerce/ui/adapter/ShoppingTabsAdapter.kt
@@ -3,7 +3,7 @@ package org.mozilla.rocket.content.ecommerce.ui.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
 import org.mozilla.rocket.content.ecommerce.ui.ShoppingViewModel
 
 @Suppress("DEPRECATION")
@@ -11,11 +11,11 @@ class ShoppingTabsAdapter(
     fm: FragmentManager,
     activity: FragmentActivity,
     private val items: List<ShoppingViewModel.ShoppingTabItem>
-) : FragmentPagerAdapter(fm) {
+) : FragmentStatePagerAdapter(fm) {
 
     private val resource = activity.resources
 
-    override fun getItem(position: Int): Fragment = items[position].fragment
+    override fun getItem(position: Int): Fragment = items[position].createFragment()
 
     override fun getCount(): Int = items.size
 
