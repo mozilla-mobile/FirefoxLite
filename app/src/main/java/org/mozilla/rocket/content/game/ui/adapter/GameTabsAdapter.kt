a22fa383d2de861b2cf8d015ea77fc4472d589df
Benjamin Cheng <chengwc@gmail.com>
2019-10-05 17:27:55 +0800

    Do not keep fragment instances in a list and reuse in tab adapter

--- app/src/main/java/org/mozilla/rocket/content/game/ui/adapter/GameTabsAdapter.kt
+++ app/src/main/java/org/mozilla/rocket/content/game/ui/adapter/GameTabsAdapter.kt
@@ -3,7 +3,7 @@ package org.mozilla.rocket.content.game.ui.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
 import org.mozilla.focus.R
 import org.mozilla.rocket.content.game.ui.DownloadGameFragment
 import org.mozilla.rocket.content.game.ui.InstantGameFragment
@@ -13,25 +13,38 @@ class GameTabsAdapter(
     fm: FragmentManager,
     activity: FragmentActivity,
     private val items: List<TabItem> = getDefaultTabs()
-) : FragmentPagerAdapter(fm) {
+) : FragmentStatePagerAdapter(fm) {
 
     private val resource = activity.resources
 
-    override fun getItem(position: Int): Fragment = items[position].fragment
+    override fun getItem(position: Int): Fragment = items[position].createFragment()
 
     override fun getCount(): Int = items.size
 
     override fun getPageTitle(position: Int): CharSequence? = resource.getString(items[position].titleResId)
 
     data class TabItem(
-        val fragment: Fragment,
+        val type: Int,
         val titleResId: Int
-    )
+    ) {
+        fun createFragment(): Fragment {
+            return when (type) {
+                TYPE_INSTANT_GAME_TAB -> InstantGameFragment.newInstance()
+                TYPE_DOWNLOAD_GAME_TAB -> DownloadGameFragment.newInstance()
+                else -> error("Unsupported game tab item type $type")
+            }
+        }
+
+        companion object {
+            const val TYPE_INSTANT_GAME_TAB = 1
+            const val TYPE_DOWNLOAD_GAME_TAB = 2
+        }
+    }
 
     companion object {
         private fun getDefaultTabs(): List<TabItem> = listOf(
-            TabItem(InstantGameFragment.newInstance(), R.string.gaming_vertical_category_1),
-            TabItem(DownloadGameFragment.newInstance(), R.string.gaming_vertical_category_2)
+            TabItem(TabItem.TYPE_INSTANT_GAME_TAB, R.string.gaming_vertical_category_1),
+            TabItem(TabItem.TYPE_DOWNLOAD_GAME_TAB, R.string.gaming_vertical_category_2)
         )
     }
 }
