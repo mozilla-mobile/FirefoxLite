aabd2b455db81a470f580676fd3837d5e3048456
Evan Lin <yanghsin.lin@gmail.com>
2019-10-08 17:11:50 +0800

    make debug countries remote configurable (#514)

--- app/src/main/java/org/mozilla/rocket/debugging/DebugActivity.kt
+++ app/src/main/java/org/mozilla/rocket/debugging/DebugActivity.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_debug.debug_locale_layout
 import kotlinx.android.synthetic.main.activity_debug.debug_locale_text
 import kotlinx.android.synthetic.main.activity_debug.toolbar
+import org.json.JSONArray
 import org.mozilla.focus.R
 import org.mozilla.focus.utils.FirebaseHelper
 import org.mozilla.rocket.preference.stringLiveData
@@ -26,9 +27,6 @@ class DebugActivity : AppCompatActivity() {
         setContentView(R.layout.activity_debug)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        debug_locale_layout.setOnClickListener {
-            showDropDownListDialog(DEBUG_LOCALES)
-        }
 
         preference = getSharedPreferences(PREF_NAME_DEBUG, Context.MODE_PRIVATE)
         initDebugLocale()
@@ -40,13 +38,20 @@ class DebugActivity : AppCompatActivity() {
     }
 
     private fun initDebugLocale() {
+        val debugLocales = getDebugLocales()
         getDebugLocaleLiveData().observe(this, Observer {
             debug_locale_text.text = it
         })
+        debug_locale_layout.setOnClickListener {
+            showDropDownListDialog(debugLocales)
+        }
     }
 
+    private fun getDebugLocales(): List<String> =
+            parseDebugLocalesJson(FirebaseHelper.getFirebase().getRcString(STR_RC_DEBUG_LOCALES))
+
     private fun getDebugLocaleLiveData(): LiveData<String> =
-            preference.stringLiveData(SHARED_PREF_KEY_DEBUG_LOCALE, DEBUG_LOCALES[0])
+            preference.stringLiveData(SHARED_PREF_KEY_DEBUG_LOCALE, DEBUG_DEFAULT_LOCALE)
 
     private fun saveDebugLocale(locale: String) {
         preference.edit().putString(SHARED_PREF_KEY_DEBUG_LOCALE, locale).apply()
@@ -76,15 +81,21 @@ class DebugActivity : AppCompatActivity() {
     }
 
     companion object {
+        private const val STR_RC_DEBUG_LOCALES = "str_debug_locales"
+
         private const val PREF_NAME_DEBUG = "debug_pref"
         private const val SHARED_PREF_KEY_DEBUG_LOCALE = "shared_pref_key_debug_locale"
-        private val DEBUG_LOCALES = listOf(
-            "Default",
-            "Taiwan",
-            "Indonesia",
-            "India"
-        )
+        private const val DEBUG_DEFAULT_LOCALE = "Default"
 
         fun getStartIntent(context: Context) = Intent(context, DebugActivity::class.java)
     }
+}
+
+private fun parseDebugLocalesJson(jsonStr: String): List<String> {
+    if (jsonStr.isEmpty()) {
+        return emptyList()
+    }
+    val jsonArray = JSONArray(jsonStr)
+    return (0 until jsonArray.length())
+            .map { jsonArray.getJSONObject(it).getString("country") }
 }
