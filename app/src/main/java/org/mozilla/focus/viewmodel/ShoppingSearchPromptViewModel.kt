71cd0f32e46b129ce7c42b907df6dc2454326e4c
dimple <dimple@sofasogood.today>
2019-10-24 08:55:24 +0800

    shopping site search prompt visibility issue

--- app/src/main/java/org/mozilla/focus/viewmodel/ShoppingSearchPromptViewModel.kt
+++ app/src/main/java/org/mozilla/focus/viewmodel/ShoppingSearchPromptViewModel.kt
@@ -10,14 +10,53 @@ import org.mozilla.rocket.shopping.search.ui.adapter.ShoppingSiteItem
 class ShoppingSearchPromptViewModel(
     private val getShoppingSiteUseCase: GetShoppingSitesUseCase
 ) : ViewModel() {
-    val isUrlShoppingSite = MutableLiveData<Boolean>()
+    val promptVisibilityState = MutableLiveData<VisibilityState>()
     val matchedShoppingSiteTitle = MutableLiveData<String?>()
     val shoppingSiteList: LiveData<List<ShoppingSiteItem>> = getShoppingSiteUseCase()
 
     val openShoppingSearch = SingleLiveEvent<Unit>()
 
-    fun checkIsShoppingSite(url: String?) {
-        matchedShoppingSiteTitle.value = shoppingSiteList.value?.filter { site -> url?.contains(site.displayUrl, true) == true }?.map { it.title }?.firstOrNull()
-        isUrlShoppingSite.value = matchedShoppingSiteTitle.value != null
+    fun checkShoppingSearchPromptVisibility(url: String?, forwardSiteUrl: String?, previousSiteUrl: String?) {
+        val currentShoppingSiteTitle: String? = getShoppingSiteTitle(url)
+        matchedShoppingSiteTitle.value = currentShoppingSiteTitle
+
+        val isShoppingSite: Boolean = (matchedShoppingSiteTitle.value != null)
+        if (isShoppingSite) {
+            if (currentShoppingSiteTitle != null && currentShoppingSiteTitle.isNotEmpty()) {
+                var hasForwardUrl = false
+                if (forwardSiteUrl != null && forwardSiteUrl.isNotEmpty()) {
+                    hasForwardUrl = true
+                    val forwardShoppingSiteTitle: String? = getShoppingSiteTitle(forwardSiteUrl)
+                    if (forwardShoppingSiteTitle != null && forwardShoppingSiteTitle.equals(currentShoppingSiteTitle, true)) {
+                        promptVisibilityState.value = VisibilityState.Collapsed
+                        return
+                    }
+                }
+
+                if (!hasForwardUrl) {
+                    if (previousSiteUrl != null && previousSiteUrl.isNotEmpty()) {
+                        val previousShoppingSiteTitle: String? = getShoppingSiteTitle(previousSiteUrl)
+                        if (previousShoppingSiteTitle != null && previousShoppingSiteTitle.equals(currentShoppingSiteTitle, true)) {
+                            promptVisibilityState.value = VisibilityState.Collapsed
+                            return
+                        }
+                    }
+                }
+            }
+
+            promptVisibilityState.value = VisibilityState.Expanded
+        } else {
+            promptVisibilityState.value = VisibilityState.Hidden
+        }
+    }
+
+    private fun getShoppingSiteTitle(url: String?): String? {
+        return shoppingSiteList.value?.filter { site -> url?.contains(site.displayUrl, true) == true }?.map { it.title }?.firstOrNull()
+    }
+
+    sealed class VisibilityState {
+        object Hidden : VisibilityState()
+        object Expanded : VisibilityState()
+        object Collapsed : VisibilityState()
     }
 }
