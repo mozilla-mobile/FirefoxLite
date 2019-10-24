cf7dd0ad7e40554cd83e067fc25541a78bdc1b4e
Evan Lin <yanghsin.lin@gmail.com>
2019-10-24 17:38:39 +0800

    show shopping search onboarding depends on remote config (#689)

--- app/src/main/java/org/mozilla/rocket/home/onboarding/domain/ShouldShowShoppingSearchOnboardingUseCase.kt
+++ app/src/main/java/org/mozilla/rocket/home/onboarding/domain/ShouldShowShoppingSearchOnboardingUseCase.kt
@@ -1,9 +1,13 @@
 package org.mozilla.rocket.home.onboarding.domain
 
 import org.mozilla.focus.utils.NewFeatureNotice
+import org.mozilla.rocket.shopping.search.data.ShoppingSearchRepository
 
-class ShouldShowShoppingSearchOnboardingUseCase(private val newFeatureNotice: NewFeatureNotice) {
+class ShouldShowShoppingSearchOnboardingUseCase(
+    private val shoppingSearchRepository: ShoppingSearchRepository,
+    private val newFeatureNotice: NewFeatureNotice
+) {
     operator fun invoke(): Boolean {
-        return !newFeatureNotice.hasHomeShoppingSearchOnboardingShown()
+        return shoppingSearchRepository.isShoppingSearchEnabled() && !newFeatureNotice.hasHomeShoppingSearchOnboardingShown()
     }
 }
