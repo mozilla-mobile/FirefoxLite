2674f9936d54340a115b69ccad5ccb154f7159f7
Benjamin Cheng <chengwc@gmail.com>
2019-10-30 17:36:44 +0800

    Fix unit test - ShoppingSearchRepositoryTest

--- app/src/test/java/org/mozilla/rocket/shopping/search/data/ShoppingSearchRepositoryTest.kt
+++ app/src/test/java/org/mozilla/rocket/shopping/search/data/ShoppingSearchRepositoryTest.kt
@@ -27,7 +27,7 @@ class ShoppingSearchRepositoryTest {
     fun `When it's the first time retrieved shopping site data from remote, it should return the exact same list from remote`() {
         Mockito.`when`(remoteDataSource.getShoppingSites())
             .thenReturn(listOf(
-                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", isEnabled = true)
+                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", true, isEnabled = true)
             ))
         Mockito.`when`(localDataSource.getShoppingSites())
             .thenReturn(emptyList())
@@ -36,7 +36,7 @@ class ShoppingSearchRepositoryTest {
 
         Assert.assertEquals(
             listOf(
-                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", isEnabled = true)
+                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", true, isEnabled = true)
             ),
             LiveDataTestUtil.getValue(shoppingSearchRepository.getShoppingSitesData()))
     }
@@ -45,23 +45,23 @@ class ShoppingSearchRepositoryTest {
     fun `When there is new shopping site data from remote, it should return local result plus new item`() {
         Mockito.`when`(remoteDataSource.getShoppingSites())
             .thenReturn(listOf(
-                ShoppingSite("Bukalapak", "https://www.bukalapak.com/products?utf8=✓&search%5Bkeywords%5D=", "bukalapak.com", isEnabled = true),
-                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", isEnabled = true),
-                ShoppingSite("Tokopedia", "https://www.tokopedia.com/search?st=product&q=", "tokopedia.com", isEnabled = true)
+                ShoppingSite("Bukalapak", "https://www.bukalapak.com/products?utf8=✓&search%5Bkeywords%5D=", "bukalapak.com", true, isEnabled = true),
+                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", true, isEnabled = true),
+                ShoppingSite("Tokopedia", "https://www.tokopedia.com/search?st=product&q=", "tokopedia.com", true, isEnabled = true)
             ))
         Mockito.`when`(localDataSource.getShoppingSites())
             .thenReturn(listOf(
-                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", isEnabled = true),
-                ShoppingSite("Bukalapak", "https://www.bukalapak.com/products?utf8=✓&search%5Bkeywords%5D=", "bukalapak.com", isEnabled = false)
+                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", true, isEnabled = true),
+                ShoppingSite("Bukalapak", "https://www.bukalapak.com/products?utf8=✓&search%5Bkeywords%5D=", "bukalapak.com", true, isEnabled = false)
             ))
 
         shoppingSearchRepository = ShoppingSearchRepository(remoteDataSource, localDataSource)
 
         Assert.assertEquals(
             listOf(
-                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", isEnabled = true),
-                ShoppingSite("Bukalapak", "https://www.bukalapak.com/products?utf8=✓&search%5Bkeywords%5D=", "bukalapak.com", isEnabled = false),
-                ShoppingSite("Tokopedia", "https://www.tokopedia.com/search?st=product&q=", "tokopedia.com", isEnabled = true)
+                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", true, isEnabled = true),
+                ShoppingSite("Bukalapak", "https://www.bukalapak.com/products?utf8=✓&search%5Bkeywords%5D=", "bukalapak.com", true, isEnabled = false),
+                ShoppingSite("Tokopedia", "https://www.tokopedia.com/search?st=product&q=", "tokopedia.com", true, isEnabled = true)
             ),
             LiveDataTestUtil.getValue(shoppingSearchRepository.getShoppingSitesData()))
     }
@@ -70,22 +70,22 @@ class ShoppingSearchRepositoryTest {
     fun `When there is item removed from remote, it should return local result without removed item`() {
         Mockito.`when`(remoteDataSource.getShoppingSites())
             .thenReturn(listOf(
-                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", isEnabled = true),
-                ShoppingSite("Bukalapak", "https://www.bukalapak.com/products?utf8=✓&search%5Bkeywords%5D=", "bukalapak.com", isEnabled = true)
+                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", true, isEnabled = true),
+                ShoppingSite("Bukalapak", "https://www.bukalapak.com/products?utf8=✓&search%5Bkeywords%5D=", "bukalapak.com", true, isEnabled = true)
             ))
         Mockito.`when`(localDataSource.getShoppingSites())
             .thenReturn(listOf(
-                ShoppingSite("Bukalapak", "https://www.bukalapak.com/products?utf8=✓&search%5Bkeywords%5D=", "bukalapak.com", isEnabled = true),
-                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", isEnabled = false),
-                ShoppingSite("Tokopedia", "https://www.tokopedia.com/search?st=product&q=", "tokopedia.com", isEnabled = true)
+                ShoppingSite("Bukalapak", "https://www.bukalapak.com/products?utf8=✓&search%5Bkeywords%5D=", "bukalapak.com", true, isEnabled = true),
+                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", true, isEnabled = false),
+                ShoppingSite("Tokopedia", "https://www.tokopedia.com/search?st=product&q=", "tokopedia.com", true, isEnabled = true)
             ))
 
         shoppingSearchRepository = ShoppingSearchRepository(remoteDataSource, localDataSource)
 
         Assert.assertEquals(
             listOf(
-                ShoppingSite("Bukalapak", "https://www.bukalapak.com/products?utf8=✓&search%5Bkeywords%5D=", "bukalapak.com", isEnabled = true),
-                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", isEnabled = false)
+                ShoppingSite("Bukalapak", "https://www.bukalapak.com/products?utf8=✓&search%5Bkeywords%5D=", "bukalapak.com", true, isEnabled = true),
+                ShoppingSite("Lazada", "https://www.lazada.co.id/catalog/?q=", "lazada.co.id", true, isEnabled = false)
             ),
             LiveDataTestUtil.getValue(shoppingSearchRepository.getShoppingSitesData()))
     }
