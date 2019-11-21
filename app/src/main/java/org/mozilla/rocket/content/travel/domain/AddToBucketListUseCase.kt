5fc7c776dc32a01cf5a781696daad6cc4cdfa55b
Benjamin Cheng <chengwc@gmail.com>
2019-11-21 18:01:50 +0800

    Save image url while adding city to the bucket list

--- app/src/main/java/org/mozilla/rocket/content/travel/domain/AddToBucketListUseCase.kt
+++ app/src/main/java/org/mozilla/rocket/content/travel/domain/AddToBucketListUseCase.kt
@@ -1,19 +1,34 @@
 package org.mozilla.rocket.content.travel.domain
 
+import org.mozilla.rocket.content.Result
 import org.mozilla.rocket.content.travel.data.BucketListCity
 import org.mozilla.rocket.content.travel.data.TravelRepository
 
 class AddToBucketListUseCase(private val travelRepository: TravelRepository) {
 
     suspend operator fun invoke(id: String, name: String) {
-        // TODO: query city imageUrl by id
-        val imageUrl = ""
-        travelRepository.addToBucketList(
-                BucketListCity(
-                    id,
-                    imageUrl,
-                    name
-                )
-        )
+        travelRepository.addToBucketList(BucketListCity(id, getImageUrl(id), name))
+    }
+
+    private suspend fun getImageUrl(id: String): String {
+        val exploreCityResult = travelRepository.getExploreList()
+        if (exploreCityResult is Result.Success) {
+            val apiEntity = exploreCityResult.data
+            for (category in apiEntity.subcategories) {
+                if (category.componentType == BANNER) {
+                    continue
+                }
+                for (item in category.items) {
+                    if (item.description == id) {
+                        return item.image
+                    }
+                }
+            }
+        }
+        return ""
+    }
+
+    companion object {
+        private const val BANNER = "banner"
     }
 }
