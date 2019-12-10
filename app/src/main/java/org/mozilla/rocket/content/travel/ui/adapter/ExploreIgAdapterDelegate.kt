d3929f7ab5d6fb649c868b1b625227efa801bb2c
Kinse Lin <kinse.lin@yarboroughtech.com>
2019-12-10 09:33:12 +0800

    Apply top sites IG icon to travel city detail

--- app/src/main/java/org/mozilla/rocket/content/travel/ui/adapter/ExploreIgAdapterDelegate.kt
+++ app/src/main/java/org/mozilla/rocket/content/travel/ui/adapter/ExploreIgAdapterDelegate.kt
@@ -3,6 +3,8 @@ package org.mozilla.rocket.content.travel.ui.adapter
 import android.view.View
 import kotlinx.android.synthetic.main.item_travel_detail_ig.*
 import org.mozilla.focus.R
+import org.mozilla.focus.utils.TopSitesUtils
+import org.mozilla.icon.FavIconUtils
 import org.mozilla.rocket.adapter.AdapterDelegate
 import org.mozilla.rocket.adapter.DelegateAdapter
 import org.mozilla.rocket.content.travel.ui.TravelCityViewModel
@@ -26,6 +28,14 @@ class ExploreIgViewHolder(
         }
 
         explore_ig_tag.text = itemView.resources.getString(R.string.travel_content_ig_link, exploreIg.title)
+
+        val igIconUri = TopSitesUtils.TOP_SITE_ASSET_PREFIX + ICON_FILE_NAME
+        val resource = FavIconUtils.getBitmapFromUri(itemView.context, igIconUri)
+        explore_ig_icon.setImageBitmap(resource)
+    }
+
+    companion object {
+        internal const val ICON_FILE_NAME = "ic_instagram.png"
     }
 }
 
