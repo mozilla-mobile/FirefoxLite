acd577b1d0d52b9aaeffb21859f9ed2b99878c43
proevan <yanghsin.lin@gmail.com>
2019-12-10 16:20:48 +0800

    add remote config to support content hub item text

--- app/src/main/java/org/mozilla/rocket/home/contenthub/ui/ContentHub.kt
+++ app/src/main/java/org/mozilla/rocket/home/contenthub/ui/ContentHub.kt
@@ -2,12 +2,13 @@ package org.mozilla.rocket.home.contenthub.ui
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.Gravity
 import android.view.View
-import android.widget.FrameLayout
-import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.core.view.MarginLayoutParamsCompat
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.item_content_hub.view.icon
+import kotlinx.android.synthetic.main.item_content_hub.view.red_dot
+import kotlinx.android.synthetic.main.item_content_hub.view.text
 import org.mozilla.focus.R
 import org.mozilla.rocket.extension.dpToPx
 
@@ -21,6 +22,8 @@ class ContentHub : LinearLayout {
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
+    private var showText = false
+
     init {
         orientation = HORIZONTAL
         clipToPadding = false
@@ -28,43 +31,34 @@ class ContentHub : LinearLayout {
         dpToPx(PADDING_IN_DP).let { setPadding(it, it, it, it) }
     }
 
+    fun setShowText(showText: Boolean) {
+        this.showText = showText
+    }
+
     fun setItems(items: List<Item>) {
         removeAllViews()
         items.forEachIndexed { i, item ->
             val isLast = i == items.size - 1
             val isUnread = item.isUnread
-            val itemSize = dpToPx(ITEM_SIZE_IN_DP)
-            addView(
-                FrameLayout(context).apply {
-                    addView(
-                        ImageView(context).apply {
-                            setImageResource(item.iconResId)
-                            scaleType = ImageView.ScaleType.CENTER_INSIDE
-                            setBackgroundResource(R.drawable.bg_content_hub_item)
-                            elevation = dpToPx(ITEM_ELEVATION_IN_DP).toFloat()
-                            setOnClickListener { clickListener?.invoke(item) }
-                        },
-                        FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-                    )
-                    if (isUnread) {
-                        val unreadDotSize = dpToPx(UNREAD_DOT_SIZE_IN_DP)
-                        addView(
-                            View(context).apply {
-                                setBackgroundResource(R.drawable.content_hub_red_dot)
-                                elevation = dpToPx(ITEM_ELEVATION_IN_DP).toFloat()
-                            },
-                            FrameLayout.LayoutParams(unreadDotSize, unreadDotSize).apply {
-                                gravity = Gravity.TOP or Gravity.END
-                            }
-                        )
-                    }
-                },
-                LayoutParams(itemSize, itemSize).apply {
-                    if (!isLast) {
-                        MarginLayoutParamsCompat.setMarginEnd(this, dpToPx(ITEM_MARGIN_IN_DP))
-                    }
+            val itemView = View.inflate(context, R.layout.item_content_hub, null)
+            itemView.icon.apply {
+                setImageResource(item.iconResId)
+                setOnClickListener { clickListener?.invoke(item) }
+            }
+            itemView.text.apply {
+                if (showText) {
+                    setText(item.textResId)
+                    isVisible = true
                 }
-            )
+            }
+            itemView.red_dot.apply {
+                isVisible = isUnread
+            }
+            addView(itemView, LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
+                if (!isLast) {
+                    MarginLayoutParamsCompat.setMarginEnd(this, dpToPx(ITEM_MARGIN_IN_DP))
+                }
+            })
         }
     }
 
@@ -72,18 +66,15 @@ class ContentHub : LinearLayout {
         clickListener = listener
     }
 
-    sealed class Item(open val iconResId: Int, open var isUnread: Boolean) {
-        data class Travel(override val iconResId: Int, override var isUnread: Boolean) : Item(iconResId, isUnread)
-        data class Shopping(override val iconResId: Int, override var isUnread: Boolean) : Item(iconResId, isUnread)
-        data class News(override val iconResId: Int, override var isUnread: Boolean) : Item(iconResId, isUnread)
-        data class Games(override val iconResId: Int, override var isUnread: Boolean) : Item(iconResId, isUnread)
+    sealed class Item(open val iconResId: Int, open val textResId: Int, open var isUnread: Boolean) {
+        data class Travel(override val iconResId: Int, override val textResId: Int, override var isUnread: Boolean) : Item(iconResId, textResId, isUnread)
+        data class Shopping(override val iconResId: Int, override val textResId: Int, override var isUnread: Boolean) : Item(iconResId, textResId, isUnread)
+        data class News(override val iconResId: Int, override val textResId: Int, override var isUnread: Boolean) : Item(iconResId, textResId, isUnread)
+        data class Games(override val iconResId: Int, override val textResId: Int, override var isUnread: Boolean) : Item(iconResId, textResId, isUnread)
     }
 
     companion object {
         private const val PADDING_IN_DP = 12f
-        private const val ITEM_SIZE_IN_DP = 44f
         private const val ITEM_MARGIN_IN_DP = 24f
-        private const val ITEM_ELEVATION_IN_DP = 4f
-        private const val UNREAD_DOT_SIZE_IN_DP = 10f
     }
 }
