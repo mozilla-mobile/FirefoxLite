d294da02bd63dd9f7339e8406be62320d7dafb85
Hart Su <hart.su@yarboroughtech.com>
2019-12-12 12:09:22 +0800

    Replace travel search result header string

--- app/src/main/java/org/mozilla/rocket/content/travel/ui/TravelCitySearchViewModel.kt
+++ app/src/main/java/org/mozilla/rocket/content/travel/ui/TravelCitySearchViewModel.kt
@@ -48,7 +48,7 @@ class TravelCitySearchViewModel(private val searchCityUseCase: SearchCityUseCase
                 if (result is Result.Success && !result.data.result.isEmpty()) {
                     list.add(CitySearchResultCategoryUiModel(R.drawable.ic_google, context.resources.getString(R.string.travel_search_engine_1, context.resources.getString(R.string.search_engine_name_google))))
                     list.add(CitySearchGoogleUiModel(keyword))
-                    list.add(CitySearchResultCategoryUiModel(R.drawable.ic_search_black, context.resources.getString(R.string.travel_search_engine_2)))
+                    list.add(CitySearchResultCategoryUiModel(R.drawable.ic_search_black, context.resources.getString(R.string.travel_search_engine_fxlite, context.resources.getString(R.string.app_name))))
                     list.addAll(result.data.result.map {
                         TravelMapper.toCitySearchResultUiModel(it.id, applyStyle(keyword, it.name), it.country, it.type)
                     })
