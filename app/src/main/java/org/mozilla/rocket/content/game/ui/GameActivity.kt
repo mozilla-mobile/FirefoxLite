636ea6deef162e035a2e799f87c17b559da716b4
proevan <yanghsin.lin@gmail.com>
2019-12-25 11:17:44 +0800

    support deep link to game item pasing through activities

--- app/src/main/java/org/mozilla/rocket/content/game/ui/GameActivity.kt
+++ app/src/main/java/org/mozilla/rocket/content/game/ui/GameActivity.kt
@@ -6,20 +6,24 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import android.os.Looper
+import android.os.Parcelable
 import android.view.View
 import androidx.fragment.app.FragmentActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.android.material.tabs.TabLayout
 import dagger.Lazy
+import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.activity_game.*
 import org.mozilla.focus.R
 import org.mozilla.focus.download.DownloadInfoManager
 import org.mozilla.focus.telemetry.TelemetryWrapper
 import org.mozilla.focus.utils.Constants
 import org.mozilla.rocket.content.appComponent
+import org.mozilla.rocket.content.common.data.ContentTabTelemetryData
 import org.mozilla.rocket.content.common.ui.VerticalTelemetryViewModel
 import org.mozilla.rocket.content.game.ui.adapter.GameTabsAdapter
 import org.mozilla.rocket.content.getViewModel
+import org.mozilla.rocket.util.sha256
 import javax.inject.Inject
 
 class GameActivity : FragmentActivity() {
@@ -42,6 +46,35 @@ class GameActivity : FragmentActivity() {
         initToolBar()
 
         initBroadcastReceivers()
+
+        intent.extras?.let {
+            parseDeepLink(it)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+    }
+
+    private fun parseDeepLink(bundle: Bundle): Boolean {
+        when (bundle.getString(EXTRA_DEEP_LINK) ?: return false) {
+            DEEP_LINK_GAME_ITEM_PAGE -> openGameItemPageFromDeepLink(bundle.getParcelable(EXTRA_GAME_ITEM_DATA)!!)
+        }
+
+        return true
+    }
+
+    private fun openGameItemPageFromDeepLink(gameItemData: DeepLink.GameItemPage.Data) {
+        val telemetryData = ContentTabTelemetryData(
+            TelemetryWrapper.Extra_Value.GAME,
+            gameItemData.feed,
+            gameItemData.source,
+            "", // no need when triggered by deep link
+            gameItemData.url.sha256(),
+            "", // no need when triggered by deep link
+            System.currentTimeMillis()
+        )
+        startActivity(GameModeActivity.getStartIntent(this, gameItemData.url, telemetryData))
     }
 
     private fun initViewPager() {
@@ -111,8 +144,30 @@ class GameActivity : FragmentActivity() {
         }
     }
 
+    sealed class DeepLink(val name: String) {
+        data class GameItemPage(val data: Data) : DeepLink(DEEP_LINK_GAME_ITEM_PAGE) {
+            @Parcelize
+            data class Data(val url: String, val feed: String, val source: String) : Parcelable
+        }
+    }
+
     companion object {
+        private const val EXTRA_DEEP_LINK = "extra_deep_link"
+        private const val EXTRA_GAME_ITEM_DATA = "extra_game_item_data"
+        private const val DEEP_LINK_GAME_ITEM_PAGE = "deep_link_game_item_page"
+
         fun getStartIntent(context: Context) =
             Intent(context, GameActivity::class.java).also { it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) }
+
+        fun getStartIntent(context: Context, deepLink: DeepLink): Intent = getStartIntent(context).apply {
+            putExtras(Bundle().apply {
+                putString(EXTRA_DEEP_LINK, deepLink.name)
+                when (deepLink) {
+                    is DeepLink.GameItemPage -> {
+                        putParcelable(EXTRA_GAME_ITEM_DATA, deepLink.data)
+                    }
+                }
+            })
+        }
     }
 }
