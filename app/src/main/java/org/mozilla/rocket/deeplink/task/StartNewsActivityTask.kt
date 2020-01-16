b38b1baea2521c08edf46061b108991b67c0e1d3
Evan Lin <yanghsin.lin@gmail.com>
2020-01-16 16:41:43 +0800

    clear task when execute vertical home deep link (#4644)

--- app/src/main/java/org/mozilla/rocket/deeplink/task/StartNewsActivityTask.kt
+++ app/src/main/java/org/mozilla/rocket/deeplink/task/StartNewsActivityTask.kt
@@ -1,10 +1,13 @@
 package org.mozilla.rocket.deeplink.task
 
 import android.content.Context
+import android.content.Intent
 import org.mozilla.rocket.content.news.ui.NewsActivity
 
 class StartNewsActivityTask : Task {
     override fun execute(context: Context) {
-        context.startActivity(NewsActivity.getStartIntent(context))
+        context.startActivity(NewsActivity.getStartIntent(context).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        })
     }
 }
--- app/src/main/java/org/mozilla/rocket/deeplink/task/StartShoppingActivityTask.kt
+++ app/src/main/java/org/mozilla/rocket/deeplink/task/StartShoppingActivityTask.kt
@@ -1,10 +1,13 @@
 package org.mozilla.rocket.deeplink.task
 
 import android.content.Context
+import android.content.Intent
 import org.mozilla.rocket.content.ecommerce.ui.ShoppingActivity
 
 class StartShoppingActivityTask : Task {
     override fun execute(context: Context) {
-        context.startActivity(ShoppingActivity.getStartIntent(context))
+        context.startActivity(ShoppingActivity.getStartIntent(context).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        })
     }
 }
