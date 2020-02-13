2ef8d93d93c90f70988394fe102d2594c0bd7a18
proevan <yanghsin.lin@gmail.com>
2020-02-13 23:16:01 +0800

    add JSON extension functions

--- app/src/main/java/org/mozilla/rocket/util/JsonUtils.kt
+++ app/src/main/java/org/mozilla/rocket/util/JsonUtils.kt
@@ -9,3 +9,20 @@ fun String.toJsonArray(): JSONArray = JSONArray(this)
 
 @Throws(JSONException::class)
 fun String.toJsonObject(): JSONObject = JSONObject(this)
+
+@Throws(JSONException::class)
+fun <T> String.toJsonArray(parser: (JSONObject) -> T): List<T> {
+    return toJsonArray().serialize(parser)
+}
+
+@Throws(JSONException::class)
+fun <T> JSONObject.getJsonArray(name: String, parser: (JSONObject) -> T): List<T> {
+    return getJSONArray(name).serialize(parser)
+}
+
+@Throws(JSONException::class)
+private fun <T> JSONArray.serialize(parser: (JSONObject) -> T): List<T> {
+    return (0 until this.length())
+            .map { index -> this.getJSONObject(index) }
+            .map { jsonObject -> parser(jsonObject) }
+}
