5930a5c9610a42d4d077144b5fc3162e8d453153
Evan Lin <yanghsin.lin@gmail.com>
2020-03-16 18:36:20 +0800

    support open private mode deep link (#4815)

--- app/src/main/java/org/mozilla/rocket/deeplink/DeepLinkConstants.kt
+++ app/src/main/java/org/mozilla/rocket/deeplink/DeepLinkConstants.kt
@@ -3,8 +3,8 @@ package org.mozilla.rocket.deeplink
 object DeepLinkConstants {
     const val SCHEMA_ROCKET = "rocket"
     const val HOST_CONTENT = "content"
+    const val HOST_PRIVATE_MODE = "private-mode"
     const val HOST_COMMAND = "command"
-
     const val PATH_GAME = "/game"
     const val PATH_GAME_ITEM = "/game/item"
     const val PATH_NEWS = "/news"
