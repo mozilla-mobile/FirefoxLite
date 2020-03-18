762f17c73a6d656161f4621db40cb3154d67e47b
Benjamin Cheng <chengwc@gmail.com>
2020-03-19 00:22:28 +0800

    Support shopping search trending terms suggestion

--- app/src/main/java/org/mozilla/rocket/shopping/search/data/KeywordSuggestionRepository.kt
+++ app/src/main/java/org/mozilla/rocket/shopping/search/data/KeywordSuggestionRepository.kt
@@ -6,7 +6,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import mozilla.components.concept.fetch.Request
 import org.json.JSONArray
+import org.json.JSONException
 import org.mozilla.focus.search.SearchEngineManager
+import org.mozilla.focus.utils.FirebaseHelper
 import org.mozilla.rocket.content.Result
 import org.mozilla.rocket.util.safeApiCall
 import org.mozilla.rocket.util.sendHttpRequest
@@ -31,6 +33,15 @@ class KeywordSuggestionRepository(appContext: Context) {
         )
     }
 
+    suspend fun fetchTrendingTerms(): Result<List<String>> = withContext(Dispatchers.IO) {
+        val terms = FirebaseHelper.getFirebase().getRcString(RC_KEY_STR_SHOPPING_SEARCH_TRENDING_TERMS)
+        return@withContext if (terms.isEmpty()) {
+            Result.Success(emptyList())
+        } else {
+            Result.Success(parseTrendingTerms(terms))
+        }
+    }
+
     private fun getSuggestionApiEndpoint(keyword: String): String {
         return searchEngine.buildSearchSuggestionUrl(keyword)
     }
@@ -49,7 +60,25 @@ class KeywordSuggestionRepository(appContext: Context) {
         return suggestions
     }
 
+    private fun parseTrendingTerms(response: String): List<String> {
+        val suggestions = arrayListOf<String>()
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                val suggestionItems = JSONArray(response)
+                val size = suggestionItems.length()
+
+                for (i in 0 until size.coerceAtMost(MAX_SUGGESTION_COUNT)) {
+                    suggestions.add(suggestionItems.getString(i))
+                }
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }
+        return suggestions
+    }
+
     companion object {
         private const val MAX_SUGGESTION_COUNT = 5
+        private const val RC_KEY_STR_SHOPPING_SEARCH_TRENDING_TERMS = "str_shopping_search_trending_terms"
     }
 }
