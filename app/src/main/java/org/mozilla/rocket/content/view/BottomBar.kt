adb0256ecbddaffd9ddaefae10555afaa18002db
proevan <yanghsin.lin@gmail.com>
2020-03-31 13:01:16 +0800

    allows BottomBar to handle CoordinatorLayout Behavior

--- app/src/main/java/org/mozilla/rocket/content/view/BottomBar.kt
+++ app/src/main/java/org/mozilla/rocket/content/view/BottomBar.kt
@@ -1,7 +1,6 @@
 package org.mozilla.rocket.content.view
 
 import android.content.Context
-import androidx.core.content.ContextCompat
 import android.util.AttributeSet
 import android.util.SparseIntArray
 import android.view.ContextThemeWrapper
@@ -10,11 +9,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
+import com.google.android.material.behavior.HideBottomViewOnScrollBehavior
 import org.mozilla.focus.R
 import org.mozilla.focus.widget.EqualDistributeGrid
 import org.mozilla.rocket.nightmode.themed.ThemedImageButton
 
-class BottomBar : FrameLayout {
+class BottomBar : FrameLayout, CoordinatorLayout.AttachedBehavior {
     private lateinit var grid: EqualDistributeGrid
     private var onItemClickListener: OnItemClickListener? = null
     private var onItemLongClickListener: OnItemLongClickListener? = null
@@ -110,6 +112,40 @@ class BottomBar : FrameLayout {
                     imageTintList = ContextCompat.getColorStateList(contextThemeWrapper, tintResId)
                 }
             }
+        }
+    }
+
+    override fun getBehavior(): CoordinatorLayout.Behavior<*> = BottomBarBehavior()
+
+    private class BottomBarBehavior : HideBottomViewOnScrollBehavior<BottomBar> {
+
+        constructor() : super()
+
+        constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+        private var currentState = STATE_SCROLLED_DOWN
+
+        override fun onNestedScroll(coordinatorLayout: CoordinatorLayout, child: BottomBar, target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int) {
+            if (currentState != STATE_SCROLLED_DOWN && dyUnconsumed < 0) {
+                slideUp(child)
+            } else if (currentState != STATE_SCROLLED_UP && dyUnconsumed > 0) {
+                slideDown(child)
+            }
+        }
+
+        override fun slideUp(child: BottomBar) {
+            super.slideUp(child)
+            currentState = STATE_SCROLLED_DOWN
+        }
+
+        override fun slideDown(child: BottomBar) {
+            super.slideDown(child)
+            currentState = STATE_SCROLLED_UP
+        }
+
+        companion object {
+            private const val STATE_SCROLLED_DOWN = 1
+            private const val STATE_SCROLLED_UP = 2
         }
     }
 }
