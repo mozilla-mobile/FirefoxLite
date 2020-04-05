c5a3f72bf6a7c234ede5788afacaa3c8bfaf875a
Benjamin Cheng <chengwc@gmail.com>
2020-04-06 00:05:03 +0800

    Remove the onboarding of shopping search setting

--- app/src/main/java/org/mozilla/rocket/shopping/search/ui/ShoppingSearchResultViewModel.kt
+++ app/src/main/java/org/mozilla/rocket/shopping/search/ui/ShoppingSearchResultViewModel.kt
@@ -9,21 +9,16 @@ import org.mozilla.rocket.download.SingleLiveEvent
 import org.mozilla.rocket.extension.switchMap
 import org.mozilla.rocket.shopping.search.domain.GetShoppingSearchSitesUseCase
 import org.mozilla.rocket.shopping.search.domain.GetShoppingSearchSitesUseCase.ShoppingSearchSite
-import org.mozilla.rocket.shopping.search.domain.SetSearchResultOnboardingIsShownUseCase
 import org.mozilla.rocket.shopping.search.domain.ShouldEnableTurboModeUseCase
-import org.mozilla.rocket.shopping.search.domain.ShouldShowSearchResultOnboardingUseCase
 
 class ShoppingSearchResultViewModel(
     private val getShoppingSearchSites: GetShoppingSearchSitesUseCase,
-    shouldEnableTurboMode: ShouldEnableTurboModeUseCase,
-    shouldShowSearchResultOnboarding: ShouldShowSearchResultOnboardingUseCase,
-    setSearchResultOnboardingIsShown: SetSearchResultOnboardingIsShownUseCase
+    shouldEnableTurboMode: ShouldEnableTurboModeUseCase
 ) : ViewModel() {
 
     private val searchKeyword = MutableLiveData<String>()
     private val shoppingSearchSites: LiveData<List<ShoppingSearchSite>> = searchKeyword.switchMap { getShoppingSearchSites(it) }
     val goPreferences = SingleLiveEvent<Unit>()
-    val showOnboardingDialog = SingleLiveEvent<Unit>()
     val goBackToInputPage = SingleLiveEvent<Unit>()
 
     val uiModel = MediatorLiveData<ShoppingSearchResultUiModel>().apply {
@@ -33,13 +28,6 @@ class ShoppingSearchResultViewModel(
                 return@addSource
             }
             value = newUiModel
-        }
-    }
-
-    init {
-        if (shouldShowSearchResultOnboarding()) {
-            showOnboardingDialog.call()
-            setSearchResultOnboardingIsShown()
         }
     }
 
