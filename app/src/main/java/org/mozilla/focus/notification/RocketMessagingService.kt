bb3de5ada680750c415cde23aa5ce9e5b0da7ee8
Nevin Chen <cnevinchen@gmail.com>
2020-04-13 11:12:50 +0800

    Add remote config to pref off server push

--- app/src/main/java/org/mozilla/focus/notification/RocketMessagingService.kt
+++ app/src/main/java/org/mozilla/focus/notification/RocketMessagingService.kt
@@ -53,6 +53,12 @@ class RocketMessagingService : FirebaseMessagingServiceWrapper() {
 
     override fun onDataMessage(data: MutableMap<String, String>) {
 
+        val serverPushDisabled = FirebaseHelper.getFirebase().getRcBoolean(BOOL_IS_SERVER_PUSH_DISABLED)
+        Log.d(TAG, "onDataMessage disabled:$$serverPushDisabled ")
+        if (serverPushDisabled) {
+            // return early if we pref off this feature in Firebase Remote Config
+            return
+        }
         val messageId = parseMessageId(data)
         val title = parseTitle(data)
         val body = parseBody(data)
@@ -133,6 +139,7 @@ class RocketMessagingService : FirebaseMessagingServiceWrapper() {
 
         private const val TAG = "RocketMessagingService"
         private const val STR_USER_TOKEN_API = "str_user_token_api"
+        private const val BOOL_IS_SERVER_PUSH_DISABLED = "bool_is_server_push_disabled"
 
         fun scheduleNotification(applicationContext: Context, messageId: String, imageUrl: String?, title: String?, body: String?, openUrl: String?, pushCommand: String?, deepLink: String?, displayTimestamp: Long) {
 
