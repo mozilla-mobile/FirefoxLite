67eb3e570f7b9f47d073279f3d0d5b591961f1c2
proevan <yanghsin.lin@gmail.com>
2020-04-20 18:30:57 +0800

    apply paging library to newspoint

--- app/src/main/java/org/mozilla/rocket/content/news/data/NewsDataSourceFactory.kt
+++ app/src/main/java/org/mozilla/rocket/content/news/data/NewsDataSourceFactory.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.paging.DataSource
 import org.mozilla.rocket.content.news.data.dailyhunt.DailyHuntNewsRemoteDataSource
 import org.mozilla.rocket.content.news.data.dailyhunt.DailyHuntProvider
+import org.mozilla.rocket.content.news.data.newspoint.NewsPointNewsRemoteDataSource
 
 class NewsDataSourceFactory(
     private val appContext: Context
@@ -13,19 +14,17 @@ class NewsDataSourceFactory(
     lateinit var language: String
 
     override fun create(): DataSource<Int, NewsItem> {
-        val dailyHuntProvider = DailyHuntProvider.getProvider(appContext)
-        return DailyHuntNewsRemoteDataSource(appContext, dailyHuntProvider, category, language)
-        // TODO: support other sources
-//        val newsProvider = NewsProvider.getNewsProvider()
-//        return if (newsProvider?.isNewsPoint() == true) {
-//            val dailyHuntProvider = DailyHuntProvider.getProvider(appContext)
-//            if (dailyHuntProvider?.shouldEnable(appContext) == true) {
-//                NewsRepository(DailyHuntNewsRemoteDataSource(appContext, dailyHuntProvider))
-//            } else {
-//                NewsRepository(NewsPointNewsRemoteDataSource(newsProvider))
-//            }
-//        } else {
+        val newsProvider = NewsProvider.getNewsProvider()
+        return if (newsProvider?.isNewsPoint() == true) {
+            val dailyHuntProvider = DailyHuntProvider.getProvider(appContext)
+            if (dailyHuntProvider?.shouldEnable(appContext) == true) {
+                DailyHuntNewsRemoteDataSource(appContext, dailyHuntProvider, category, language)
+            } else {
+                NewsPointNewsRemoteDataSource(NewsProvider.getNewsProvider(), category, language)
+            }
+        } else {
+            TODO("support other sources")
 //            NewsRepository(RssNewsRemoteDataSource(newsProvider))
-//        }
+        }
     }
 }
--- app/src/main/java/org/mozilla/rocket/content/news/data/newspoint/NewsPointNewsRemoteDataSource.kt
+++ app/src/main/java/org/mozilla/rocket/content/news/data/newspoint/NewsPointNewsRemoteDataSource.kt
@@ -1,35 +1,65 @@
 package org.mozilla.rocket.content.news.data.newspoint
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import androidx.paging.PageKeyedDataSource
 import mozilla.components.concept.fetch.Request
 import org.json.JSONObject
 import org.mozilla.rocket.content.Result
-import org.mozilla.rocket.content.news.data.NewsDataSource
 import org.mozilla.rocket.content.news.data.NewsItem
 import org.mozilla.rocket.content.news.data.NewsProvider
-import org.mozilla.rocket.util.safeApiCall
 import org.mozilla.rocket.util.sendHttpRequest
 import org.mozilla.rocket.util.sha256
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class NewsPointNewsRemoteDataSource(private val newsProvider: NewsProvider?) : NewsDataSource {
+class NewsPointNewsRemoteDataSource(
+    private val newsProvider: NewsProvider?,
+    private val category: String,
+    private val language: String
+) : PageKeyedDataSource<Int, NewsItem>() {
 
-    override suspend fun getNewsItems(category: String, language: String, pages: Int, pageSize: Int): Result<List<NewsItem>> = withContext(Dispatchers.IO) {
-        return@withContext safeApiCall(
-            call = {
-                sendHttpRequest(request = Request(url = getApiEndpoint(category, language, pages, pageSize), method = Request.Method.GET),
-                    onSuccess = {
-                        Result.Success(fromJson(it.body.string()))
-                    },
-                    onError = {
-                        Result.Error(it)
-                    }
-                )
+    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, NewsItem>) {
+        val pageSize = params.requestedLoadSize
+        val pages = 1
+
+        val result = fetchNewsItems(category, language, pageSize, pages)
+        if (result is Result.Success) {
+            callback.onResult(result.data, null, 2)
+        } // TODO: error handling
+    }
+
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, NewsItem>) {
+        val pageSize = params.requestedLoadSize
+        val pages = params.key
+
+        val result = fetchNewsItems(category, language, pageSize, pages)
+        if (result is Result.Success) {
+            callback.onResult(result.data, pages - 1)
+        } // TODO: error handling
+    }
+
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, NewsItem>) {
+        val pageSize = params.requestedLoadSize
+        val pages = params.key
+
+        val result = fetchNewsItems(category, language, pageSize, pages)
+        if (result is Result.Success) {
+            callback.onResult(result.data, pages + 1)
+        } // TODO: error handling
+    }
+
+    private fun fetchNewsItems(category: String, language: String, pages: Int, pageSize: Int): Result<List<NewsItem>> {
+        return sendHttpRequest(
+            request = Request(
+                url = getApiEndpoint(category, language, pages, pageSize),
+                method = Request.Method.GET
+            ),
+            onSuccess = {
+                Result.Success(fromJson(it.body.string()))
             },
-            errorMessage = "Unable to get news items ($category, $language, $pages, $pageSize)"
+            onError = {
+                Result.Error(it)
+            }
         )
     }
 
