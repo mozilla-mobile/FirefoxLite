1b58f2d9c684908cfcec17f5b0801d916b5ce007
Benjamin Cheng <chengwc@gmail.com>
2020-05-06 15:07:06 +0800

    Add default buttons for landscape mode

--- app/src/main/java/org/mozilla/rocket/chrome/BottomBarViewModel.kt
+++ app/src/main/java/org/mozilla/rocket/chrome/BottomBarViewModel.kt
@@ -7,18 +7,29 @@ import org.mozilla.rocket.chrome.BottomBarItemAdapter.ItemData
 
 class BottomBarViewModel : ViewModel() {
     val items = MutableLiveData<List<ItemData>>()
+    private var isLandscapeMode = false
 
     init {
         refresh()
     }
 
     fun refresh() {
-        val configuredItems = getConfiguredItems() ?: DEFAULT_BOTTOM_BAR_ITEMS
+        val configuredItems =
+            if (isLandscapeMode) {
+                DEFAULT_LANDSCAPE_BOTTOM_BAR_ITEMS
+            } else {
+                getConfiguredItems() ?: DEFAULT_BOTTOM_BAR_ITEMS
+            }
         items.value.let { currentValue ->
             if (configuredItems != currentValue) {
                 items.value = configuredItems
             }
         }
+    }
+
+    fun onScreenRotatedToLandscape(isLandscapeMode: Boolean) {
+        this.isLandscapeMode = isLandscapeMode
+        refresh()
     }
 
     private fun getConfiguredItems(): List<ItemData>? = AppConfigWrapper.getBottomBarItems()
@@ -31,6 +42,15 @@ class BottomBarViewModel : ViewModel() {
             ItemData(BottomBarItemAdapter.TYPE_SEARCH),
             ItemData(BottomBarItemAdapter.TYPE_TAB_COUNTER),
             ItemData(BottomBarItemAdapter.TYPE_MENU)
+        )
+
+        @JvmStatic
+        val DEFAULT_LANDSCAPE_BOTTOM_BAR_ITEMS: List<ItemData> = listOf(
+            ItemData(BottomBarItemAdapter.TYPE_HOME),
+            ItemData(BottomBarItemAdapter.TYPE_REFRESH),
+            ItemData(BottomBarItemAdapter.TYPE_SEARCH),
+            ItemData(BottomBarItemAdapter.TYPE_TAB_COUNTER),
+            ItemData(BottomBarItemAdapter.TYPE_SHARE)
         )
     }
 }
