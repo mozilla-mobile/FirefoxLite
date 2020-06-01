9309d9eebaed5311e87b4636c0d0b4c184e47ae0
proevan <yanghsin.lin@gmail.com>
2020-06-01 15:09:09 +0800

    set up click listener for menu items

--- app/src/main/java/org/mozilla/rocket/menu/HomeMenuDialog.kt
+++ app/src/main/java/org/mozilla/rocket/menu/HomeMenuDialog.kt
@@ -3,18 +3,30 @@ package org.mozilla.rocket.menu
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.annotation.StyleRes
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.Lazy
+import kotlinx.android.synthetic.main.bottom_sheet_home_menu.view.btn_private_browsing
 import kotlinx.android.synthetic.main.bottom_sheet_home_menu.view.menu_bookmark
+import kotlinx.android.synthetic.main.bottom_sheet_home_menu.view.menu_delete
 import kotlinx.android.synthetic.main.bottom_sheet_home_menu.view.menu_download
+import kotlinx.android.synthetic.main.bottom_sheet_home_menu.view.menu_exit
 import kotlinx.android.synthetic.main.bottom_sheet_home_menu.view.menu_history
+import kotlinx.android.synthetic.main.bottom_sheet_home_menu.view.menu_night_mode
+import kotlinx.android.synthetic.main.bottom_sheet_home_menu.view.menu_preferences
 import kotlinx.android.synthetic.main.bottom_sheet_home_menu.view.menu_screenshots
+import org.mozilla.fileutils.FileUtils
 import org.mozilla.focus.R
 import org.mozilla.focus.telemetry.TelemetryWrapper
+import org.mozilla.focus.utils.FormatUtils
 import org.mozilla.rocket.chrome.ChromeViewModel
 import org.mozilla.rocket.content.appComponent
 import org.mozilla.rocket.content.getActivityViewModel
+import org.mozilla.rocket.extension.toFragmentActivity
+import org.mozilla.rocket.nightmode.AdjustBrightnessDialog
 import javax.inject.Inject
 
 class HomeMenuDialog : BottomSheetDialog {
@@ -35,11 +47,13 @@ class HomeMenuDialog : BottomSheetDialog {
         chromeViewModel = getActivityViewModel(chromeViewModelCreator)
 
         initLayout()
+        observeChromeAction()
     }
 
     private fun initLayout() {
         contentLayout = layoutInflater.inflate(R.layout.bottom_sheet_home_menu, null)
         initMenuTabs(contentLayout)
+        initMenuItems(contentLayout)
         setContentView(contentLayout)
     }
 
@@ -65,5 +79,50 @@ class HomeMenuDialog : BottomSheetDialog {
                 TelemetryWrapper.clickMenuDownload()
             }
         }
+    }
+
+    private fun initMenuItems(contentLayout: View) {
+        contentLayout.apply {
+            btn_private_browsing.setOnClickListener {
+                cancel()
+                chromeViewModel.togglePrivateMode.call()
+                TelemetryWrapper.togglePrivateMode(true)
+            }
+            menu_night_mode.setOnClickListener {
+                chromeViewModel.adjustNightMode()
+            }
+            menu_preferences.setOnClickListener {
+                cancel()
+                chromeViewModel.checkToDriveDefaultBrowser()
+                chromeViewModel.openPreference.call()
+                TelemetryWrapper.clickMenuSettings()
+            }
+            menu_delete.setOnClickListener {
+                cancel()
+                onDeleteClicked()
+                TelemetryWrapper.clickMenuClearCache()
+            }
+            menu_exit.setOnClickListener {
+                cancel()
+                chromeViewModel.exitApp.call()
+                TelemetryWrapper.clickMenuExit()
+            }
+        }
+    }
+
+    private fun onDeleteClicked() {
+        val diff = FileUtils.clearCache(context)
+        val stringId = if (diff < 0) R.string.message_clear_cache_fail else R.string.message_cleared_cached
+        val msg = context.getString(stringId, FormatUtils.getReadableStringFromFileSize(diff))
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun observeChromeAction() {
+        val activity = context.toFragmentActivity()
+        chromeViewModel.showAdjustBrightness.observe(activity, Observer { showAdjustBrightness() })
+    }
+
+    private fun showAdjustBrightness() {
+        ContextCompat.startActivity(context, AdjustBrightnessDialog.Intents.getStartIntentFromMenu(context), null)
     }
 }
