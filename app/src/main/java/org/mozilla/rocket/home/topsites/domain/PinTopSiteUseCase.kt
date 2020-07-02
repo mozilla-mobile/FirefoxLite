11a71d9e75ffa40bee09c5849b45086a32edcabf
Benjamin Cheng <chengwc@gmail.com>
2020-07-02 12:43:13 +0800

    Do not allow to add the recommended site which is already a fixed site

--- app/src/main/java/org/mozilla/rocket/home/topsites/domain/PinTopSiteUseCase.kt
+++ app/src/main/java/org/mozilla/rocket/home/topsites/domain/PinTopSiteUseCase.kt
@@ -14,13 +14,13 @@ class PinTopSiteUseCase(private val topSitesRepo: TopSitesRepo) {
         val fixedSites = topSitesRepo.getConfiguredFixedSites() ?: emptyList()
         val pinnedSites = topSitesRepo.getPinnedSites()
         val siteModel = site.toSiteModel()
-        val existingIndex = pinnedSites.indexOfFirst { pinnedSite -> pinnedSite.title == siteModel.title && pinnedSite.url == siteModel.url }
+        val existingIndex = (fixedSites + pinnedSites).indexOfFirst { pinnedSite -> pinnedSite.title == siteModel.title && pinnedSite.url == siteModel.url }
         return@withContext when {
             fixedSites.size + pinnedSites.size >= MAX_TOP_SITES_COUNT -> {
                 PinTopSiteResult.FullyPinned
             }
             existingIndex != -1 -> {
-                PinTopSiteResult.Existing(fixedSites.size + existingIndex)
+                PinTopSiteResult.Existing(existingIndex)
             }
             else -> {
                 topSitesRepo.pin(site.toSiteModel())
