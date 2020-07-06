f720b70333292177cfa47dca86a5eba5243537be
proevan <yanghsin.lin@gmail.com>
2020-07-06 17:30:25 +0800

    implement UI of first run page for new onboarding

--- app/src/main/java/org/mozilla/focus/fragment/FirstrunFragment.kt
+++ app/src/main/java/org/mozilla/focus/fragment/FirstrunFragment.kt
@@ -5,13 +5,81 @@
 
 package org.mozilla.focus.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first_run.description
+import kotlinx.android.synthetic.main.fragment_first_run.item_browsing
+import kotlinx.android.synthetic.main.fragment_first_run.item_games
+import kotlinx.android.synthetic.main.fragment_first_run.item_news
+import kotlinx.android.synthetic.main.fragment_first_run.item_shopping
+import org.mozilla.focus.R
+import org.mozilla.focus.fragment.FirstrunFragment.ContentPrefItem.Browsing
+import org.mozilla.focus.fragment.FirstrunFragment.ContentPrefItem.Games
+import org.mozilla.focus.fragment.FirstrunFragment.ContentPrefItem.News
+import org.mozilla.focus.fragment.FirstrunFragment.ContentPrefItem.Shopping
 import org.mozilla.focus.navigation.ScreenNavigator.Screen
 
 class FirstrunFragment : Fragment(), Screen {
 
+    private var currentSelectedItem: ContentPrefItem? = null
+
     override fun getFragment(): Fragment {
         return this
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_first_run, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        description.text = getString(R.string.firstrun_fxlite_2_5_title_B, getString(R.string.app_name))
+        initContentPrefItems()
+    }
+
+    private fun initContentPrefItems() {
+        setContentPrefSelected(Browsing)
+
+        item_browsing.setOnClickListener { setContentPrefSelected(Browsing) }
+        item_shopping.setOnClickListener { setContentPrefSelected(Shopping) }
+        item_games.setOnClickListener { setContentPrefSelected(Games) }
+        item_news.setOnClickListener { setContentPrefSelected(News) }
+    }
+
+    private fun setContentPrefSelected(item: ContentPrefItem) {
+        if (currentSelectedItem == item) return
+
+        currentSelectedItem = item
+        listOf(Browsing, Shopping, Games, News).groupBy { it == item }.run {
+            get(true)?.forEach { view?.setContentPrefSelected(it, true) }
+            get(false)?.forEach { view?.setContentPrefSelected(it, false) }
+        }
+    }
+
+    private fun View.setContentPrefSelected(item: ContentPrefItem, selected: Boolean) {
+        val view = this.findViewById<View>(item.viewId)
+        val icon = view.findViewById<View>(item.iconId)
+        val textView = view.findViewById<TextView>(item.textId)
+        view.isSelected = selected
+        icon.isVisible = selected
+        val textColorId = if (selected) {
+            R.color.paletteWhite100
+        } else {
+            R.color.paletteDarkGreyC100
+        }
+        textView.setTextColor(ContextCompat.getColor(context, textColorId))
+    }
+
+    private sealed class ContentPrefItem(val viewId: Int, val textId: Int, val iconId: Int) {
+        object Browsing : ContentPrefItem(R.id.item_browsing, R.id.text_browsing, R.id.icon_browsing)
+        object Shopping : ContentPrefItem(R.id.item_shopping, R.id.text_shopping, R.id.icon_shopping)
+        object Games : ContentPrefItem(R.id.item_games, R.id.text_games, R.id.icon_games)
+        object News : ContentPrefItem(R.id.item_news, R.id.text_news, R.id.icon_news)
     }
 
     // TODO: Evan, add back to version 2.5 onboarding
