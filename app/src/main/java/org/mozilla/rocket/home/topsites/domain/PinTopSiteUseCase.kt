cdbb9bb27d9a875cbc2e96ecb4d6532fb2914756
Benjamin Cheng <chengwc@gmail.com>
2020-07-07 13:42:22 +0800

    Sync duplicated top site logic

--- app/src/main/java/org/mozilla/rocket/home/topsites/domain/PinTopSiteUseCase.kt
+++ app/src/main/java/org/mozilla/rocket/home/topsites/domain/PinTopSiteUseCase.kt
@@ -14,7 +14,7 @@ class PinTopSiteUseCase(private val topSitesRepo: TopSitesRepo) {
         val fixedSites = topSitesRepo.getConfiguredFixedSites() ?: emptyList()
         val pinnedSites = topSitesRepo.getPinnedSites()
         val siteModel = site.toSiteModel()
-        val existingIndex = (fixedSites + pinnedSites).indexOfFirst { pinnedSite -> pinnedSite.title == siteModel.title && pinnedSite.url == siteModel.url }
+        val existingIndex = (fixedSites + pinnedSites).indexOfFirst { pinnedSite -> pinnedSite.url.removeUrlPostSlash() == siteModel.url.removeUrlPostSlash() }
         return@withContext when {
             fixedSites.size + pinnedSites.size >= MAX_TOP_SITES_COUNT -> {
                 PinTopSiteResult.FullyPinned
