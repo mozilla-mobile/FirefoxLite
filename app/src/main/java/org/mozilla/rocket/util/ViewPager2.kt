c6137f04b4577f6bbae7236787c32a8f6dc648e3
Benjamin Cheng <chengwc@gmail.com>
2020-07-09 17:31:14 +0800

    Implement smooth scroll effect on top site view pager

---
+++ app/src/main/java/org/mozilla/rocket/util/ViewPager2.kt
@@ -0,0 +1,47 @@
+package org.mozilla.rocket.util
+
+import android.animation.Animator
+import android.animation.TimeInterpolator
+import android.animation.ValueAnimator
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.viewpager2.widget.ViewPager2
+import org.mozilla.focus.utils.ViewUtils
+
+fun ViewPager2.setCurrentItem(
+    item: Int,
+    duration: Long,
+    interpolator: TimeInterpolator = AccelerateDecelerateInterpolator(),
+    pagePxWidth: Int = width // Default value taken from getWidth() from ViewPager2 view
+) {
+    val pxToDrag: Int = pagePxWidth * (item - currentItem)
+    val animator = ValueAnimator.ofInt(0, pxToDrag)
+    var previousValue = 0
+    animator.addUpdateListener { valueAnimator ->
+        val currentValue = valueAnimator.animatedValue as Int
+        val currentPxToDrag = (currentValue - previousValue).toFloat()
+        if (ViewUtils.isRTL(this)) {
+            fakeDragBy(currentPxToDrag)
+        } else {
+            fakeDragBy(-currentPxToDrag)
+        }
+        previousValue = currentValue
+    }
+    animator.addListener(object : Animator.AnimatorListener {
+        override fun onAnimationStart(animation: Animator?) {
+            beginFakeDrag()
+        }
+
+        override fun onAnimationEnd(animation: Animator?) {
+            endFakeDrag()
+        }
+
+        override fun onAnimationCancel(animation: Animator?) { /* Ignored */
+        }
+
+        override fun onAnimationRepeat(animation: Animator?) { /* Ignored */
+        }
+    })
+    animator.interpolator = interpolator
+    animator.duration = duration
+    animator.start()
+}
