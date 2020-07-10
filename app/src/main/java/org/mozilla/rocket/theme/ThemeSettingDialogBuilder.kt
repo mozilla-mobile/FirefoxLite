b077d2abdcd91d145194f1f83a7e0dedc92bd086
Benjamin Cheng <chengwc@gmail.com>
2020-07-10 13:13:58 +0800

    Implement theme setting dialog behavior

---
+++ app/src/main/java/org/mozilla/rocket/theme/ThemeSettingDialogBuilder.kt
@@ -0,0 +1,103 @@
+package org.mozilla.rocket.theme
+
+import android.app.Dialog
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.FragmentActivity
+import org.mozilla.focus.R
+import org.mozilla.focus.utils.Settings
+import org.mozilla.rocket.home.HomeViewModel
+
+class ThemeSettingDialogBuilder(
+    private val activity: FragmentActivity,
+    private val homeViewModel: HomeViewModel
+) {
+
+    private lateinit var themeButton1: ImageView
+    private lateinit var themeButton2: ImageView
+    private lateinit var themeButton3: ImageView
+    private lateinit var themeButton4: ImageView
+    private lateinit var themeButton5: ImageView
+
+    fun show() {
+        create().show()
+    }
+
+    private fun create(): Dialog {
+        val customContentView = View.inflate(activity, R.layout.dialog_theme_setting, null)
+
+        return AlertDialog.Builder(activity, R.style.ThemeSettingDialog)
+            .setView(customContentView)
+            .create()
+            .also { dialog ->
+                val currentTheme = ThemeManager.getCurrentThemeName(activity)
+                val isDarkThemeEnable = Settings.getInstance(activity).isDarkThemeEnable
+                customContentView.setOnClickListener { dialog.dismiss() }
+                themeButton1 = customContentView.findViewById<ImageView>(R.id.theme_image_view_1).apply {
+                    if (currentTheme == ThemeManager.ThemeSet.Default.name && !isDarkThemeEnable) {
+                        isSelected = true
+                    }
+                    setOnClickListener { view ->
+                        onThemeButtonClicked(view)
+                    }
+                }
+                themeButton2 = customContentView.findViewById<ImageView>(R.id.theme_image_view_2).apply {
+                    if (currentTheme == ThemeManager.ThemeSet.Theme2.name && !isDarkThemeEnable) {
+                        isSelected = true
+                    }
+                    setOnClickListener { view ->
+                        onThemeButtonClicked(view)
+                    }
+                }
+                themeButton3 = customContentView.findViewById<ImageView>(R.id.theme_image_view_3).apply {
+                    if (currentTheme == ThemeManager.ThemeSet.Theme3.name && !isDarkThemeEnable) {
+                        isSelected = true
+                    }
+                    setOnClickListener { view ->
+                        onThemeButtonClicked(view)
+                    }
+                }
+                themeButton4 = customContentView.findViewById<ImageView>(R.id.theme_image_view_4).apply {
+                    if (currentTheme == ThemeManager.ThemeSet.Theme4.name && !isDarkThemeEnable) {
+                        isSelected = true
+                    }
+                    setOnClickListener { view ->
+                        onThemeButtonClicked(view)
+                    }
+                }
+                themeButton5 = customContentView.findViewById<ImageView>(R.id.theme_image_view_5).apply {
+                    if (isDarkThemeEnable) {
+                        isSelected = true
+                    }
+                    setOnClickListener { view ->
+                        onThemeButtonClicked(view)
+                    }
+                }
+                customContentView.findViewById<View>(R.id.action_done).apply {
+                    setOnClickListener {
+                        dialog.dismiss()
+                    }
+                }
+            }
+    }
+
+    private fun onThemeButtonClicked(clickedThemeButton: View) {
+        listOf(themeButton1, themeButton2, themeButton3, themeButton4, themeButton5).forEach { themeButton ->
+            if (themeButton == clickedThemeButton) {
+                if (!themeButton.isSelected) {
+                    when (clickedThemeButton) {
+                        themeButton1 -> homeViewModel.onThemeClicked(false, ThemeManager.ThemeSet.Default)
+                        themeButton2 -> homeViewModel.onThemeClicked(false, ThemeManager.ThemeSet.Theme2)
+                        themeButton3 -> homeViewModel.onThemeClicked(false, ThemeManager.ThemeSet.Theme3)
+                        themeButton4 -> homeViewModel.onThemeClicked(false, ThemeManager.ThemeSet.Theme4)
+                        themeButton5 -> homeViewModel.onThemeClicked(true, ThemeManager.ThemeSet.Default)
+                    }
+                }
+                themeButton.isSelected = true
+            } else {
+                themeButton.isSelected = false
+            }
+        }
+    }
+}
