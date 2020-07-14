4b54c14af8b87e403bc24f414f154c9e11de0186
Evan Lin <yanghsin.lin@gmail.com>
2020-07-14 10:26:27 +0800

    clean up FirstrunFragment before starting develop version 2.5 onboarding (#5180)
    * clean up FirstrunFragment before starting develop 2.5 onboarding
    
    * delete first run unused files
    
    * remove unused resources
    
    * set unused first run string resource to untransable before deleting

--- app/src/main/java/org/mozilla/focus/fragment/FirstrunFragment.kt
+++ app/src/main/java/org/mozilla/focus/fragment/FirstrunFragment.kt
@@ -5,214 +5,32 @@
 
 package org.mozilla.focus.fragment
 
-import android.content.Context
-import android.content.Intent
-import android.content.pm.ApplicationInfo
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.TransitionDrawable
-import android.os.Bundle
-import android.transition.TransitionInflater
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.PagerAdapter
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
-import org.mozilla.focus.R
-import org.mozilla.focus.activity.MainActivity
-import org.mozilla.focus.firstrun.UpgradeFirstrunPagerAdapter
 import org.mozilla.focus.navigation.ScreenNavigator.Screen
-import org.mozilla.focus.telemetry.TelemetryWrapper
-import org.mozilla.focus.utils.DialogUtils
-import org.mozilla.focus.utils.NewFeatureNotice
-import org.mozilla.rocket.periodic.FirstLaunchWorker
-import org.mozilla.rocket.periodic.PeriodicReceiver
 
-class FirstrunFragment : Fragment(), View.OnClickListener, Screen {
-    private lateinit var viewPager: ViewPager
-
-    private lateinit var bgTransitionDrawable: TransitionDrawable
-    private lateinit var bgDrawables: Array<Drawable>
-
-    private var isTelemetryValid = true
-    private var telemetryStartTimestamp: Long = 0
-    private var pageStartTime: Long = 0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (isNewUser()) {
-            TelemetryWrapper.showFirstRunOnBoarding()
-        } else {
-            TelemetryWrapper.showWhatsnewOnBoarding()
-        }
-        initDrawables()
-    }
-
-    private fun isNewUser(): Boolean = NewFeatureNotice.getInstance(requireContext()).lastShownFeatureVersion == 0
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-
-        val transition = TransitionInflater.from(context).inflateTransition(R.transition.firstrun_exit)
-
-        exitTransition = transition
-
-        // We will send a telemetry event whenever a new firstrun page is shown. However this page
-        // listener won't fire for the initial page we are showing. So we are going to firing here.
-        isTelemetryValid = true
-        telemetryStartTimestamp = System.currentTimeMillis()
-        pageStartTime = System.currentTimeMillis()
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_firstrun, container, false)
-        view.isClickable = true
-
-        view.findViewById<View>(R.id.skip).setOnClickListener(this)
-
-        val background = view.findViewById<View>(R.id.background)
-        background.background = bgTransitionDrawable
-
-        val adapter = findPagerAdapter(container!!.context, this)
-        if (adapter == null) {
-            finishFirstrun()
-            return view
-        }
-
-        viewPager = view.findViewById<View>(R.id.pager) as ViewPager
-
-        viewPager.setPageTransformer(true) { page, position -> page.alpha = 1 - 0.5f * Math.abs(position) }
-
-        viewPager.clipToPadding = false
-        viewPager.adapter = adapter
-        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-
-            override fun onPageSelected(newIdx: Int) {
-                val duration = 400
-                val nextDrawable = bgDrawables[newIdx % bgDrawables.size]
-
-                if (newIdx % 2 == 0) {
-                    // next page is even number
-                    bgTransitionDrawable.setDrawableByLayerId(R.id.first_run_bg_even, nextDrawable)
-                    bgTransitionDrawable.reverseTransition(duration) // odd -> even
-                } else {
-                    // next page is odd number
-                    bgTransitionDrawable.setDrawableByLayerId(R.id.first_run_bg_odd, nextDrawable)
-                    bgTransitionDrawable.startTransition(duration) // even -> odd
-                }
-            }
-
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {}
-
-            override fun onPageScrollStateChanged(state: Int) {}
-        })
-
-        if (adapter.count > 1) {
-            val tabLayout = view.findViewById<View>(R.id.tabs) as TabLayout
-            tabLayout.setupWithViewPager(viewPager, true)
-        }
-
-        return view
-    }
-
-    override fun onPause() {
-        super.onPause()
-        isTelemetryValid = false
-    }
-
-    override fun onClick(view: View) {
-        when (view.id) {
-            R.id.next -> {
-                val timeSpent = System.currentTimeMillis() - pageStartTime
-                pageStartTime = System.currentTimeMillis()
-                if (isNewUser()) {
-                    TelemetryWrapper.clickFirstRunOnBoarding(timeSpent, viewPager.currentItem, false)
-                } else {
-                    TelemetryWrapper.clickWhatsnewOnBoarding(timeSpent, viewPager.currentItem, false)
-                }
-                viewPager.currentItem = viewPager.currentItem + 1
-            }
-
-            R.id.skip -> finishFirstrun()
-
-            R.id.finish -> {
-                promoteSetDefaultBrowserIfPreload()
-
-                if (isTelemetryValid) {
-                    val mode = NewFeatureNotice.getInstance(context).lastShownFeatureVersion
-                    TelemetryWrapper.finishFirstRunEvent(System.currentTimeMillis() - telemetryStartTimestamp, mode)
-                    val timeSpent = System.currentTimeMillis() - pageStartTime
-                    if (isNewUser()) {
-                        TelemetryWrapper.clickFirstRunOnBoarding(timeSpent, viewPager.currentItem, true)
-                    } else {
-                        TelemetryWrapper.clickWhatsnewOnBoarding(timeSpent, viewPager.currentItem, true)
-                    }
-                }
-                // reset the pref
-                finishFirstrun()
-            }
-
-            else -> throw IllegalArgumentException("Unknown view")
-        }
-    }
+class FirstrunFragment : Fragment(), Screen {
 
     override fun getFragment(): Fragment {
         return this
     }
 
-    private fun isSystemApp() = context?.applicationInfo?.flags?.and(ApplicationInfo.FLAG_SYSTEM) != 0
-
-    private fun promoteSetDefaultBrowserIfPreload(): FirstrunFragment {
-        // if it's a system app(preload), we'll like to promote set default browser when the user finish first run
-        if (isSystemApp()) {
-            DialogUtils.showDefaultSettingNotification(requireContext())
-        }
-        return this
-    }
-
-    private fun findPagerAdapter(context: Context, onClickListener: View.OnClickListener): PagerAdapter? {
-        return if (NewFeatureNotice.getInstance(getContext()).shouldShowLiteUpdate()) {
-            UpgradeFirstrunPagerAdapter(context, onClickListener)
-        } else {
-            null
-        }
-    }
-
-    private fun wrapButtonClickListener(onClickListener: View.OnClickListener): View.OnClickListener {
-        return View.OnClickListener { view ->
-            if (view.id == R.id.finish) {
-                activity?.sendBroadcast(Intent(activity, PeriodicReceiver::class.java).apply {
-                    action = FirstLaunchWorker.ACTION
-                })
-            }
-            onClickListener.onClick(view)
-        }
-    }
-
-    private fun finishFirstrun() {
-        NewFeatureNotice.getInstance(context).setFirstRunDidShow()
-        NewFeatureNotice.getInstance(context).setLiteUpdateDidShow()
-        (activity as MainActivity).firstrunFinished()
-    }
-
-    // FirstRun fragment is not used often, so we create drawables programmatically, instead of add
-    // lots of drawable resources
-    private fun initDrawables() {
-        bgDrawables = arrayOf(resources.getDrawable(R.drawable.bg_platform_onboarding_color, context?.theme), resources.getDrawable(R.drawable.bg_platform_onboarding_color, context?.theme), resources.getDrawable(R.drawable.bg_platform_onboarding_color, context?.theme), resources.getDrawable(R.drawable.bg_platform_onboarding_color, context?.theme))
-
-        bgTransitionDrawable = TransitionDrawable(bgDrawables)
-        bgTransitionDrawable.setId(0, R.id.first_run_bg_even)
-        bgTransitionDrawable.setId(1, R.id.first_run_bg_odd)
-    }
+    // TODO: Evan, add back to version 2.5 onboarding
+//    private fun wrapButtonClickListener(onClickListener: View.OnClickListener): View.OnClickListener {
+//        return View.OnClickListener { view ->
+//            if (view.id == R.id.finish) {
+//                activity?.sendBroadcast(Intent(activity, PeriodicReceiver::class.java).apply {
+//                    action = FirstLaunchWorker.ACTION
+//                })
+//            }
+//            onClickListener.onClick(view)
+//        }
+//    }
+//
+//    private fun finishFirstrun() {
+//        NewFeatureNotice.getInstance(context).setFirstRunDidShow()
+//        NewFeatureNotice.getInstance(context).setLiteUpdateDidShow()
+//        (activity as MainActivity).firstrunFinished()
+//    }
 
     companion object {
         @JvmStatic
