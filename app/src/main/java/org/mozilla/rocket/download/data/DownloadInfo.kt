8766f9de41e3b3f830d9c0ea2414db05943bb57d
Benjamin Cheng <chengwc@gmail.com>
2020-07-27 17:20:13 +0800

    Convert DownloadInfo to Kotlin by IDE

---
+++ app/src/main/java/org/mozilla/rocket/download/data/DownloadInfo.kt
@@ -0,0 +1,104 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.mozilla.rocket.download.data
+
+import android.text.TextUtils
+import java.util.Calendar
+import java.util.Formatter
+import java.util.Locale
+
+/**
+ * Created by anlin on 27/07/2017.
+ */
+class DownloadInfo {
+    var rowId: Long? = null
+    var downloadId: Long? = null
+    var status = 0
+        private set
+    var reason = 0
+    var size: String? = null
+        private set
+    var date: String? = null
+        private set
+    var fileName = ""
+        set(fileName) {
+            if (!TextUtils.isEmpty(fileName)) {
+                field = fileName
+            }
+        }
+    var mediaUri = ""
+        set(mediaUri) {
+            if (!TextUtils.isEmpty(mediaUri)) {
+                field = mediaUri
+            }
+        }
+    var mimeType = ""
+        set(mimeType) {
+            if (!TextUtils.isEmpty(mimeType)) {
+                field = mimeType
+            }
+        }
+    var fileUri = ""
+        set(fileUri) {
+            if (!TextUtils.isEmpty(fileUri)) {
+                field = fileUri
+            }
+        }
+    var fileExtension = ""
+        set(fileExtension) {
+            if (!TextUtils.isEmpty(fileExtension)) {
+                field = fileExtension
+            }
+        }
+    var sizeSoFar = 0.0
+    var sizeTotal = 0.0
+    var isRead = false
+
+    fun setStatusInt(status: Int) {
+        this.status = status
+    }
+
+    fun setSize(size: Double) {
+        this.size = convertByteToReadable(size)
+    }
+
+    fun setDate(millis: Long) {
+        date = convertMillis(millis)
+    }
+
+    private fun convertMillis(millis: Long): String {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = millis
+        return Formatter().format("%tB %td", calendar, calendar).toString()
+    }
+
+    private fun convertByteToReadable(bytes: Double): String {
+        var bytes = bytes
+        val dictionary = arrayOf("bytes", "KB", "MB", "GB")
+        var index: Int
+        index = 0
+        while (index < dictionary.size) {
+            if (bytes < 1024) {
+                break
+            }
+            bytes = bytes / 1024
+            index++
+        }
+        return String.format(Locale.getDefault(), "%.1f", bytes) + dictionary[index]
+    }
+
+    fun existInDownloadManager(): Boolean {
+        return status != STATUS_DELETED
+    }
+
+    companion object {
+        // An overriding extra definition at the same level with DownloadManager.STATUS_SUCCESSFUL
+        // for the status field of this class which is an alternative when the
+        // DownloadManager.COLUMN_STATUS field is not available because no matching entry is available
+        // in the DownloadManager's table.
+        const val STATUS_DELETED = -1
+        const val REASON_DEFAULT = -2
+    }
+}
