2bf87cb87b1c9cb8255b520bf4edef53e829a8fe
Stefan Arentz <stefan@arentz.ca>
2021-01-24 14:37:32 -0500

    Change target_sdk to 29

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val min_sdk = 21
-    const val target_sdk = 28
+    const val target_sdk = 29
     const val compile_sdk = 28
     const val build_tools = "28.0.3"
     const val version_code = 1
